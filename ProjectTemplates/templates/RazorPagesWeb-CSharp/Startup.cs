d510f46c57f31d7ba63fa97c114f8a03fa943192
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-04-22 19:36:06 +0200

    Adding a new web app teamplate generating a razor project (the other one is an mvc project)
    Improving the test-templates.bat file to take this new template into account

---
+++ ProjectTemplates/templates/RazorPagesWeb-CSharp/Startup.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+#if (OrganizationalAuth || IndividualB2CAuth)
+using Microsoft.AspNetCore.Authentication;
+#endif
+#if (OrganizationalAuth)
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+#if (MultiOrgAuth)
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+#endif
+using Microsoft.AspNetCore.Authorization;
+#endif
+#if (IndividualB2CAuth)
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+#endif
+using Microsoft.AspNetCore.Builder;
+#if (IndividualLocalAuth)
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+#endif
+using Microsoft.AspNetCore.Hosting;
+#if (RequiresHttps)
+using Microsoft.AspNetCore.HttpsPolicy;
+#endif
+#if (OrganizationalAuth)
+using Microsoft.AspNetCore.Mvc.Authorization;
+#endif
+#if (IndividualLocalAuth)
+using Microsoft.EntityFrameworkCore;
+using Company.WebApplication1.Data;
+#endif
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+#if(MultiOrgAuth)
+using Microsoft.IdentityModel.Tokens;
+#endif
+
+namespace Company.WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+#if (IndividualLocalAuth)
+            services.AddDbContext<ApplicationDbContext>(options =>
+#if (UseLocalDB)
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+#else
+                options.UseSqlite(
+                    Configuration.GetConnectionString("DefaultConnection")));
+#endif
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+#elif (OrganizationalAuth)
+            services.AddSignIn(Configuration, "AzureAd");
+            // Uncomment the following lines if you want your Web API to call a downstream API
+            // services.AddWebAppCallsProtectedWebApi(Configuration, 
+            //                                        new string[] { "user.read" }, 
+            //                                        "AzureAd")
+            //         .AddInMemoryTokenCaches();
+
+#elif (IndividualB2CAuth)
+            services.AddSignIn(Configuration, "AzureAdB2C");
+#endif
+#if (OrganizationalAuth)
+
+            services.AddRazorPages().AddMvcOptions(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            }).AddMicrosoftIdentityUI();
+#else
+            services.AddRazorPages();
+#endif
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+#if (IndividualLocalAuth)
+                app.UseDatabaseErrorPage();
+#endif
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+#if (RequiresHttps)
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+#else
+            }
+
+#endif
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+#if (OrganizationalAuth || IndividualAuth)
+            app.UseAuthentication();
+#endif
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+#if (IndividualB2CAuth || OrganizationalAuth)
+                endpoints.MapControllers();
+#endif
+            });
+        }
+    }
+}
--- ProjectTemplates/test-templates.bat
+++ ProjectTemplates/test-templates.bat
@@ -1,51 +1,74 @@
-REM Build and Install templates
-msbuild /t:pack AspNetCoreMicrosoftIdentityPlatformTemplates.csproj
+echo "Build and Install templates"
+msbuild /t:restore AspNetCoreMicrosoftIdentityWebProjectTemplates.csproj
+msbuild /t:pack AspNetCoreMicrosoftIdentityWebProjectTemplates.csproj
 cd bin
 cd Debug
-dotnet new -i Microsoft.IdentityPlatform.Templates.0.1.0.nupkg
+dotnet new -i Microsoft.Identity.Web.ProjectTemplates.0.1.0.nupkg
 
-REM Test templates
+echo "Test templates"
 mkdir tests
 cd tests
 
-REM Test Web app (Microsoft identity platform, Single Org)
-mkdir webapp
-cd webapp
+echo " Test Web app (Microsoft identity platform, MVC, Single Org)"
+mkdir mvcwebapp
+cd mvcwebapp
 dotnet new mvc2 --auth SingleOrg
 msbuild
 cd ..
 
-REM Test Web app (Microsoft identity platform, Mutiple Orgs)
-mkdir webapp-multi-org
-cd webapp-multi-org
+echo " Test Web app (Microsoft identity platform, MVC, Multiple Orgs)"
+mkdir mvcwebapp-multi-org
+cd mvcwebapp-multi-org
 dotnet new mvc2 --auth MultiOrg
 msbuild
 cd ..
 
-REM Test Web app (Azure AD B2C)
-mkdir webapp-b2c
-cd webapp-b2c
+echo " Test Web app (MVC, Azure AD B2C)"
+mkdir mvcwebapp-b2c
+cd mvcwebapp-b2c
 dotnet new mvc2 --auth  IndividualB2C
 msbuild
 cd ..
 
 
-REM Test Web API  (Microsoft identity platform, SingleOrg)
+echo " Test Web app (Microsoft identity platform, Razor, Single Org)"
+mkdir webapp
+cd webapp
+dotnet new webapp2 --auth SingleOrg
+msbuild
+cd ..
+
+echo " Test Web app (Microsoft identity platform, Razor, Multiple Orgs)"
+mkdir webapp-multi-org
+cd webapp-multi-org
+dotnet new webapp2 --auth MultiOrg
+msbuild
+cd ..
+
+echo " Test Web app Razor, Azure AD B2C)"
+mkdir webapp-b2c
+cd webapp-b2c
+dotnet new webapp2 --auth  IndividualB2C
+msbuild
+cd ..
+
+
+echo " Test Web API  (Microsoft identity platform, SingleOrg)"
 mkdir webapi
 cd webapi
 dotnet new webapi2 --auth SingleOrg
 msbuild
 cd ..
 
-REM Test Web API  (AzureAD B2C)
+echo " Test Web API  (AzureAD B2C)"
 mkdir webapi-b2c
 cd webapi-b2c
 dotnet new webapi2 --auth IndividualB2C
 msbuild
 cd ..
 
-REM Uninstall templates
+echo "Uninstall templates"
 cd ..
-dotnet new -u Microsoft.IdentityPlatform.Templates
+dotnet new -u Microsoft.Identity.Web.ProjectTemplates
 cd ..
 cd ..
