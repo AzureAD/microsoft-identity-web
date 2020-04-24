813844be0d20ab9284c5ec3bbecdd1ad58cf089a
jennyf19 <jeferrie@microsoft.com>
2020-04-24 09:23:34 -0700

    leverage msal.net's httpclient factory (#127)

---
+++ src/Microsoft.Identity.Web/MsalAspNetCoreHttpClientFactory.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Identity.Client;
+using System.Net.Http;
+
+namespace Microsoft.Identity.Web
+{
+    internal class MsalAspNetCoreHttpClientFactory : IMsalHttpClientFactory
+    {
+        private IHttpClientFactory _httpClientFactory;
+
+        public MsalAspNetCoreHttpClientFactory(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public HttpClient GetHttpClient()
+        {
+            return _httpClientFactory.CreateClient();
+        }
+    }
+}
