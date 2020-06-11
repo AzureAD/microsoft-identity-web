6f61d055aefac7322c7ef40654e5378fae867fde
Jenny Ferries <jeferrie@microsoft.com>
2020-06-11 12:26:41 -0700

    spelling edits

--- src/Microsoft.Identity.Web/MicrosoftIdentityOptions.cs
+++ src/Microsoft.Identity.Web/MicrosoftIdentityOptions.cs
@@ -123,11 +123,11 @@ namespace Microsoft.Identity.Web
 
         /// <summary>
         /// Specifies if the x5c claim (public key of the certificate) should be sent to the STS.
-        /// Sending the x5c enables application developers to achieve easy certificate roll-over in Azure AD:
+        /// Sending the x5c enables application developers to achieve easy certificate rollover in Azure AD:
         /// this method will send the public certificate to Azure AD along with the token request,
         /// so that Azure AD can use it to validate the subject name based on a trusted issuer policy.
         /// This saves the application admin from the need to explicitly manage the certificate rollover
-        /// (either via portal or powershell/CLI operation). For details see https://aka.ms/msal-net-sni.
+        /// (either via portal or PowerShell/CLI operation). For details see https://aka.ms/msal-net-sni.
         /// </summary>
         /// The default is <c>false.</c>
         public bool SendX5C { get; set; } = false;
