57184a32eb4dc97db0309d7e78002f5bb080f68a
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-06-19 23:29:21 +0200

    Adding calls to Web api in RazorPagesWeb-CSharp project template (#225)
    * Adding the webapp2 template
    * Fix the mvc2 template which was generating the "call an API" even without the flags to enable it (--called-api-url  and --called-api-scopes)
    
    Fixing the identity of the Web API 2.
    
    Updating the test-templates.bat file to:
    - add test for webapp2 and mvc2 calling Web APIs
    - replace msbuild by dotnet build

--- ProjectTemplates/templates/RazorPagesWeb-CSharp/Pages/Index.cshtml.cs
+++ ProjectTemplates/templates/RazorPagesWeb-CSharp/Pages/Index.cshtml.cs
@@ -2,16 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+#if (GenerateApi)
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
+using System.Net;
+using System.Net.Http;
+#endif
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace Company.WebApplication1.Pages
 {
+#if (GenerateApi)
+    using Services;
+
+    [AuthorizeForScopes(ScopeKeySection = "CalledApi:CalledApiScopes")]
+#endif 
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
+#if (GenerateApi)
+        private readonly IDownstreamWebApi _downstreamWebApi;
+
+        public IndexModel(ILogger<IndexModel> logger,
+                          IDownstreamWebApi downstreamWebApi)
+        {
+             _logger = logger;
+            _downstreamWebApi = downstreamWebApi;
+       }
+
+        public async Task OnGet()
+        {
+            ViewData["ApiResult"] = await _downstreamWebApi.CallWebApi();
+
+            // You can also specify the relative endpoint and the scopes
+            // ViewData["ApiResult"] = await _downstreamWebApi.CallWebApi("me",
+            //                                                             new string[] {"user.read"});
+        }
+#else
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -21,5 +51,6 @@ namespace Company.WebApplication1.Pages
         {
 
         }
+#endif
     }
 }
--- ProjectTemplates/test-templates.bat
+++ ProjectTemplates/test-templates.bat
@@ -1,9 +1,8 @@
 echo "Build and Install templates"
-msbuild /t:restore AspNetCoreMicrosoftIdentityWebProjectTemplates.csproj
-msbuild /t:pack AspNetCoreMicrosoftIdentityWebProjectTemplates.csproj
+dotnet pack AspNetCoreMicrosoftIdentityWebProjectTemplates.csproj
 cd bin
 cd Debug
-dotnet new -i Microsoft.Identity.Web.ProjectTemplates.0.1.0.nupkg
+dotnet new -i Microsoft.Identity.Web.ProjectTemplates.0.1.5.nupkg
 
 echo "Test templates"
 mkdir tests
@@ -13,21 +12,43 @@ echo " Test Web app (Microsoft identity platform, MVC, Single Org)"
 mkdir mvcwebapp
 cd mvcwebapp
 dotnet new mvc2 --auth SingleOrg
-msbuild
+dotnet build
 cd ..
 
 echo " Test Web app (Microsoft identity platform, MVC, Multiple Orgs)"
 mkdir mvcwebapp-multi-org
 cd mvcwebapp-multi-org
 dotnet new mvc2 --auth MultiOrg
-msbuild
+dotnet build
 cd ..
 
 echo " Test Web app (MVC, Azure AD B2C)"
 mkdir mvcwebapp-b2c
 cd mvcwebapp-b2c
 dotnet new mvc2 --auth  IndividualB2C
-msbuild
+dotnet build
+cd ..
+
+
+echo " Test Web app calling Web API (Microsoft identity platform, MVC, Single Org)"
+mkdir mvcwebapp-api
+cd mvcwebapp-api
+dotnet new mvc2 --auth SingleOrg --called-api-url "https://graph.microsoft.com/beta" --called-api-scopes "user.read"
+dotnet build
+cd ..
+
+echo " Test Web app calling Web API  (Microsoft identity platform, MVC, Multiple Orgs)"
+mkdir mvcwebapp-multi-org-api
+cd mvcwebapp-multi-org-api
+dotnet new mvc2 --auth MultiOrg --called-api-url "https://graph.microsoft.com/beta" --called-api-scopes "user.read"
+dotnet build
+cd ..
+
+echo " Test Web app calling Web API  (MVC, Azure AD B2C)"
+mkdir mvcwebapp-b2c-api
+cd mvcwebapp-b2c-api
+dotnet new mvc2 --auth  IndividualB2C --called-api-url "https://localhost:44332" --called-api-scopes "https://fabrikamb2c.onmicrosoft.com/tasks/read"
+dotnet build
 cd ..
 
 
@@ -35,36 +56,59 @@ echo " Test Web app (Microsoft identity platform, Razor, Single Org)"
 mkdir webapp
 cd webapp
 dotnet new webapp2 --auth SingleOrg
-msbuild
+dotnet build
 cd ..
 
 echo " Test Web app (Microsoft identity platform, Razor, Multiple Orgs)"
 mkdir webapp-multi-org
 cd webapp-multi-org
 dotnet new webapp2 --auth MultiOrg
-msbuild
+dotnet build
 cd ..
 
-echo " Test Web app Razor, Azure AD B2C)"
+echo " Test Web app (Razor, Azure AD B2C)"
 mkdir webapp-b2c
 cd webapp-b2c
 dotnet new webapp2 --auth  IndividualB2C
-msbuild
+dotnet build
 cd ..
+
+
+echo " Test Web app calling Web API  (Microsoft identity platform, Razor, Single Org)"
+mkdir webapp-api
+cd webapp-api
+dotnet new webapp2 --auth SingleOrg --called-api-url "https://graph.microsoft.com/beta" --called-api-scopes "user.read"
+dotnet build
+cd ..
+
+echo " Test Web app calling Web API  (Microsoft identity platform, Razor, Multiple Orgs)"
+mkdir webapp-multi-org-api
+cd webapp-multi-org-api
+dotnet new webapp2 --auth MultiOrg --called-api-url "https://graph.microsoft.com/beta" --called-api-scopes "user.read"
+dotnet build
+cd ..
+
+echo " Test Web app calling Web API (Razor, Azure AD B2C)"
+mkdir webapp-b2c-api
+cd webapp-b2c-api
+dotnet new webapp2 --auth  IndividualB2C --called-api-url "https://localhost:44332" --called-api-scopes "https://fabrikamb2c.onmicrosoft.com/tasks/read"
+dotnet build
+cd ..
+
 
 
 echo " Test Web API  (Microsoft identity platform, SingleOrg)"
 mkdir webapi
 cd webapi
 dotnet new webapi2 --auth SingleOrg
-msbuild
+dotnet build
 cd ..
 
 echo " Test Web API  (AzureAD B2C)"
 mkdir webapi-b2c
 cd webapi-b2c
 dotnet new webapi2 --auth IndividualB2C
-msbuild
+dotnet build
 cd ..
 
 echo "Uninstall templates"
