097315e8cacb17fc38fd062af5131e72a3228980
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-06-19 19:50:11 +0200

    Updating the outstanding project templates

--- ProjectTemplates/templates/RazorPagesWeb-CSharp/Pages/Index.cshtml.cs
+++ ProjectTemplates/templates/RazorPagesWeb-CSharp/Pages/Index.cshtml.cs
@@ -2,20 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+#if (GenerateApi)
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
+using System.Net;
+using System.Net.Http;
+#endif
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace Company.WebApplication1.Pages
 {
+#if (GenerateApi)
+    using Services;
+
+#endif 
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
+#if (GenerateApi)
+        private readonly IDownstreamWebApi _downstreamWebApi;
+
+        public IndexModel(ILogger<HomeController> logger,
+                          IDownstreamWebApi downstreamWebApi)
+        {
+             _logger = logger;
+            _downstreamWebApi = downstreamWebApi;
+       }
+
+        [AuthorizeForScopes(ScopeKeySection = "CalledApi:CalledApiScopes")]
+        public void OnGet()
+        {
+            ViewData["ApiResult"] = await _downstreamWebApi.CallWebApi();
+
+            // You can also specify the relative endpoint and the scopes
+            // ViewData["ApiResult"] = await _downstreamWebApi.CallWebApi("me", new string[] {"user.read"});
+
+            return View();
+        }
+#else
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
+#endif
 
         public void OnGet()
         {
