64a99b6f4b5274fbc1997f51a5e119f1867caef6
jennyf19 <jeferrie@microsoft.com>
2020-06-29 10:02:41 -0700

    create new class to inherit from the DistributedCacheEntryOptions to provide flexiblity w/token cache lifetime (#266)

---
+++ src/Microsoft.Identity.Web/TokenCacheProviders/Distributed/MsalDistributedTokenCacheAdapterOptions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Microsoft.Identity.Web.TokenCacheProviders.Distributed
+{
+    /// <summary>
+    /// Options for the MSAL token cache serialization adapter,
+    /// which delegates the serialization to the IDistributedCache implementations
+    /// available with .NET Core.
+    /// </summary>
+    public class MsalDistributedTokenCacheAdapterOptions : DistributedCacheEntryOptions
+    {
+    }
+}
