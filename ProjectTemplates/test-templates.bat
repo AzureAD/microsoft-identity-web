33d7bacef9bcbecd29e955535bfbbdda20f73f41
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-07-17 19:00:50 +0200

    Jmprieur/adding blazor templates (#329)
    * Initial commit
    
    * Fixing the template
    
    * Fixing the blazor template for B2C, to call graphs and API.
    All builds
    
    Tested for:
    - no auth
    - single auth
    - calls-graph
    - B2C
    
    Need to test
    - Calls API
    - B2C calls API
    
    * Requires Microsoft.Identity.Web 0.2.1-preview
    
    * Adding the possibliity of calling a Web API
    (from AAD and B2C)

--- ProjectTemplates/test-templates.bat
+++ ProjectTemplates/test-templates.bat
@@ -7,7 +7,7 @@ dotnet new -i Microsoft.Identity.Web.ProjectTemplates.0.2.0-preview.nupkg
 echo "Test templates"
 mkdir tests
 cd tests
-dotnet new sln
+dotnet new sln --name tests
 
 echo " Test Web app (No Auth)"
 mkdir webapp-noauth
@@ -56,14 +56,14 @@ cd ..
 echo " Test Web app calling Web API (Microsoft identity platform, MVC, Single Org)"
 mkdir mvcwebapp-api
 cd mvcwebapp-api
-dotnet new mvc2 --auth SingleOrg --called-api-url "https://graph.microsoft.com/beta" --called-api-scopes "user.read"
+dotnet new mvc2 --auth SingleOrg --called-api-url "https://graph.microsoft.com/beta/me" --called-api-scopes "user.read"
 dotnet sln ..\tests.sln add mvcwebapp-api.csproj
 cd ..
 
 echo " Test Web app calling Web API  (Microsoft identity platform, MVC, Multiple Orgs)"
 mkdir mvcwebapp-multi-org-api
 cd mvcwebapp-multi-org-api
-dotnet new mvc2 --auth MultiOrg --called-api-url "https://graph.microsoft.com/beta" --called-api-scopes "user.read"
+dotnet new mvc2 --auth MultiOrg --called-api-url "https://graph.microsoft.com/beta/me" --called-api-scopes "user.read"
 dotnet sln ..\tests.sln add mvcwebapp-multi-org-api.csproj
 cd ..
 
@@ -115,14 +115,14 @@ cd ..
 echo " Test Web app calling Web API  (Microsoft identity platform, Razor, Single Org)"
 mkdir webapp-api
 cd webapp-api
-dotnet new webapp2 --auth SingleOrg --called-api-url "https://graph.microsoft.com/beta" --called-api-scopes "user.read"
+dotnet new webapp2 --auth SingleOrg --called-api-url "https://graph.microsoft.com/beta/me" --called-api-scopes "user.read"
 dotnet sln ..\tests.sln add webapp-api.csproj
 cd ..
 
 echo " Test Web app calling Web API  (Microsoft identity platform, Razor, Multiple Orgs)"
 mkdir webapp-multi-org-api
 cd webapp-multi-org-api
-dotnet new webapp2 --auth MultiOrg --called-api-url "https://graph.microsoft.com/beta" --called-api-scopes "user.read"
+dotnet new webapp2 --auth MultiOrg --called-api-url "https://graph.microsoft.com/beta/me" --called-api-scopes "user.read"
 dotnet sln ..\tests.sln add webapp-multi-org-api.csproj
 cd ..
 
@@ -165,7 +165,7 @@ cd ..
 echo " Test Web API calling Web API (Microsoft identity platform, SingleOrg)"
 mkdir webapi-api
 cd webapi-api
-dotnet new webapi2 --auth SingleOrg --called-api-url "https://graph.microsoft.com/beta" --called-api-scopes "user.read"
+dotnet new webapi2 --auth SingleOrg --called-api-url "https://graph.microsoft.com/beta/me" --called-api-scopes "user.read"
 dotnet sln ..\tests.sln add webapi-api.csproj
 cd ..
 
@@ -183,8 +183,60 @@ dotnet new webapi2 --auth SingleOrg --calls-graph
 dotnet sln ..\tests.sln add webapi-graph.csproj
 cd ..
 
+
+mkdir blazor
+cd blazor
+
+echo " Test Blazor app (No auth)"
+mkdir blazorserver
+cd blazorserver
+dotnet new blazorserver2
+dotnet sln ..\..\tests.sln add blazorserver.csproj
+cd ..
+
+echo " Test Blazor app (Microsoft identity platform, SingleOrg)"
+mkdir blazorserver-SingleOrg
+cd blazorserver-SingleOrg
+dotnet new blazorserver2 --auth SingleOrg
+dotnet sln ..\..\tests.sln add blazorserver-SingleOrg.csproj
+cd ..
+
+echo " Test Blazor app   (AzureAD B2C)"
+mkdir blazorserver-b2c
+cd blazorserver-b2c
+dotnet new blazorserver2 --auth IndividualB2C
+dotnet sln ..\..\tests.sln add blazorserver-b2c.csproj
+cd ..
+
+echo " TTest Blazor app  calling Web API (Microsoft identity platform, SingleOrg)"
+mkdir blazorserver-api
+cd blazorserver-api
+dotnet new blazorserver2 --auth SingleOrg --called-api-url "https://graph.microsoft.com/beta/me" --called-api-scopes "user.read"
+dotnet sln ..\..\tests.sln add blazorserver-api.csproj
+cd ..
+
+echo " Test Blazor app  calling Web API (AzureAD B2C)"
+mkdir blazorserver-b2c-api
+cd blazorserver-b2c-api
+dotnet new blazorserver2 --auth IndividualB2C --called-api-url "https://localhost:44332" --called-api-scopes "https://fabrikamb2c.onmicrosoft.com/tasks/read"
+dotnet sln ..\..\tests.sln add blazorserver-b2c-api.csproj
+cd ..
+
+echo " Test Blazor app  calling Graph (Microsoft identity platform, SingleOrg)"
+mkdir blazorserver-graph
+cd blazorserver-graph
+dotnet new blazorserver2 --auth SingleOrg --calls-graph
+dotnet sln ..\..\tests.sln add blazorserver-graph.csproj
+cd ..
+
+REM Blazor
+cd ..
+
+
 echo "Build the solution with all the projects created by applying the templates"
 dotnet build
+
+
 
 echo "Uninstall templates"
 cd ..
