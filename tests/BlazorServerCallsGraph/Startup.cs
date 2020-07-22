79cbdfbe865884850416240027573e624589d8cd
Jenny Ferries <jeferrie@microsoft.com>
2020-07-22 09:48:32 -0700

    add license info to blazor dev apps

--- tests/BlazorServerCallsGraph/Startup.cs
+++ tests/BlazorServerCallsGraph/Startup.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
