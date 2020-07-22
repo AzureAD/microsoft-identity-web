f570e6076a4d464d75e9e5c4d113e6feb5b4fd6d
Jenny Ferries <jeferrie@microsoft.com>
2020-07-21 20:26:50 -0700

    fix warning

--- src/Microsoft.Identity.Web/Constants/ErrorCodes.cs
+++ src/Microsoft.Identity.Web/Constants/ErrorCodes.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Identity.Web
     {
         public const string MissingClientCredentials = "missing_client_credentials";
         public const string DuplicateClientCredentials = "duplicate_client_credentials";
-        
+
         // AzureAD B2C
         public const string B2CPasswordResetErrorCode = "AADSTS50013";
         public const string B2CForgottenPassword = "AADB2C90118";
