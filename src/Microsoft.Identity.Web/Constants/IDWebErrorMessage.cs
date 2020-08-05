d33ab13b7ef11f33fa4b8a4eca7949a79c95a144
pmaytak <34331512+pmaytak@users.noreply.github.com>
2020-08-04 23:07:16 -0700

    Fix validating B2C issuer with tfp in issuer URI. (#340)
    * Fix validating B2C issuer with tfp in issuer URI.

--- src/Microsoft.Identity.Web/Constants/IDWebErrorMessage.cs
+++ src/Microsoft.Identity.Web/Constants/IDWebErrorMessage.cs
@@ -33,6 +33,7 @@ namespace Microsoft.Identity.Web
         public const string IssuerMetadataUrlIsRequired = "IDW10301: Azure AD Issuer metadata address URL is required. ";
         public const string NoMetadataDocumentRetrieverProvided = "IDW10302: No metadata document retriever is provided. ";
         public const string IssuerDoesNotMatchValidIssuers = "IDW10303: Issuer: '{0}', does not match any of the valid issuers provided for this application. ";
+        public const string B2CTfpIssuerNotSupported = "IDW10304: Microsoft Identity Web does not support a B2C issuer with 'tfp' in the URI. See https://aka.ms/ms-id-web/b2c-issuer for details.";
 
         // Protocol IDW10400 = "IDW10400:"
         public const string TenantIdClaimNotPresentInToken = "IDW10401: Neither `tid` nor `tenantId` claim is present in the token obtained from Microsoft identity platform. ";
