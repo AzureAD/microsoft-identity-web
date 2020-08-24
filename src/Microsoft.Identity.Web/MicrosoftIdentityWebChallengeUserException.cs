f0339fccd41f145dbd15ce4b7b017a2ab8807275
Jenny Ferries <jeferrie@microsoft.com>
2020-08-23 20:16:53 -0700

    add blazorwsm for b2c testing and fix xml

--- src/Microsoft.Identity.Web/MicrosoftIdentityWebChallengeUserException.cs
+++ src/Microsoft.Identity.Web/MicrosoftIdentityWebChallengeUserException.cs
@@ -32,7 +32,7 @@ namespace Microsoft.Identity.Web
         /// Handles the user challenge for Blazor or Razor pages.
         /// </summary>
         /// <param name="msalUiRequiredException">Exception thrown by MSAL when a user challenge is encountered.</param>
-        /// <param name="scopes">Scopes to request.</param
+        /// <param name="scopes">Scopes to request.</param>
         /// <param name="userflow">Userflow used in B2C.</param>
         public MicrosoftIdentityWebChallengeUserException(
             MsalUiRequiredException msalUiRequiredException,
