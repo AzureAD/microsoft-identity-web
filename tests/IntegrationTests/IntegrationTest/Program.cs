53a2c8856c5868f16b9d2c3f62b1259203d23781
Jenny Ferries <jeferrie@microsoft.com>
2020-08-29 20:53:20 -0700

    add integration tests initial commit

---
+++ tests/IntegrationTests/IntegrationTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntegrationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
