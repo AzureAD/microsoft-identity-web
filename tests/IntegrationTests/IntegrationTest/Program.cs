6168cf739c76e30a24ab92ae599a26b4fbf1aaa2
Jenny Ferries <jeferrie@microsoft.com>
2020-08-30 19:48:54 -0700

    add lab integration

--- tests/IntegrationTests/IntegrationTest/Program.cs
+++ tests/IntegrationTests/IntegrationTest/Program.cs
@@ -2,21 +2,34 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using IntegrationTest.ClientBuilder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Client;
+using Microsoft.Identity.Web.Test.Common;
+using Microsoft.Identity.Web.Test.LabInfrastructure;
 
 namespace IntegrationTest
 {
     class Program
     {
         private static HttpClient httpClient = new HttpClient();
+        private const string Authority = "https://login.microsoftonline.com/organizations/";
+        private static readonly string[] s_scopes = { "User.Read" };
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            RunIntegrationTestLogic();
+        }
+
+        public static void RunIntegrationTestLogic()
+        {
+            KeyVaultSecretsProvider keyVault = new KeyVaultSecretsProvider();
+
             IServiceCollection services = new ServiceCollection();
 
             services.AddDistributedMemoryCache();
@@ -29,8 +42,8 @@ namespace IntegrationTest
 
             services.AddMicrosoftIdentityConfidentialClient((clientApplicationBuilderOptions) =>
             {
-                clientApplicationBuilderOptions.ClientId = "";
-                clientApplicationBuilderOptions.ClientSecret = "";
+                clientApplicationBuilderOptions.ClientId = TestConstants.ConfidentialClientId;
+                clientApplicationBuilderOptions.ClientSecret = keyVault.GetSecret(TestConstants.ConfidentialClientKeyVaultUri).Value;
             });
 
             IServiceProvider serviceProvider = services.BuildServiceProvider();
@@ -46,7 +59,23 @@ namespace IntegrationTest
             IConfidentialClientApplication confidentialClientApplication,
             ILogger<Program> logger)
         {
-            throw new NotImplementedException();
+            var result = await AcquireTokenForLabUserAsync();
+        }
+
+        private static async Task<AuthenticationResult> AcquireTokenForLabUserAsync()
+        {
+            var labResponse = await LabUserHelper.GetDefaultUserAsync().ConfigureAwait(false);
+            var msalPublicClient = PublicClientApplicationBuilder
+               .Create(labResponse.App.AppId)
+               .WithAuthority(labResponse.Lab.Authority, "organizations")
+               .Build();
+
+            AuthenticationResult authResult = await msalPublicClient
+                .AcquireTokenByUsernamePassword(s_scopes, labResponse.User.Upn, new NetworkCredential("", labResponse.User.GetOrFetchPassword()).SecurePassword)
+                .ExecuteAsync(CancellationToken.None)
+                .ConfigureAwait(false);
+
+            return authResult;
         }
     }
 }
