080d112958b99839cb2bb72acd351739f9fa427c
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-09-01 21:37:17 +0200

    Fix for https://github.com/AzureAD/microsoft-identity-web/issues/506 Separate Microsoft Graph support from Microsoft.Identity.web
    Needs more work on templates.

---
+++ src/Microsoft.Identity.Web.MicrosoftGraph/IDWebErrorMessage.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Identity.Web
+{
+    /// <summary>
+    /// Constants related to the error messages.
+    /// </summary>
+    internal static class IDWebErrorMessage
+    {
+        public const string CalledApiScopesAreNull = "IDW10207: The CalledApiScopes cannot be null. ";
+    }
+}
