8c18b0fb235170fb8e5b620a8df71d23e59a80f5
Jenny Ferries <jeferrie@microsoft.com>
2020-09-01 11:11:32 -0700

    add auth

--- tests/IntegrationTests/IntegrationTestService/Startup.cs
+++ tests/IntegrationTests/IntegrationTestService/Startup.cs
@@ -46,6 +46,7 @@ namespace IntegrationTestService
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
