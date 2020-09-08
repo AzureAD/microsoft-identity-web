31ab02c0b8dfd7636a7c4b97eaaec72cc04f082e
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-09-08 09:05:52 +0200

    Update src/Microsoft.Identity.Web/TokenAcquisition.cs
    Co-authored-by: jennyf19 <jeferrie@microsoft.com>

--- src/Microsoft.Identity.Web/TokenAcquisition.cs
+++ src/Microsoft.Identity.Web/TokenAcquisition.cs
@@ -672,7 +672,7 @@ namespace Microsoft.Identity.Web
         /// <param name="userFlow">Azure AD B2C user flow to target.</param>
         /// <param name="user">Optional claims principal representing the user. If not provided, will use the signed-in
         /// user (in a web app), or the user for which the token was received (in a web API)
-        /// cases where a given account is guest in other tenants, and you want to acquire tokens for a specific tenant, like where the user is a guest in.</param>
+        /// cases where a given account is a guest in other tenants, and you want to acquire tokens for a specific tenant, like where the user is a guest in.</param>
         /// <returns>An access token to call the downstream API and populated with this downstream API's scopes.</returns>
         /// <remarks>Calling this method from a web API supposes that you have previously called,
         /// in a method called by JwtBearerOptions.Events.OnTokenValidated, the HttpContextExtensions.StoreTokenUsedToCallWebAPI method
