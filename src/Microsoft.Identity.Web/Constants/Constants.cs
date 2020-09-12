96f7ac5c3599d1622b77abc3f5809040e6b1c93b
Jenny Ferries <jeferrie@microsoft.com>
2020-09-12 08:53:45 -0700

    move some constants to public

--- src/Microsoft.Identity.Web/Constants/Constants.cs
+++ src/Microsoft.Identity.Web/Constants/Constants.cs
@@ -4,69 +4,102 @@
 namespace Microsoft.Identity.Web
 {
     /// <summary>
-    /// General constants.
+    /// General constants for Microsoft Identity Web.
     /// </summary>
-    internal static class Constants
+    public static class Constants
     {
+        /// <summary>
+        /// LoginHint.
+        /// Represents the preferred_username claim in the ID token.
+        /// </summary>
+        public const string LoginHint = "loginHint";
+
+        /// <summary>
+        /// DomainHint.
+        /// Determined by the tenant Id.
+        /// </summary>
+        public const string DomainHint = "domainHint";
+
+        /// <summary>
+        /// Claims.
+        /// Determined from the signed-in user.
+        /// </summary>
+        public const string Claims = "claims";
+
+        /// <summary>
+        /// Bearer.
+        /// Predominant type of access token used with OAuth 2.0.
+        /// </summary>
+        public const string Bearer = "Bearer";
+
+        /// <summary>
+        /// AzureAd.
+        /// Configuration section name for AzureAd.
+        /// </summary>
+        public const string AzureAd = "AzureAd";
+
+        /// <summary>
+        /// AzureAdB2C.
+        /// Configuration section name for AzureAdB2C.
+        /// </summary>
+        public const string AzureAdB2C = "AzureAdB2C";
+
+        /// <summary>
+        /// Scope.
+        /// </summary>
+        public const string Scope = "scope";
+
         // IssuerMetadata
-        public const string TenantDiscoveryEndpoint = "tenant_discovery_endpoint";
-        public const string ApiVersion = "api-version";
-        public const string Metadata = "metadata";
+        internal const string TenantDiscoveryEndpoint = "tenant_discovery_endpoint";
+        internal const string ApiVersion = "api-version";
+        internal const string Metadata = "metadata";
 
         // Metadata
-        public const string PreferredNetwork = "preferred_network";
-        public const string PreferredCache = "preferred_cache";
-        public const string Aliases = "aliases";
+        internal const string PreferredNetwork = "preferred_network";
+        internal const string PreferredCache = "preferred_cache";
+        internal const string Aliases = "aliases";
 
         // AadIssuerValidator
-        public const string AzureADIssuerMetadataUrl = "https://login.microsoftonline.com/common/discovery/instance?authorization_endpoint=https://login.microsoftonline.com/common/oauth2/v2.0/authorize&api-version=1.1";
-        public const string FallbackAuthority = "https://login.microsoftonline.com/";
+        internal const string AzureADIssuerMetadataUrl = "https://login.microsoftonline.com/common/discovery/instance?authorization_endpoint=https://login.microsoftonline.com/common/oauth2/v2.0/authorize&api-version=1.1";
+        internal const string FallbackAuthority = "https://login.microsoftonline.com/";
 
         // RegisterValidAudience
-        public const string Version = "ver";
-        public const string V1 = "1.0";
-        public const string V2 = "2.0";
+        internal const string Version = "ver";
+        internal const string V1 = "1.0";
+        internal const string V2 = "2.0";
 
         // ClaimsPrincipalExtension
-        public const string MsaTenantId = "9188040d-6c67-4c5b-b112-36a304b66dad";
-        public const string Consumers = "consumers";
-        public const string Organizations = "organizations";
-        public const string Common = "common";
+        internal const string MsaTenantId = "9188040d-6c67-4c5b-b112-36a304b66dad";
+        internal const string Consumers = "consumers";
+        internal const string Organizations = "organizations";
+        internal const string Common = "common";
 
         // ClientInfo
-        public const string ClientInfo = "client_info";
-        public const string One = "1";
+        internal const string ClientInfo = "client_info";
+        internal const string One = "1";
 
         // Certificates
-        public const string MediaTypePksc12 = "application/x-pkcs12";
-        public const string PersonalUserCertificateStorePath = "CurrentUser/My";
+        internal const string MediaTypePksc12 = "application/x-pkcs12";
+        internal const string PersonalUserCertificateStorePath = "CurrentUser/My";
 
         // Miscellaneous
-        public const string UserAgent = "User-Agent";
-        public const string JwtSecurityTokenUsedToCallWebApi = "JwtSecurityTokenUsedToCallWebAPI";
-        public const string AzureAd = "AzureAd";
-        public const string AzureAdB2C = "AzureAdB2C";
-        public const string PreferredUserName = "preferred_username";
-        public const string NameClaim = "name";
-        public const string Tfp = "tfp";
-        public const string Consent = "consent";
-        public const string ConsentUrl = "consentUri";
-        public const string Claims = "claims";
-        public const string Scopes = "scopes";
-        public const string Scope = "scope";
-        public const string ProposedAction = "proposedAction";
-        public const string Bearer = "Bearer";
-        public const string LoginHint = "loginHint";
-        public const string DomainHint = "domainHint";
-        public const string Authorization = "Authorization";
-        public const string ApplicationJson = "application/json";
-        public const string ISessionStore = "ISessionStore";
+        internal const string UserAgent = "User-Agent";
+        internal const string JwtSecurityTokenUsedToCallWebApi = "JwtSecurityTokenUsedToCallWebAPI";
+        internal const string PreferredUserName = "preferred_username";
+        internal const string NameClaim = "name";
+        internal const string Consent = "consent";
+        internal const string ConsentUrl = "consentUri";
+        internal const string Scopes = "scopes";
+        internal const string ProposedAction = "proposedAction";
+        internal const string Authorization = "Authorization";
+        internal const string ApplicationJson = "application/json";
+        internal const string ISessionStore = "ISessionStore";
 
         // Blazor challenge URI
-        public const string BlazorChallengeUri = "MicrosoftIdentity/Account/Challenge?redirectUri=";
+        internal const string BlazorChallengeUri = "MicrosoftIdentity/Account/Challenge?redirectUri=";
 
         // Microsoft Graph
-        public const string UserReadScope = "user.read";
-        public const string GraphBaseUrlV1 = "https://graph.microsoft.com/v1.0";
+        internal const string UserReadScope = "user.read";
+        internal const string GraphBaseUrlV1 = "https://graph.microsoft.com/v1.0";
     }
 }
