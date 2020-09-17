272d20aedc0d77f5f7b544ea54c7fb4a3182a885
pmaytak <34331512+pmaytak@users.noreply.github.com>
2020-09-16 23:52:02 -0700

    Add TestController.

---
+++ tests/IntegrationTests/IntegrationTestService/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegrationTestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public string Index()
+        {
+            var result = "Result";
+            return result;
+        }
+        
+        [Authorize]
+        public string IndexAuth()
+        {
+            var result = "Result";
+            return result;
+        }
+    }
+}
