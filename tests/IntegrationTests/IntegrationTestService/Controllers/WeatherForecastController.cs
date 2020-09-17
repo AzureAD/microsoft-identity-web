56799dcde2f0fd0fcce86768159d7e9683b7370a
pmaytak <34331512+pmaytak@users.noreply.github.com>
2020-09-16 23:52:02 -0700

    Add TestController.

--- tests/IntegrationTests/IntegrationTestService/Controllers/WeatherForecastController.cs
+++ tests/IntegrationTests/IntegrationTestService/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Graph;
@@ -14,7 +15,7 @@ using Microsoft.Identity.Web.Test.Common;
 namespace IntegrationTestService.Controllers
 {
     [ApiController]
-    [Microsoft.AspNetCore.Authorization.Authorize]
+    [Authorize]
     [Route("SecurePage")]
     public class WeatherForecastController : ControllerBase
     {
