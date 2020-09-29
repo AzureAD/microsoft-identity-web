f8b4de843264e2cf1ddf171d4367611bf4d31756
Jennyf19 <jeferrie@microsoft.com>
2020-09-29 06:15:46 -0700

    separate out graph & call web api in blazorwasm

---
+++ ProjectTemplates/templates/ComponentsWebAssembly-CSharp/Server/Controllers/CallWebApiController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Identity.Web.Resource;
+
+namespace AspNetCoreMicrosoftIdentityWebProjectTemplates.templates.ComponentsWebAssembly_CSharp.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class CallWebApiController : Controller
+    {
+        // The web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
+        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
+
+        private readonly IDownstreamWebApi _downstreamWebApi;
+
+        public CallWebApiController(ILogger<CallWebApiController> logger,
+                              IDownstreamWebApi downstreamWebApi)
+        {
+            _logger = logger;
+            _downstreamWebApi = downstreamWebApi;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+
+            using var response = await _downstreamWebApi.CallWebApiForUserAsync("DownstreamApi").ConfigureAwait(false);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                // Do something
+            }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                throw new HttpRequestException($"Invalid status code in the HttpResponseMessage: {response.StatusCode}: {error}");
+            }
+        }
+    }
+}
