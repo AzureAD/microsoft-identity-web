c3ba7a494b2660499bdf08a918b780671433bbab
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-10-06 17:02:54 +0200

    Attempt to fix the failing test in .NET 5.0 (#658)
    * Update dotnetcore.yml
    
    Attempt to understand why the build with preview 7 fails on GitHub but passes locally
    
    * Update dotnetcore.yml
    
    Apart from the platform, another variation with our local build is the configuration
    
    * Attempt to build on GitHub
    
    * Updating the Release build
    
    * Fixing stylecop issue
    
    * Update dotnetcore.yml
    
    * Attempt to remove the credscan error in the release build

--- tests/Microsoft.Identity.Web.Test/WebAppExtensionsTests.cs
+++ tests/Microsoft.Identity.Web.Test/WebAppExtensionsTests.cs
@@ -706,14 +706,14 @@ namespace Microsoft.Identity.Web.Test
             // then, the PopulateOpenIdOptionsFromMicrosoftIdentityOptions method
             // needs to be updated. For this uncomment the 2 lines below, and run the test
             // then diff the files to find what are the new properties
-            int numberOfProperties = typeof(OpenIdConnectOptions).GetProperties().Length;
+            int numberOfProperties = typeof(OpenIdConnectOptions).GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).Length;
 
             int expectedNumberOfProperties;
 #if DOTNET_CORE_31
             expectedNumberOfProperties = 54;
             // System.IO.File.WriteAllLines(@"c:\temp\core31.txt", typeof(OpenIdConnectOptions).GetProperties().Select(p => p.Name));
 #elif DOTNET_50
-            expectedNumberOfProperties = 56;
+            expectedNumberOfProperties = 57;
             // System.IO.File.WriteAllLines(@"c:\temp\net5.txt", typeof(OpenIdConnectOptions).GetProperties().Select(p => p.Name));
 #endif
             Assert.Equal(expectedNumberOfProperties, numberOfProperties);
