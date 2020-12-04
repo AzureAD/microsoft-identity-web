b80d35b28e785ad47709f699f966696d7f7c41fc
jennyf19 <jeferrie@microsoft.com>
2020-12-04 12:28:53 -0800

    add more generic consent handler (#807)

--- src/Microsoft.Identity.Web/MicrosoftIdentityCircuitHandler.cs
+++ src/Microsoft.Identity.Web/MicrosoftIdentityCircuitHandler.cs
@@ -40,6 +40,25 @@ namespace Microsoft.Identity.Web
             builder.Services.TryAddScoped<MicrosoftIdentityConsentAndConditionalAccessHandler>();
             return builder;
         }
+
+        /// <summary>
+        /// Add the incremental consent and conditional access handler for
+        /// web app pages, Razor pages, controllers, views, etc...
+        /// </summary>
+        /// <param name="services">Service collection.</param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection AddMicrosoftIdentityConsentHandler(
+            this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.TryAddEnumerable(ServiceDescriptor.Scoped<CircuitHandler, MicrosoftIdentityServiceHandler>());
+            services.TryAddScoped<MicrosoftIdentityConsentAndConditionalAccessHandler>();
+            return services;
+        }
     }
 
     /// <summary>
