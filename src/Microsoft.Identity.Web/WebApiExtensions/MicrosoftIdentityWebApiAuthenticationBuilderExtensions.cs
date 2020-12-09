7f5c422bffaf6e6b74addb767e211f78746a2fe4
jennyf19 <jeferrie@microsoft.com>
2020-12-09 08:30:18 -0800

    Jennyf/fix warnings (#829)
    * testing something for null ref warning
    
    * revert change from intellisense
    
    * handle null ref warning

--- src/Microsoft.Identity.Web/WebApiExtensions/MicrosoftIdentityWebApiAuthenticationBuilderExtensions.cs
+++ src/Microsoft.Identity.Web/WebApiExtensions/MicrosoftIdentityWebApiAuthenticationBuilderExtensions.cs
@@ -219,10 +219,10 @@ namespace Microsoft.Identity.Web
                     var tokenValidatedHandler = options.Events.OnTokenValidated;
                     options.Events.OnTokenValidated = async context =>
                     {
-                        if (!microsoftIdentityOptions.AllowWebApiToBeAuthorizedByACL && !context!.Principal.Claims.Any(x => x.Type == ClaimConstants.Scope)
-                            && !context.Principal.Claims.Any(y => y.Type == ClaimConstants.Scp)
-                            && !context.Principal.Claims.Any(y => y.Type == ClaimConstants.Roles)
-                            && !context.Principal.Claims.Any(y => y.Type == ClaimConstants.Role))
+                        if (!microsoftIdentityOptions.AllowWebApiToBeAuthorizedByACL && !context!.Principal!.Claims.Any(x => x.Type == ClaimConstants.Scope)
+                            && !context!.Principal!.Claims.Any(y => y.Type == ClaimConstants.Scp)
+                            && !context!.Principal!.Claims.Any(y => y.Type == ClaimConstants.Roles)
+                            && !context!.Principal!.Claims.Any(y => y.Type == ClaimConstants.Role))
                         {
                             throw new UnauthorizedAccessException(IDWebErrorMessage.NeitherScopeOrRolesClaimFoundInToken);
                         }
