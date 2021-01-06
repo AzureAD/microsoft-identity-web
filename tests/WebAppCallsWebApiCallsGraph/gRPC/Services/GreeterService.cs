a4488699917a8ffa9102eba3a6cb8891f1d2f9a2
Jennyf19 <jeferrie@microsoft.com>
2021-01-06 13:49:36 -0800

    updates to test app

--- tests/WebAppCallsWebApiCallsGraph/gRPC/Services/GreeterService.cs
+++ tests/WebAppCallsWebApiCallsGraph/gRPC/Services/GreeterService.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.AspNetCore.Authorization;
