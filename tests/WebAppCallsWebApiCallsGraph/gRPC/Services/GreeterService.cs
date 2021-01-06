29f493b6b5ce034b250ae40fce0c16227eb258ff
Jennyf19 <jeferrie@microsoft.com>
2021-01-05 19:37:07 -0800

    initial commit for gRPC

---
+++ tests/WebAppCallsWebApiCallsGraph/gRPC/Services/GreeterService.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
+
+namespace grpc
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        static string[] scopeRequiredByAPI = new string[] { "access_as_user" };
+
+        [Authorize]
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            var httpContext = context.GetHttpContext();
+            httpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByAPI);
+
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
