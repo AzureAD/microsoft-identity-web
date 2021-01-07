cee375826b8f4db042d3b513597478fcdf4fa1b1
Sahil Malik <sahilmalik@winsmarts.com>
2021-01-07 03:01:54 -0500

    Addressed jennyf19's PR comments

--- src/Microsoft.Identity.Web/AzureFunctionsAuthenticationHttpContextExtension.cs
+++ src/Microsoft.Identity.Web/AzureFunctionsAuthenticationHttpContextExtension.cs
@@ -18,18 +18,18 @@ namespace Microsoft.Identity.Web
         /// Enables Bearer authentication for an API for use in Azure Functions.
         /// </summary>
         /// <param name="httpContext">The current HTTP Context, such as req.HttpContext.</param>
-        /// <returns>A task indicating success or failure. In case of failure <see cref="Microsoft.Identity.Web.UnauthorizedObjectResult"/>.</returns>
+        /// <returns>A task indicating success or failure. In case of failure <see cref="Microsoft.AspNetCore.Mvc.UnauthorizedObjectResult"/>.</returns>
         public static async Task<(bool, IActionResult?)> AuthenticateAzureFunctionAsync(
             this HttpContext httpContext)
         {
             if (httpContext == null)
             {
-                throw new ArgumentNullException("Parameter httpContext cannot be null");
+                throw new ArgumentNullException(nameof(httpContext));
             }
 
             AuthenticateResult? result =
-                await httpContext.AuthenticateAsync("Bearer").ConfigureAwait(false);
-            if (result.Succeeded)
+                await httpContext.AuthenticateAsync(Constants.Bearer).ConfigureAwait(false);
+            if (result != null && result.Succeeded)
             {
                 httpContext.User = result.Principal;
                 return (true, null);
@@ -39,7 +39,7 @@ namespace Microsoft.Identity.Web
                 return (false, new UnauthorizedObjectResult(new ProblemDetails
                 {
                     Title = "Authorization failed.",
-                    Detail = result.Failure?.Message,
+                    Detail = result?.Failure?.Message,
                 }));
             }
         }
