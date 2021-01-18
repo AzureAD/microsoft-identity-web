a8f98b359a8349c92577017d11e6dcf3b1bb2608
jennyf19 <jeferrie@microsoft.com>
2021-01-18 15:27:10 -0800

    fix grpc template startup (#888)

--- ProjectTemplates/templates/Worker-CSharp/Startup.cs
+++ ProjectTemplates/templates/Worker-CSharp/Startup.cs
@@ -24,6 +24,8 @@ namespace Company.Application1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddGrpc();
+
 #if (OrganizationalAuth)
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
