324a140588131a3bcbe2d48b54a8308d391db94d
Jennyf19 <jeferrie@microsoft.com>
2021-01-23 10:58:21 -0800

    move api calls to greeterService

--- ProjectTemplates/templates/Worker-CSharp/Worker.cs
+++ ProjectTemplates/templates/Worker-CSharp/Worker.cs
@@ -3,24 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-#if (!NoAuth)
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-#endif
-#if (GenerateApi)
-using Microsoft.Extensions.Configuration;
-using Microsoft.Identity.Web;
-using System.Net;
-using System.Net.Http;
-#endif
-#if (GenerateGraph)
-using Microsoft.Graph;
-#endif
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-#if (OrganizationalAuth || IndividualB2CAuth)
-using Microsoft.Identity.Web.Resource;
-#endif
 
 namespace Company.Application1
 {
@@ -28,69 +12,6 @@ namespace Company.Application1
     {
         private readonly ILogger<Worker> _logger;
 
-#if (!NoAuth)
-        // The web API will only accept tokens 1) for users, and 2) having the "api-scope" scope for this API
-        static readonly string[] scopeRequiredByApi = new string[] { "api-scope" };
-#endif
-
-
-#if (GenerateApi)
-        private readonly IDownstreamWebApi _downstreamWebApi;
-
-        public Worker(ILogger<Worker> logger,
-                        IDownstreamWebApi downstreamWebApi)
-        {
-            _logger = logger;
-            _downstreamWebApi = downstreamWebApi;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
-
-            using var response = await _downstreamWebApi.CallWebApiForUserAsync("DownstreamApi").ConfigureAwait(false);
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                var apiResult = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                // Do something
-            }
-            else
-            {
-                var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                throw new HttpRequestException($"Invalid status code in the HttpResponseMessage: {response.StatusCode}: {error}");
-            }
-        }
-
-#elseif (GenerateGraph)
-        private readonly GraphServiceClient _graphServiceClient;
-
-         public Worker(ILogger<Worker> logger,
-                        GraphServiceClient graphServiceClient)
-        {
-            _logger = logger;
-            _graphServiceClient = graphServiceClient;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
-
-            var user = await _graphServiceClient.Me.Request().GetAsync();
-        }
-
-#else
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
@@ -98,15 +19,11 @@ namespace Company.Application1
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-#if (!NoAuth)
-            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-#endif
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
-#endif
     }
 }
