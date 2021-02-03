43bc082a953c73a77f1fc1bd350dc7b48c8acc4e
Jean-Marc Prieur <jmprieur@microsoft.com>
2021-02-03 16:58:09 +0100

    Returning JSon, instead of string

--- tests/AzureFunctions/SampleFunc/SampleFunc.cs
+++ tests/AzureFunctions/SampleFunc/SampleFunc.cs
@@ -59,7 +59,8 @@ namespace SampleFunc
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            return new JsonResult(responseMessage);
+          //  return new OkObjectResult(responseMessage);
         }
     }
 }
