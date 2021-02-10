9beb3de0c4dc8473e984e1c3cdc673ddd6603697
jennyf19 <jeferrie@microsoft.com>
2021-02-10 08:47:59 -0800

    make cert loader public (#949)

--- src/Microsoft.Identity.Web/CertificateManagement/ICertificateLoader.cs
+++ src/Microsoft.Identity.Web/CertificateManagement/ICertificateLoader.cs
@@ -5,8 +5,20 @@ namespace Microsoft.Identity.Web
 {
     /// <summary>
     /// Interface to implement loading of a certificate.
+    /// Only use when loading a certificate from a daemon application, or an ASP NET app, using MSAL .NET directly.
+    /// For an ASP NET Core app, <b>Microsoft Identity Web will handle the certificate loading</b> for you.
+    /// <example><code>
+    /// IConfidentialClientApplication app;
+    /// ICertificateLoader certificateLoader = new DefaultCertificateLoader();
+    ///     certificateLoader.LoadIfNeeded(config.CertificateDescription);
+    ///
+    ///    app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+    ///           .WithCertificate(config.CertificateDescription.Certificate)
+    ///           .WithAuthority(new Uri(config.Authority))
+    ///           .Build();
+    /// </code></example>
     /// </summary>
-    internal interface ICertificateLoader
+    public interface ICertificateLoader
     {
         /// <summary>
         /// Load the certificate from the description, if needed.
