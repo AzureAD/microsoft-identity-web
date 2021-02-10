740eb39003b462e950d3d9bd9c84c33c2e2123bc
Jennyf19 <jeferrie@microsoft.com>
2021-02-10 08:33:24 -0800

    make cert loader public

--- src/Microsoft.Identity.Web/CertificateManagement/DefaultCertificateLoader.cs
+++ src/Microsoft.Identity.Web/CertificateManagement/DefaultCertificateLoader.cs
@@ -14,8 +14,20 @@ namespace Microsoft.Identity.Web
 {
     /// <summary>
     /// Certificate Loader.
+    /// Only use when loading a certificate from a daemon application, or an ASP NET app, using MSAL .NET directly.
+    /// For an ASP NET Core app, <b>Microsoft Identity Web will handle the certificate loading</b> for you.
+    /// <example><code>
+    /// IConfidentialClientApplication app;
+    /// ICertificateLoader certificateLoader = new DefaultCertificateLoader();
+    ///     certificateLoader.LoadIfNeeded(config.CertificateDescription);
+    ///
+    ///    app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+    ///           .WithCertificate(config.CertificateDescription.Certificate)
+    ///           .WithAuthority(new Uri(config.Authority))
+    ///           .Build();
+    /// </code></example>
     /// </summary>
-    internal class DefaultCertificateLoader : ICertificateLoader
+    public class DefaultCertificateLoader : ICertificateLoader
     {
         /// <summary>
         /// Load the certificate from the description, if needed.
@@ -23,6 +35,11 @@ namespace Microsoft.Identity.Web
         /// <param name="certificateDescription">Description of the certificate.</param>
         public void LoadIfNeeded(CertificateDescription certificateDescription)
         {
+            if (certificateDescription == null)
+            {
+                throw new ArgumentNullException(nameof(certificateDescription));
+            }
+
             if (certificateDescription.Certificate == null)
             {
                 switch (certificateDescription.SourceType)
