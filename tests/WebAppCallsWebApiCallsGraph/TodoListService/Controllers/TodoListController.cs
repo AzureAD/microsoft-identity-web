042442ac2c8dff7bc5c07f8c5bb86ba91e682357
jennyf19 <jeferrie@microsoft.com>
2021-03-16 16:02:29 -0700

    add l2 cache to dev app web API (#1072)
    * add l2 cache to dev app web API
    
    * uncomment code

--- tests/WebAppCallsWebApiCallsGraph/TodoListService/Controllers/TodoListController.cs
+++ tests/WebAppCallsWebApiCallsGraph/TodoListService/Controllers/TodoListController.cs
@@ -44,9 +44,10 @@ namespace TodoListService.Controllers
         {
             string owner = User.GetDisplayName();
             // Below is for testing multi-tenants
+             await _tokenAcquisition.GetAccessTokenForUserAsync(new string[] { "user.read" }).ConfigureAwait(false); // for testing OBO
             // string token1 = await _tokenAcquisition.GetAccessTokenForUserAsync(new string[] { "user.read" }, "7f58f645-c190-4ce5-9de4-e2b7acd2a6ab").ConfigureAwait(false);
             // string token2 = await _tokenAcquisition.GetAccessTokenForUserAsync(new string[] { "user.read" }, "3ebb7dbb-24a5-4083-b60c-5a5977aabf3d").ConfigureAwait(false);
-            
+
             await Task.FromResult(0); // fix CS1998 while the lines about the 2 tokens are commented out.
             return TodoStore.Values.Where(x => x.Owner == owner);
         }
--- tests/WebAppCallsWebApiCallsGraph/TodoListService/Startup.cs
+++ tests/WebAppCallsWebApiCallsGraph/TodoListService/Startup.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+//#define UseRedisCache
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Web;
 
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web.TokenCacheProviders.Distributed;
 
 namespace TodoListService
 {
@@ -30,11 +32,35 @@ namespace TodoListService
             // This flag ensures that the ClaimsIdentity claims collection will be built from the claims in the token
             // JwtSecurityTokenHandler.DefaultMapInboundClaims = false;            
 
+#if UseRedisCache
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration.GetConnectionString("Redis");
+                options.InstanceName = "RedisDemos_"; //should be unique to the app
+            });
+            services.Configure<MsalDistributedTokenCacheAdapterOptions>(options =>
+            {
+                options.OnL2CacheFailure = (ex) =>
+                {
+                    if (ex is StackExchange.Redis.RedisConnectionException)
+                    {
+                        // action: try to reconnect or something
+                        return true; //try to do the cache operation again
+                    }
+                    return false;
+                };
+            });
+#endif
+
             // Adds Microsoft Identity platform (AAD v2.0) support to protect this Api
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                     .AddMicrosoftIdentityWebApi(Configuration, "AzureAd")
                         .EnableTokenAcquisitionToCallDownstreamApi()
-                        .AddInMemoryTokenCaches();
+#if UseRedisCache
+                     .AddDistributedTokenCaches();
+#else
+                     .AddInMemoryTokenCaches();
+#endif
 
             services.AddControllers();
 
