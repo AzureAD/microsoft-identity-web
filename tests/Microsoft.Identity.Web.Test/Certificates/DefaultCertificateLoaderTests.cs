cd81fa7674bab0dbfdba93fd8f1a8b5f82d1fdba
jennyf19 <jeferrie@microsoft.com>
2021-06-07 10:57:03 -0700

    decrypt cert fix (#1248)
    * decrypt cert fix
    
    * fix test

--- tests/Microsoft.Identity.Web.Test/Certificates/DefaultCertificateLoaderTests.cs
+++ tests/Microsoft.Identity.Web.Test/Certificates/DefaultCertificateLoaderTests.cs
@@ -90,12 +90,16 @@ namespace Microsoft.Identity.Web.Test.Certificates
                 ReferenceOrValue = referenceOrValue,
             });
 
+            certDescriptions.Add(CertificateDescription.FromCertificate(null));
+
             IEnumerable<X509Certificate2> certificates = DefaultCertificateLoader.LoadAllCertificates(certDescriptions);
 
             Assert.NotNull(certificates);
             Assert.Equal(2, certificates.Count());
+            Assert.Equal(3, certDescriptions.Count);
             Assert.Equal("CN=ACS2ClientCertificate", certificates.First().Issuer);
             Assert.Equal("CN=ACS2ClientCertificate", certificates.Last().Issuer);
+            Assert.Null(certDescriptions.ElementAt(2).Certificate);
         }
 
         private IEnumerable<CertificateDescription> CreateCertificateDescriptions(
