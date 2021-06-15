f057d6461675d78e521c028fc7fe7bce4c5f530b
jennyf19 <jeferrie@microsoft.com>
2021-06-15 11:47:53 -0700

    Jennyf/fix legacy (#1266)
    * add .Clone() for roles bug
    
    * fix legacy options

--- src/Microsoft.Identity.Web/MergedOptions.cs
+++ src/Microsoft.Identity.Web/MergedOptions.cs
@@ -275,7 +275,7 @@ namespace Microsoft.Identity.Web
             }
 
             mergedOptions.IsDefaultPlatformLoggingEnabled = confidentialClientApplicationOptions.IsDefaultPlatformLoggingEnabled;
-            mergedOptions.LegacyCacheCompatibilityEnabled = confidentialClientApplicationOptions.LegacyCacheCompatibilityEnabled;
+            // mergedOptions.LegacyCacheCompatibilityEnabled = confidentialClientApplicationOptions.LegacyCacheCompatibilityEnabled; // must be set through id web options
             mergedOptions.LogLevel = confidentialClientApplicationOptions.LogLevel;
             if (string.IsNullOrEmpty(mergedOptions.RedirectUri) && !string.IsNullOrEmpty(confidentialClientApplicationOptions.RedirectUri))
             {
