4163240552dd165568a0429fdb46fbc40aa6f7a0
Jean-Marc Prieur <jmprieur@microsoft.com>
2021-06-18 17:05:53 +0200

    Logging every minute (#1272)
    More Logging in the callback:
    - display the  URL to call the callback
    - display the expiry of the OBO token

--- tests/WebAppCallsWebApiCallsGraph/TodoListService/Controllers/TodoListController.cs
+++ tests/WebAppCallsWebApiCallsGraph/TodoListService/Controllers/TodoListController.cs
@@ -83,7 +83,7 @@ namespace TodoListService.Controllers
                 HttpClient httpClient = new HttpClient();
                 
                 var message = await httpClient.GetAsync(url);
-            }, null, 1000, 1000 * 60 * 10);
+            }, null, 1000, 1000 * 60 * 1);  // Callback every minute
         }
 
 
