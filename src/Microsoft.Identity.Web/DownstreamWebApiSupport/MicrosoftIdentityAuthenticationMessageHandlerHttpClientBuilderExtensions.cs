721643bc5614498490e8166c12a0d81da4b20337
Guillaume Rouchon <qetza@users.noreply.github.com>
2021-07-23 16:11:25 +0200

    Add message handlers for downstream api auth (#1276)
    * add message handlers for downstream api auth
    
    * Fix issue if auth header already present
    
    * Refactor downstream options
    
    * Add unit tests

---
+++ src/Microsoft.Identity.Web/DownstreamWebApiSupport/MicrosoftIdentityAuthenticationMessageHandlerHttpClientBuilderExtensions.cs
@@ -0,0 +1,135 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Identity.Web
+{
+    /// <summary>
+    /// Extension for IHttpClientBuilder for startup initialization of Microsoft Identity authentication handlers.
+    /// </summary>
+    public static class MicrosoftIdentityAuthenticationMessageHandlerHttpClientBuilderExtensions
+    {
+        /// <summary>
+        /// Adds a named Microsoft Identity user authentication message handler related to a specific configuration section.
+        /// </summary>
+        /// <param name="builder">Builder.</param>
+        /// <param name="serviceName">Name of the configuration for the service.</param>
+        /// <param name="configuration">Configuration.</param>
+        /// <returns>The builder for chaining.</returns>
+        public static IHttpClientBuilder AddMicrosoftIdentityUserAuthenticationHandler(
+            this IHttpClientBuilder builder,
+            string serviceName,
+            IConfiguration configuration)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.Configure<MicrosoftIdentityAuthenticationMessageHandlerOptions>(serviceName, configuration);
+            builder.AddHttpMessageHandler(services =>
+            {
+                return new MicrosoftIdentityUserAuthenticationMessageHandler(
+                    services.GetRequiredService<ITokenAcquisition>(),
+                    services.GetRequiredService<IOptionsMonitor<MicrosoftIdentityAuthenticationMessageHandlerOptions>>(),
+                    services.GetRequiredService<IOptionsMonitor<MicrosoftIdentityOptions>>(),
+                    serviceName);
+            });
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds a named Microsoft Identity user authentication message handler initialized with delegates.
+        /// </summary>
+        /// <param name="builder">Builder.</param>
+        /// <param name="serviceName">Name of the configuration for the service.</param>
+        /// <param name="configureOptions">Action to configure the options.</param>
+        /// <returns>The builder for chaining.</returns>
+        public static IHttpClientBuilder AddMicrosoftIdentityUserAuthenticationHandler(
+            this IHttpClientBuilder builder,
+            string serviceName,
+            Action<MicrosoftIdentityAuthenticationMessageHandlerOptions> configureOptions)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.Configure(serviceName, configureOptions);
+            builder.AddHttpMessageHandler(services =>
+            {
+                return new MicrosoftIdentityUserAuthenticationMessageHandler(
+                    services.GetRequiredService<ITokenAcquisition>(),
+                    services.GetRequiredService<IOptionsMonitor<MicrosoftIdentityAuthenticationMessageHandlerOptions>>(),
+                    services.GetRequiredService<IOptionsMonitor<MicrosoftIdentityOptions>>(),
+                    serviceName);
+            });
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds a named Microsoft Identity application authentication message handler related to a specific configuration section.
+        /// </summary>
+        /// <param name="builder">Builder.</param>
+        /// <param name="serviceName">Name of the configuration for the service.</param>
+        /// <param name="configuration">Configuration.</param>
+        /// <returns>The builder for chaining.</returns>
+        public static IHttpClientBuilder AddMicrosoftIdentityAppAuthenticationHandler(
+            this IHttpClientBuilder builder,
+            string serviceName,
+            IConfiguration configuration)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.Configure<MicrosoftIdentityAuthenticationMessageHandlerOptions>(serviceName, configuration);
+            builder.AddHttpMessageHandler(services =>
+            {
+                return new MicrosoftIdentityAppAuthenticationMessageHandler(
+                    services.GetRequiredService<ITokenAcquisition>(),
+                    services.GetRequiredService<IOptionsMonitor<MicrosoftIdentityAuthenticationMessageHandlerOptions>>(),
+                    serviceName);
+            });
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds a named Microsoft Identity application authentication message handler initialized with delegates.
+        /// </summary>
+        /// <param name="builder">Builder.</param>
+        /// <param name="serviceName">Name of the configuration for the service.</param>
+        /// <param name="configureOptions">Action to configure the options.</param>
+        /// <returns>The builder for chaining.</returns>
+        public static IHttpClientBuilder AddMicrosoftIdentityAppAuthenticationHandler(
+            this IHttpClientBuilder builder,
+            string serviceName,
+            Action<MicrosoftIdentityAuthenticationMessageHandlerOptions> configureOptions)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.Configure(serviceName, configureOptions);
+            builder.AddHttpMessageHandler(services =>
+            {
+                return new MicrosoftIdentityAppAuthenticationMessageHandler(
+                    services.GetRequiredService<ITokenAcquisition>(),
+                    services.GetRequiredService<IOptionsMonitor<MicrosoftIdentityAuthenticationMessageHandlerOptions>>(),
+                    serviceName);
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ tests/Microsoft.Identity.Web.Test/HttpClientBuilderExtensionsTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Web.Test.Common;
+
+using Xunit;
+
+namespace Microsoft.Identity.Web.Test
+{
+    public class HttpClientBuilderExtensionsTests
+    {
+        private const string HttpClientName = "test-client";
+        private const string ServiceName = "test-service";
+
+        private readonly IConfigurationSection _configSection;
+
+        public HttpClientBuilderExtensionsTests()
+        {
+            _configSection = GetConfigSection(ServiceName);
+        }
+
+        private IConfigurationSection GetConfigSection(string key)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddInMemoryCollection(
+                new Dictionary<string, string>()
+                {
+                    { $"{key}:Scopes", TestConstants.Scopes },
+                    { $"{key}:Tenant", TestConstants.TenantIdAsGuid },
+                    { $"{key}:UserFlow", TestConstants.B2CSignUpSignInUserFlow },
+                    { $"{key}:IsProofOfPossessionRequest", "false" },
+                    { $"{key}:AuthenticationScheme", JwtBearerDefaults.AuthenticationScheme },
+                });
+
+            return builder.Build().GetSection(key);
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void AddMicrosoftIdentityAuthenticationHandler_WithConfiguration(bool useApp)
+        {
+            // arrange
+            var services = new ServiceCollection();
+
+            // act
+            if (useApp)
+            {
+                services.AddHttpClient(HttpClientName)
+                    .AddMicrosoftIdentityAppAuthenticationHandler(ServiceName, _configSection);
+            }
+            else
+            {
+                services.AddHttpClient(HttpClientName)
+                    .AddMicrosoftIdentityUserAuthenticationHandler(ServiceName, _configSection);
+            }
+
+            // assert
+            Assert.Contains(services, s => s.ServiceType == typeof(IConfigureOptions<MicrosoftIdentityAuthenticationMessageHandlerOptions>));
+
+            var provider = services.BuildServiceProvider();
+            var options = provider.GetRequiredService<IOptionsSnapshot<MicrosoftIdentityAuthenticationMessageHandlerOptions>>();
+
+            Assert.Equal(TestConstants.Scopes, options.Get(ServiceName).Scopes);
+            Assert.Equal(TestConstants.TenantIdAsGuid, options.Get(ServiceName).Tenant);
+            Assert.Equal(TestConstants.B2CSignUpSignInUserFlow, options.Get(ServiceName).UserFlow);
+            Assert.False(options.Get(ServiceName).IsProofOfPossessionRequest);
+            Assert.Equal(JwtBearerDefaults.AuthenticationScheme, options.Get(ServiceName).AuthenticationScheme);
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void AddMicrosoftIdentityAuthenticationHandler_WithOptions(bool useApp)
+        {
+            // arrange
+            var services = new ServiceCollection();
+            Action<MicrosoftIdentityAuthenticationMessageHandlerOptions> configureOptions = options =>
+            {
+                options.Scopes = TestConstants.GraphScopes;
+                options.Tenant = TestConstants.TenantIdAsGuid;
+                options.UserFlow = TestConstants.B2CResetPasswordUserFlow;
+                options.IsProofOfPossessionRequest = true;
+                options.AuthenticationScheme = JwtBearerDefaults.AuthenticationScheme;
+            };
+
+            // act
+            if (useApp)
+            {
+                services.AddHttpClient(HttpClientName)
+                    .AddMicrosoftIdentityAppAuthenticationHandler(ServiceName, configureOptions);
+            }
+            else
+            {
+                services.AddHttpClient(HttpClientName)
+                    .AddMicrosoftIdentityUserAuthenticationHandler(ServiceName, configureOptions);
+            }
+
+            // assert
+            Assert.Contains(services, s => s.ServiceType == typeof(IConfigureOptions<MicrosoftIdentityAuthenticationMessageHandlerOptions>));
+
+            var provider = services.BuildServiceProvider();
+            var options = provider.GetRequiredService<IOptionsSnapshot<MicrosoftIdentityAuthenticationMessageHandlerOptions>>();
+
+            Assert.Equal(TestConstants.GraphScopes, options.Get(ServiceName).Scopes);
+            Assert.Equal(TestConstants.TenantIdAsGuid, options.Get(ServiceName).Tenant);
+            Assert.Equal(TestConstants.B2CResetPasswordUserFlow, options.Get(ServiceName).UserFlow);
+            Assert.True(options.Get(ServiceName).IsProofOfPossessionRequest);
+            Assert.Equal(JwtBearerDefaults.AuthenticationScheme, options.Get(ServiceName).AuthenticationScheme);
+        }
+    }
+}
