9698a1cac711986f53336f20067956e961f194b8
Jennyf19 <jeferrie@microsoft.com>
2021-10-16 18:33:28 -0700

    update tests

--- tests/Microsoft.Identity.Web.IssuerValidator.Test/IssuerConfigurationRetrieverTests.cs
+++ tests/Microsoft.Identity.Web.IssuerValidator.Test/IssuerConfigurationRetrieverTests.cs
@@ -21,13 +21,27 @@ namespace Microsoft.Identity.Web.IssuerValidator.Test
             string expectedErrorMessage = IssuerValidatorErrorMessage.IssuerMetadataUrlIsRequired + " (Parameter 'address')";
 
             var exception = await Assert.ThrowsAsync<ArgumentNullException>("address", () => configurationRetriever.GetConfigurationAsync(null, null, CancellationToken.None)).ConfigureAwait(false);
-            Assert.Equal(expectedErrorMessage, exception.Message);
 
-            exception = await Assert.ThrowsAsync<ArgumentNullException>("address", () => configurationRetriever.GetConfigurationAsync(string.Empty, null, CancellationToken.None)).ConfigureAwait(false);
+#if DOTNET_462 || DOTNET_472
+            string netFrameworkErrorMessage = "IDW10301: Azure AD Issuer metadata address URL is required. \r\nParameter name: address";
+            Assert.Equal(netFrameworkErrorMessage, exception.Message);
+#else
             Assert.Equal(expectedErrorMessage, exception.Message);
+#endif
+            exception = await Assert.ThrowsAsync<ArgumentNullException>("address", () => configurationRetriever.GetConfigurationAsync(string.Empty, null, CancellationToken.None)).ConfigureAwait(false);
+#if DOTNET_462 || DOTNET_472
+            Assert.Equal(netFrameworkErrorMessage, exception.Message);
+#else
+            Assert.Equal(expectedErrorMessage, exception.Message);
+#endif
 
             exception = await Assert.ThrowsAsync<ArgumentNullException>("retriever", () => configurationRetriever.GetConfigurationAsync("address", null, CancellationToken.None)).ConfigureAwait(false);
+#if DOTNET_462 || DOTNET_472
+            netFrameworkErrorMessage = "IDW10302: No metadata document retriever is provided. \r\nParameter name: retriever";
+            Assert.Equal(netFrameworkErrorMessage, exception.Message);
+#else
             Assert.Equal(IssuerValidatorErrorMessage.NoMetadataDocumentRetrieverProvided + " (Parameter 'retriever')", exception.Message);
+#endif
         }
 
         [Fact]
