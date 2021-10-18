742c0da70a6203ca671dfb921e5cfa89401b1726
Jennyf19 <jeferrie@microsoft.com>
2021-10-17 18:49:49 -0700

    add auth builder for token acqusition

--- src/Microsoft.Identity.Web/WebApiExtensions/MicrosoftIdentityWebApiAuthenticationBuilder.cs
+++ src/Microsoft.Identity.Web/WebApiExtensions/MicrosoftIdentityWebApiAuthenticationBuilder.cs
@@ -100,8 +100,7 @@ namespace Microsoft.Identity.Web
                 {
                     MergedOptions mergedOptions = mergedOptionsMonitor.Get(jwtBearerAuthenticationScheme);
 
-                    MergedOptions.UpdateMergedOptionsFromConfidentialClientApplicationOptions(ccaOptions.Value, mergedOptions); // legacy scenario w/out auth scheme
-                    MergedOptions.UpdateMergedOptionsFromConfidentialClientApplicationOptions(ccaOptionsMonitor.Get(jwtBearerAuthenticationScheme), mergedOptions); // w/auth scheme
+                    MergedOptions.UpdateMergedOptionsFromConfidentialClientApplicationOptions(ccaOptions.Value, mergedOptions);
 
                     options.Events ??= new JwtBearerEvents();
 
---
+++ src/Microsoft.Identity.Web/WebApiExtensions/WebApiBuildersInternal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
+
+namespace Microsoft.Identity.Web.Internal
+{
+    public class WebApiBuildersInternal
+    {
+        public static MicrosoftIdentityAppCallsWebApiAuthenticationBuilder EnableTokenAcquisitionWithMise(
+            Action<ConfidentialClientApplicationOptions> configureConfidentialClientApplicationOptions,
+            string authenticationScheme,
+            IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddOptions<ConfidentialClientApplicationOptions>(authenticationScheme)
+                            .Configure<IOptionsMonitor<MergedOptions>>((
+                               ccaOptions, mergedOptionsMonitor) =>
+                            {
+                                configureConfidentialClientApplicationOptions(ccaOptions);
+                                MergedOptions mergedOptions = mergedOptionsMonitor.Get(authenticationScheme);
+                                MergedOptions.UpdateMergedOptionsFromConfidentialClientApplicationOptions(ccaOptions, mergedOptions);
+                            });
+
+            services.AddTokenAcquisition();
+
+            return new MicrosoftIdentityAppCallsWebApiAuthenticationBuilder(
+                services,
+                config as IConfigurationSection);
+        }
+    }
+}
