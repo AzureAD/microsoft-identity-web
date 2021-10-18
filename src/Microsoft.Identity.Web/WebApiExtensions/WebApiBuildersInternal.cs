efbadeef71fcb2318ad25b03ed69bb450e4ac165
Jennyf19 <jeferrie@microsoft.com>
2021-10-18 08:57:53 -0700

    update XML

--- src/Microsoft.Identity.Web/WebApiExtensions/WebApiBuildersInternal.cs
+++ src/Microsoft.Identity.Web/WebApiExtensions/WebApiBuildersInternal.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -6,13 +9,26 @@ using Microsoft.Identity.Client;
 
 namespace Microsoft.Identity.Web.Internal
 {
+    /// <summary>
+    /// Web API authentication builder.
+    /// </summary>
     public class WebApiBuildersInternal
     {
+        /// <summary>
+        /// Enables token acquisition with Microsoft.Identity.Service.Essentials (MISE).
+        /// Developers should continue to use `EnableTokenAcquisitionToCallDownstreamApi`.
+        /// This API is not considered part of the public API and may change.
+        /// </summary>
+        /// <param name="configureConfidentialClientApplicationOptions">The action to configure <see cref="ConfidentialClientApplicationOptions"/>.</param>
+        /// <param name="authenticationScheme">Authentication scheme.</param>
+        /// <param name="services">The services being configured.</param>
+        /// <param name="configuration">Configuration.</param>
+        /// <returns>The authentication builder to chain.</returns>
         public static MicrosoftIdentityAppCallsWebApiAuthenticationBuilder EnableTokenAcquisitionWithMise(
             Action<ConfidentialClientApplicationOptions> configureConfidentialClientApplicationOptions,
             string authenticationScheme,
             IServiceCollection services,
-            IConfiguration config)
+            IConfiguration configuration)
         {
             services.AddOptions<ConfidentialClientApplicationOptions>(authenticationScheme)
                             .Configure<IOptionsMonitor<MergedOptions>>((
@@ -27,7 +43,7 @@ namespace Microsoft.Identity.Web.Internal
 
             return new MicrosoftIdentityAppCallsWebApiAuthenticationBuilder(
                 services,
-                config as IConfigurationSection);
+                configuration as IConfigurationSection);
         }
     }
 }
