9a2029a7aa95c2483cb4e2984cb69edd173ed406
Jean-Marc Prieur <jmprieur@microsoft.com>
2022-01-01 18:22:41 -0800

    Adding a unit test

---
+++ tests/Microsoft.Identity.Web.Test/ClientAssertionTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Identity.Web.Test
+{
+    public class ClientAssertionTests
+    {
+        [Fact]
+        public async Task TestClientAssertion()
+        {
+            int n = 0;
+            ClientAssertionDescription clientAssertionDescription = new ClientAssertionDescription(
+                cancellationToken =>
+                {
+                    n++;
+                    return Task.FromResult(new ClientAssertion(
+                        n.ToString(CultureInfo.InvariantCulture),
+                        DateTime.Now + TimeSpan.FromSeconds(1)));
+                });
+
+            string assertion = await clientAssertionDescription.GetSignedAssertion(CancellationToken.None).ConfigureAwait(false);
+
+            Assert.Equal("1", assertion);
+            assertion = await clientAssertionDescription.GetSignedAssertion(CancellationToken.None).ConfigureAwait(false);
+            Assert.Equal("1", assertion);
+
+            await Task.Delay(1000).ConfigureAwait(false);
+            assertion = await clientAssertionDescription.GetSignedAssertion(CancellationToken.None).ConfigureAwait(false);
+            Assert.Equal("2", assertion);
+        }
+    }
+}
