9613eb51b852415a771a2b339f372fcda6b0e1b8
Matt Kotsenas <matt.kotsenas@gmail.com>
2022-03-29 10:00:53 -0700

    Move common configuration to private Core method

--- src/Microsoft.Identity.Web/DownstreamWebApiSupport/MicrosoftIdentityAuthenticationMessageHandlerHttpClientBuilderExtensions.cs
+++ src/Microsoft.Identity.Web/DownstreamWebApiSupport/MicrosoftIdentityAuthenticationMessageHandlerHttpClientBuilderExtensions.cs
@@ -2,9 +2,10 @@
 // Licensed under the MIT License.
 
 using System;
-
+using System.Net.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.Identity.Web
 {
@@ -31,13 +32,7 @@ namespace Microsoft.Identity.Web
             }
 
             builder.Services.Configure<MicrosoftIdentityAuthenticationMessageHandlerOptions>(serviceName, configuration);
-            builder.Services.AddSingleton<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory, DefaultMicrosoftIdentityAuthenticationDelegatingHandlerFactory>();
-            builder.AddHttpMessageHandler(services =>
-            {
-                return services
-                    .GetRequiredService<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory>()
-                    .CreateUserHandler(serviceName);
-            });
+            builder.AddMicrosoftIdentityAuthenticationHandlerCore(factory => factory.CreateUserHandler(serviceName));
 
             return builder;
         }
@@ -60,13 +55,7 @@ namespace Microsoft.Identity.Web
             }
 
             builder.Services.Configure(serviceName, configureOptions);
-            builder.Services.AddSingleton<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory, DefaultMicrosoftIdentityAuthenticationDelegatingHandlerFactory>();
-            builder.AddHttpMessageHandler(services =>
-            {
-                return services
-                    .GetRequiredService<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory>()
-                    .CreateUserHandler(serviceName);
-            });
+            builder.AddMicrosoftIdentityAuthenticationHandlerCore(factory => factory.CreateUserHandler(serviceName));
 
             return builder;
         }
@@ -89,13 +78,7 @@ namespace Microsoft.Identity.Web
             }
 
             builder.Services.Configure<MicrosoftIdentityAuthenticationMessageHandlerOptions>(serviceName, configuration);
-            builder.Services.AddSingleton<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory, DefaultMicrosoftIdentityAuthenticationDelegatingHandlerFactory>();
-            builder.AddHttpMessageHandler(services =>
-            {
-                return services
-                    .GetRequiredService<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory>()
-                    .CreateAppHandler(serviceName);
-            });
+            builder.AddMicrosoftIdentityAuthenticationHandlerCore(factory => factory.CreateAppHandler(serviceName));
 
             return builder;
         }
@@ -118,15 +101,32 @@ namespace Microsoft.Identity.Web
             }
 
             builder.Services.Configure(serviceName, configureOptions);
-            builder.Services.AddSingleton<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory, DefaultMicrosoftIdentityAuthenticationDelegatingHandlerFactory>();
-            builder.AddHttpMessageHandler(services =>
-            {
-                return services
-                    .GetRequiredService<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory>()
-                    .CreateAppHandler(serviceName);
-            });
+            builder.AddMicrosoftIdentityAuthenticationHandlerCore(factory => factory.CreateAppHandler(serviceName));
 
             return builder;
+        }
+
+        /// <summary>
+        /// Adds the common configuration for message handlers.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IHttpClientBuilder"/> to configure.
+        /// </param>
+        /// <param name="configureHandler">
+        /// A Func that takes the <see cref="IMicrosoftIdentityAuthenticationDelegatingHandlerFactory"/> and returns
+        /// the <see cref="DelegatingHandler"/>. This func allows us to reuse the logic to add message handlers,
+        /// while allowing the caller to decide if it needs an app handler or a user handler.
+        /// </param>
+        private static void AddMicrosoftIdentityAuthenticationHandlerCore(
+            this IHttpClientBuilder builder,
+            Func<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory, DelegatingHandler> configureHandler)
+        {
+            builder.Services.TryAddSingleton<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory, DefaultMicrosoftIdentityAuthenticationDelegatingHandlerFactory>();
+            builder.AddHttpMessageHandler(services =>
+            {
+                var factory = services.GetRequiredService<IMicrosoftIdentityAuthenticationDelegatingHandlerFactory>();
+                return configureHandler(factory);
+            });
         }
     }
 }
