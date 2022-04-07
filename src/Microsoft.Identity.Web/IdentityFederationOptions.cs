c6496ed0e787157fffbe613a79ad901d3cc71148
jennyf19 <jeferrie@microsoft.com>
2022-04-06 19:02:48 -0700

    update xml in identityFederationOptions class

--- src/Microsoft.Identity.Web/IdentityFederationOptions.cs
+++ src/Microsoft.Identity.Web/IdentityFederationOptions.cs
@@ -1,10 +1,11 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 namespace Microsoft.Identity.Web
 {
     /// <summary>
     /// Options for configuring Identity Federation options.
+    /// See https://aka.ms/ms-id-web/identity-federation.
     /// </summary>
     public class IdentityFederationOptions
     {
@@ -19,6 +20,8 @@ namespace Microsoft.Identity.Web
         /// and Azure Active Directory. If Azure AD is the issuer, this value should be the object
         /// ID of the managed identity service principal in the tenant that will be used to 
         /// impersonate the app.
+        /// Can be null if you are using the machine assigned managed identity.
+        /// Needs to be assigned if you are using a user assigned managed identity.
         /// </summary>
         public string? SubjectIdentifier { get; set; }
     }
