7af0936d29afda9430f255287be7e259dd2b7882
Jean-Marc Prieur <jmprieur@microsoft.com>
2022-04-16 11:06:37 -0700

    Update

---
+++ src/Microsoft.Identity.Web.TokenAcquisition.Abstractions/ITokenAcquirerFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Microsoft.Identity.Web
+{
+    public interface ITokenAcquirerFactory
+    {
+        ITokenAcquirer GetTokenAcquirer(string authority, string region, string clientId, X509Certificate2 certificate);
+        ITokenAcquirer GetTokenAcquirer(string authenticationScheme);
+    }
+}
