649dab7f3e466146a53fd7134a407fc897586835
Jean-Marc Prieur <jmprieur@microsoft.com>
2022-04-25 08:53:59 -0700

    Fixing warnings

--- src/Microsoft.Identity.Web.TokenAcquisition.Abstractions/TokenAcquirerFactory.cs
+++ src/Microsoft.Identity.Web.TokenAcquisition.Abstractions/TokenAcquirerFactory.cs
@@ -19,11 +19,26 @@ namespace Microsoft.Identity.Web
 
         IDictionary<string, ITokenAcquirer> tokenAcquirers = new Dictionary<string, ITokenAcquirer>();
 
+        /// <summary>
+        /// Get a token acquirer for  a given authority, region, clientId, certificate?
+        /// </summary>
+        /// <param name="authority"></param>
+        /// <param name="region"></param>
+        /// <param name="clientId"></param>
+        /// <param name="certificate"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public ITokenAcquirer GetTokenAcquirer(string authority, string region, string clientId, X509Certificate2 certificate)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Get a token described by a specific authentication scheme/configuration
+        /// </summary>
+        /// <param name="authenticationScheme"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public ITokenAcquirer GetTokenAcquirer(string authenticationScheme)
         {
             throw new NotImplementedException();
