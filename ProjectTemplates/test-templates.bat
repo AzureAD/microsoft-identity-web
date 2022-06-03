34cfae879481354ca104ea7875f50e46be01cf7f
jennyf19 <jeferrie@microsoft.com>
2022-06-03 10:25:50 -0700

    update testing files to 1.25 (#1748)

--- ProjectTemplates/test-templates.bat
+++ ProjectTemplates/test-templates.bat
@@ -1,6 +1,6 @@
 echo "Ensure ClientSemVer"
 if "%ClientSemVer%" == "" (
-set ClientSemVer=1.24.0
+set ClientSemVer=1.25.0
 )
 Set TemplateNugetPackageName="Microsoft.Identity.Web.ProjectTemplates"
 
