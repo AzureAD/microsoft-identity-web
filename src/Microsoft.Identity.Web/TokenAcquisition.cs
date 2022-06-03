fdc81a924cf6e893102c33a6eebd3b7ebe5dbb41
jennyf19 <jeferrie@microsoft.com>
2022-06-03 10:11:16 -0700

    fix for 1738 (#1743)

--- src/Microsoft.Identity.Web/TokenAcquisition.cs
+++ src/Microsoft.Identity.Web/TokenAcquisition.cs
@@ -852,11 +852,25 @@ namespace Microsoft.Identity.Web
                 if (builder != null)
                 {
                     builder.WithSendX5C(mergedOptions.SendX5C);
+
+                    ClaimsPrincipal? user = GetUserFromHttpContext();
+                    var userTenant = string.Empty;
+                    if (user != null)
+                    {
+                        userTenant = user.GetTenantId();
+                        builder.WithCcsRoutingHint(user.GetObjectId(), userTenant);
+                    }
                     if (!string.IsNullOrEmpty(tenantId))
                     {
                         builder.WithTenantId(tenantId);
                     }
-
+                    else
+                    {
+                        if (!string.IsNullOrEmpty(userTenant))
+                        {
+                            builder.WithTenantId(userTenant);
+                        }
+                    }
                     if (tokenAcquisitionOptions != null)
                     {
                         builder.WithExtraQueryParameters(tokenAcquisitionOptions.ExtraQueryParameters);
@@ -867,13 +881,7 @@ namespace Microsoft.Identity.Web
                         {
                             builder.WithProofOfPossession(tokenAcquisitionOptions.PoPConfiguration);
                         }
-                    }
-
-                    ClaimsPrincipal? user = GetUserFromHttpContext();
-                    if (user != null)
-                    {
-                        builder.WithCcsRoutingHint(user.GetObjectId(), user.GetTenantId());
-                    }
+                    }                    
 
                     return await builder.ExecuteAsync(tokenAcquisitionOptions != null ? tokenAcquisitionOptions.CancellationToken : CancellationToken.None)
                                         .ConfigureAwait(false);
