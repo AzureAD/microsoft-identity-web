cc4902e1dd1011d3bc49f0180ad3b056e7647029
Jean-Marc Prieur <jmprieur@microsoft.com>
2022-06-14 13:51:10 -0700

    Attempt to reconcile ClaimsPrincipal and S2SAuthenticationTicket

---
+++ src/Microsoft.Identity.Web.TokenAcquisition.Abstractions/MicrosoftIdentityAuthenticationTicket.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Linq;
+using System.Security.Claims;
+
+// Just to build this assembly. If we like it this class should really be
+// in Microsoft.IdentityModel.Tokens?
+using SecurityToken = System.String;
+
+namespace Microsoft.Identity.Web
+{
+    class MicrosoftIdentityAuthenticationTicket : ClaimsPrincipal 
+    {
+        // TODO: decide the contructors to have.
+
+        /// <summary>
+        /// Subject identity
+        /// </summary>
+        public MicrosoftIdentityClaimsIdentity? SubjectIdentity
+        {
+            get
+            {
+                return Identities.
+                    OfType<MicrosoftIdentityClaimsIdentity>()
+                    .FirstOrDefault(i => i.Label == "SubjectIdentity");
+            }
+        }
+
+        public MicrosoftIdentityClaimsIdentity? ApplicationIdentity
+        {
+            get
+            {
+                return Identities.
+                    OfType<MicrosoftIdentityClaimsIdentity>()
+                    .FirstOrDefault(i => i.Label == "ApplicationIdentity") ;
+            }
+        }
+
+        public string? AccessTokenType { get; private set; }
+        public string? AuthenticationScheme { get; private set; }
+        public string? AuthenticationMode { get; private set; }
+
+
+        /// <summary>
+        /// Gets the <see cref="SecurityToken"/> that was used to create to the <see cref="ApplicationIdentity"/>.
+        /// </summary>
+        public SecurityToken? ApplicationToken => ApplicationIdentity?.Token;
+
+        /// <summary>
+        /// Gets othe rawData associated with the ApplicationToken.
+        /// </summary>
+        public string?ApplicationTokenRawData => ApplicationIdentity?.TokenRawData;
+
+
+        /// <summary>
+        /// Gets the <see cref="SecurityToken"/> that was used to create to the <see cref="ApplicationIdentity"/>.
+        /// </summary>
+        public SecurityToken? SubjectToken => SubjectIdentity?.Token;
+
+        /// <summary>
+        /// Gets othe rawData associated with the SubjectToken.
+        /// </summary>
+        public string? SubjectTokenRawData => SubjectIdentity?.TokenRawData;
+
+    }
+}
---
+++ src/Microsoft.Identity.Web.TokenAcquisition.Abstractions/MicrosoftIdentityClaimsIdentity.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Security.Claims;
+
+// Just to build this assembly. If we like it this class should really be
+// in Microsoft.IdentityModel.Tokens?
+using SecurityToken = System.String;
+
+namespace Microsoft.Identity.Web
+{
+    /// <summary>
+    /// Extension of ClaimsIdentity to represent the data like the Token, the raw token
+    /// </summary>
+    class MicrosoftIdentityClaimsIdentity : ClaimsIdentity
+    {
+        // TODO: decide the contructors to have.
+
+
+        /// <summary>
+        /// Token associated with this ClaimsIdentity (if any)
+        /// </summary>
+        public SecurityToken? Token { get; }
+
+        /// <summary>
+        /// Token raw data associated with this claims identity (if any)
+        /// Could also be the BootstrapContext?
+        /// </summary>
+        public string? TokenRawData { get; }
+    }
+}
