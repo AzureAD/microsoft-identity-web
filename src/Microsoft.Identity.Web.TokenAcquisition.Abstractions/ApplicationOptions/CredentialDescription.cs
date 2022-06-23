22f85e946d4d433f271930de1a9b440d564b3bff
Jennyf19 <jeferrie@microsoft.com>
2022-06-23 16:17:45 -0700

    add abstactions class

---
+++ src/Microsoft.Identity.Web.TokenAcquisition.Abstractions/ApplicationOptions/CredentialDescription.cs
@@ -0,0 +1,218 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Microsoft.Identity.Web
+{
+    /// <summary>
+    /// Description of a certificate.
+    /// </summary>
+    public class CredentialDescription
+    {
+        /// <summary>
+        /// Type of the source of the certificate.
+        /// </summary>
+        public CredentialSource SourceType { get; set; }
+
+        /// <summary>
+        /// Container in which to find the certificate.
+        /// <list type="bullet">
+        /// <item>If <see cref="SourceType"/> equals <see cref="CredentialSource.KeyVault"/>, then
+        /// the container is the Key Vault base URL.</item>
+        /// <item>If <see cref="SourceType"/> equals <see cref="CredentialSource.Base64Encoded"/>, then
+        /// this value is not used.</item>
+        /// <item>If <see cref="SourceType"/> equals <see cref="CredentialSource.Path"/>, then
+        /// this value is the path on disk where to find the certificate.</item>
+        /// <item>If <see cref="SourceType"/> equals <see cref="CredentialSource.StoreWithDistinguishedName"/>,
+        /// or <see cref="CredentialSource.StoreWithThumbprint"/>, then
+        /// this value is the path to the certificate in the cert store, for instance <c>CurrentUser/My</c>.</item>
+        /// </list>
+        /// </summary>
+        internal string? Container
+        {
+            get
+            {
+                switch (SourceType)
+                {
+                    case CredentialSource.Certificate:
+                        return null;
+                    case CredentialSource.KeyVault:
+                        return KeyVaultUrl;
+                    case CredentialSource.Base64Encoded:
+                        return null;
+                    case CredentialSource.Path:
+                        return CertificateDiskPath;
+                    case CredentialSource.StoreWithThumbprint:
+                    case CredentialSource.StoreWithDistinguishedName:
+                        return CertificateStorePath;
+                    default:
+                        return null;
+                }
+            }
+            set
+            {
+                switch (SourceType)
+                {
+                    case CredentialSource.Certificate:
+                        break;
+                    case CredentialSource.KeyVault:
+                        KeyVaultUrl = value;
+                        break;
+                    case CredentialSource.Base64Encoded:
+                        break;
+                    case CredentialSource.Path:
+                        CertificateDiskPath = value;
+                        break;
+                    case CredentialSource.StoreWithDistinguishedName:
+                    case CredentialSource.StoreWithThumbprint:
+                        CertificateStorePath = value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// URL of the Key Vault, for instance https://msidentitywebsamples.vault.azure.net.
+        /// </summary>
+        public string? KeyVaultUrl { get; set; }
+
+        /// <summary>
+        /// Certificate store path, for instance "CurrentUser/My".
+        /// </summary>
+        /// <remarks>This property should only be used in conjunction with DistinguishedName or Thumbprint.</remarks>
+        public string? CertificateStorePath { get; set; }
+
+        /// <summary>
+        /// Certificate distinguished name.
+        /// </summary>
+        public string? CertificateDistinguishedName { get; set; }
+
+        /// <summary>
+        /// Name of the certificate in Key Vault.
+        /// </summary>
+        public string? KeyVaultCertificateName { get; set; }
+
+        /// <summary>
+        /// Certificate thumbprint.
+        /// </summary>
+        public string? CertificateThumbprint { get; set; }
+
+        /// <summary>
+        /// Path on disk to the certificate.
+        /// </summary>
+        public string? CertificateDiskPath { get; set; }
+
+        /// <summary>
+        /// Path on disk to the certificate password.
+        /// </summary>
+        public string? CertificatePassword { get; set; }
+
+        /// <summary>
+        /// Base64 encoded certificate value.
+        /// </summary>
+        public string? Base64EncodedValue { get; set; }
+
+        /// <summary>
+        /// Client Secret.
+        /// </summary>
+        public string? ClientSecret { get; set; }
+
+        /// <summary>
+        /// ClientId of the Azure managed identity.
+        /// </summary>
+        public string? ManagedIdentityClientId { get; set; }
+
+        /// <summary>
+        /// Path on disk to the signed assertion (for Kubernetes).
+        /// </summary>
+        public string? SignedAssertionFileDiskPath { get; set; }
+
+        /// <summary>
+        /// Reference to the certificate or value.
+        /// </summary>
+        /// <list type="bullet">
+        /// <item>If <see cref="SourceType"/> equals <see cref="CredentialSource.KeyVault"/>, then
+        /// the reference is the name of the certificate in Key Vault (maybe the version?).</item>
+        /// <item>If <see cref="SourceType"/> equals <see cref="CredentialSource.Base64Encoded"/>, then
+        /// this value is the base 64 encoded certificate itself.</item>
+        /// <item>If <see cref="SourceType"/> equals <see cref="CredentialSource.Path"/>, then
+        /// this value is the password to access the certificate (if needed).</item>
+        /// <item>If <see cref="SourceType"/> equals <see cref="CredentialSource.StoreWithDistinguishedName"/>,
+        /// this value is the distinguished name.</item>
+        /// <item>If <see cref="SourceType"/> equals <see cref="CredentialSource.StoreWithThumbprint"/>,
+        /// this value is the thumbprint.</item>
+        /// </list>
+        internal string? ReferenceOrValue
+        {
+            get
+            {
+                switch (SourceType)
+                {
+                    case CredentialSource.KeyVault:
+                        return KeyVaultCertificateName;
+                    case CredentialSource.Path:
+                        return CertificatePassword;
+                    case CredentialSource.StoreWithThumbprint:
+                        return CertificateThumbprint;
+                    case CredentialSource.StoreWithDistinguishedName:
+                        return CertificateDistinguishedName;
+                    case CredentialSource.Certificate:
+                    case CredentialSource.Base64Encoded:
+                        return Base64EncodedValue;
+                    case CredentialSource.ClientSecret:
+                        return ClientSecret;
+                    case CredentialSource.SignedAssertionFromManagedIdentity:
+                        return ManagedIdentityClientId;
+                    case CredentialSource.SignedAssertionFilePath:
+                        return SignedAssertionFileDiskPath;
+                    default:
+                        return null;
+                }
+            }
+            set
+            {
+                switch (SourceType)
+                {
+                    case CredentialSource.Certificate:
+                        break;
+                    case CredentialSource.KeyVault:
+                        KeyVaultCertificateName = value;
+                        break;
+                    case CredentialSource.Base64Encoded:
+                        Base64EncodedValue = value;
+                        break;
+                    case CredentialSource.Path:
+                        CertificateDiskPath = value;
+                        break;
+                    case CredentialSource.StoreWithThumbprint:
+                        CertificateThumbprint = value;
+                        break;
+                    case CredentialSource.StoreWithDistinguishedName:
+                        CertificateDistinguishedName = value;
+                        break;
+                    case CredentialSource.ClientSecret:
+                        ClientSecret = value;
+                        break;
+                    case CredentialSource.SignedAssertionFromManagedIdentity:
+                        ManagedIdentityClientId = value;
+                        break;
+                    case CredentialSource.SignedAssertionFilePath:
+                        SignedAssertionFileDiskPath = value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The certificate, either provided directly in code
+        /// or loaded from the description.
+        /// </summary>
+        public X509Certificate2? Certificate { get; protected internal set; }
+    }
+}
---
+++ src/Microsoft.Identity.Web.TokenAcquisition.Abstractions/TokenAcquisition/AcquireTokenOptions.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Identity.Web
+{
+    /// <summary>
+    /// Options directing the token acquisition.
+    /// </summary>
+    public class AcquireTokenOptions
+    {
+        /// <summary>
+        /// Enables to override the tenant/account for which to get a token. 
+        /// This is useful in multi-tenant apps in the cases where a given user account is a guest 
+        /// in other tenants, and you want to acquire tokens for a specific tenant.
+        /// </summary>
+        /// <remarks>Can be the tenant ID or domain name.</remarks>
+        public string? Tenant { get; set; }
+
+        /// <summary>
+        /// In the case of AzureAD B2C, uses a particular user flow.
+        /// </summary>
+        public string? UserFlow { get; set; }
+
+        /// <summary>
+        /// Gets the parameters describing the confidential client application (ClientId,
+        /// Region, Authority, client credentials) from a particular 
+        /// (ASP.NET Core) authentication scheme / settings.
+        /// </summary>
+        public string? AuthenticationScheme { get; set; }
+
+        /// <summary>
+        /// Sets the correlation id to be used in the request to the STS "/token" endpoint.
+        /// </summary>
+        public Guid CorrelationId { get; set; }
+
+        /// <summary>
+        /// Sets query parameters for the query string in the HTTP request to the 
+        /// "/token" endpoint.
+        /// </summary>
+        public Dictionary<string, string>? ExtraQueryParameters { get; set; }
+
+        /// Sets extra headers in the HTTP request to the STS "/token" endpoint.
+        public Dictionary<string, string>? ExtraHeadersParameters { get; set; }
+
+        /// <summary>
+        /// A string with one or multiple claims to request. It's a json blob (encoded or not)
+        /// Normally used with Conditional Access. It receives the Claims member of the UiRequiredException.
+        /// </summary>
+        public string? Claims { get; set; }
+
+        /// <summary>
+        /// Specifies if the token request will ignore the access token in the token cache
+        /// and will attempt to acquire a new access token.
+        /// If <c>true</c>, the request will ignore the token cache. The default is <c>false</c>.
+        /// Use this option with care and only when needed, for instance, if you know that conditional access policies have changed,
+        /// for it induces performance degradation, as the token cache is not utilized, and the STS might throttle the app.
+        /// </summary>
+        public bool ForceRefresh { get; set; }
+
+        /// <summary>
+        /// Modifies the token acquisition request so that the acquired token is a Proof of Possession token (PoP),
+        /// rather than a Bearer token.
+        /// PoP tokens are similar to Bearer tokens, but are bound to the HTTP request and to a cryptographic key,
+        /// which MSAL can manage. See https://aka.ms/msal-net-pop.
+        /// </summary>
+        public string? PopPublicKey { get; set; }
+
+        /// <summary>
+        /// Key used for long running web APIs that need to call downstream web
+        /// APIs on behalf of the user. Can be null, if you are not developing a long
+        /// running web API, <see cref="LongRunningWebApiSessionKeyAuto"/> if you want
+        /// Microsoft.Identity.Web to allocate a session key for you, or your own string
+        /// if you want to associate the session with some information you have externally
+        /// (for instance a Microsoft Graph hook identifier).
+        /// </summary>
+        public string? LongRunningWebApiSessionKey { get; set; }
+
+        /// <summary>
+        /// Value that can be used for <see cref="LongRunningWebApiSessionKey"/> so that
+        /// MSAL.NET allocates the long running web api session key for the developer.
+        /// </summary>
+        public static string LongRunningWebApiSessionKeyAuto { get; set; } = "AllocateForMe";
+
+        /// <summary>
+        /// Clone the options (to be able to override them).
+        /// </summary>
+        /// <returns>A clone of the options.</returns>
+        public AcquireTokenOptions Clone()
+        {
+            return new AcquireTokenOptions
+            {
+                Tenant = Tenant,
+                UserFlow = UserFlow,
+                AuthenticationScheme = AuthenticationScheme,
+                CorrelationId = CorrelationId,
+                ExtraQueryParameters = ExtraQueryParameters,
+                ExtraHeadersParameters = ExtraHeadersParameters,
+                ForceRefresh = ForceRefresh,
+                Claims = Claims,
+                PopPublicKey = PopPublicKey,
+                LongRunningWebApiSessionKey = LongRunningWebApiSessionKey,
+            };
+        }
+    }
+}
