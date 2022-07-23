b20c2114597693193e6b5ec446e8518b78e34848
Jennyf19 <jeferrie@microsoft.com>
2022-07-23 16:24:51 -0700

    fix build issues

--- src/Microsoft.Identity.Web.MicrosoftGraph/MicrosoftGraphExtensions.cs
+++ src/Microsoft.Identity.Web.MicrosoftGraph/MicrosoftGraphExtensions.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using Microsoft.Graph;
 
 namespace Microsoft.Identity.Web
