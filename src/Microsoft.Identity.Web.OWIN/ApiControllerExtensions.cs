bbbbf6db8943e852ca67f8652eef8c59e9e70ae4
Jean-Marc Prieur <jmprieur@microsoft.com>
2022-11-06 16:49:44 -0800

    Addressing PR feedback

--- src/Microsoft.Identity.Web.OWIN/ApiControllerExtensions.cs
+++ src/Microsoft.Identity.Web.OWIN/ApiControllerExtensions.cs
@@ -23,7 +23,7 @@ namespace Microsoft.Identity.Web
             GraphServiceClient? graphServiceClient = TokenAcquirerFactory.GetDefaultInstance().ServiceProvider?.GetService(typeof(GraphServiceClient)) as GraphServiceClient;
             if (graphServiceClient == null)
             {
-                throw new ConfigurationErrorsException("Cannot get GraphServiceClient. Did you add services.AddMicrosoftGraph() in Startup_Auth.cs?. See https://aka.ms/ms-id-web/owin");
+                throw new ConfigurationErrorsException("Cannot find GraphServiceClient. Did you add services.AddMicrosoftGraph() in Startup_Auth.cs?. See https://aka.ms/ms-id-web/owin. ");
             }    
             return graphServiceClient;
         }
@@ -37,7 +37,7 @@ namespace Microsoft.Identity.Web
             IAuthorizationHeaderProvider? headerProvider = TokenAcquirerFactory.GetDefaultInstance().ServiceProvider?.GetService(typeof(IAuthorizationHeaderProvider)) as IAuthorizationHeaderProvider;
             if (headerProvider == null)
             {
-                throw new ConfigurationErrorsException("Cannot get GraphServiceClient. Did you create an OwinTokenAcquirerFactory in Startup_Auth.cs?. See https://aka.ms/ms-id-web/owin");
+                throw new ConfigurationErrorsException("Cannot find IAuthorizationHeaderProvider. Did you create an OwinTokenAcquirerFactory in Startup_Auth.cs?. See https://aka.ms/ms-id-web/owin. ");
             }
             return headerProvider;
         }
@@ -51,7 +51,7 @@ namespace Microsoft.Identity.Web
             IDownstreamRestApi? downstreamRestApi = TokenAcquirerFactory.GetDefaultInstance().ServiceProvider?.GetService(typeof(IDownstreamRestApi)) as IDownstreamRestApi;
             if (downstreamRestApi == null)
             {
-                throw new ConfigurationErrorsException("Cannot get IDownstreamRestApi. Did you add services.AddMicrosoftGraph() in Startup_Auth.cs? See https://aka.ms/ms-id-web/owin");
+                throw new ConfigurationErrorsException("Cannot find IDownstreamRestApi. Did you add services.AddDownstreamRestApi() in Startup_Auth.cs? See https://aka.ms/ms-id-web/owin. ");
             }
             return downstreamRestApi;
         }
