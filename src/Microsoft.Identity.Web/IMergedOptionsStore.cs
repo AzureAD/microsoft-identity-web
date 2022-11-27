bbc85e794972e201c1561f9d5ce0944536d2d39f
Jennyf19 <jeferrie@microsoft.com>
2022-11-27 15:34:10 -0800

    add license info

--- src/Microsoft.Identity.Web/IMergedOptionsStore.cs
+++ src/Microsoft.Identity.Web/IMergedOptionsStore.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.Identity.Web
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Identity.Web
 {
     internal interface IMergedOptionsStore
     {
