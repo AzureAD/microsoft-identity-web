c9d875a19c407af1bc000daf19fbaba8b10dd9ea
Jean-Marc Prieur <jmprieur@microsoft.com>
2023-01-02 18:49:04 -0800

    AddDownstreamRestApi on MicrosoftIdentityAppCallsWebApiAuthenticationBuilder

--- src/Microsoft.Identity.Web.DownstreamRestApi/DownstreamRestApiExtensions.cs
+++ src/Microsoft.Identity.Web.DownstreamRestApi/DownstreamRestApiExtensions.cs
@@ -4,6 +4,7 @@
 using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.Identity.Abstractions;
 
 namespace Microsoft.Identity.Web
@@ -55,5 +56,45 @@ namespace Microsoft.Identity.Web
             services.Configure(serviceName, configureOptions);
             return services;
         }
+
+#if NETCOREAPP
+        /// <summary>
+        /// Adds a named downstream web REST service related to a specific configuration section.
+        /// </summary>
+        /// <param name="builder">Builder.</param>
+        /// <param name="serviceName">Name of the configuration for the service.
+        /// This is the name used when calling the service from controller/pages.</param>
+        /// <param name="configuration">Configuration.</param>
+        /// <returns>The builder for chaining.</returns>
+        public static MicrosoftIdentityAppCallsWebApiAuthenticationBuilder AddDownstreamRestApi(
+            this MicrosoftIdentityAppCallsWebApiAuthenticationBuilder builder,
+            string serviceName,
+            IConfiguration configuration)
+        {
+            _ = Throws.IfNull(builder);
+
+            builder.Services.AddDownstreamRestApi(serviceName, configuration);
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds a named downstream REST API service initialized with delegates.
+        /// </summary>
+        /// <param name="builder">Builder.</param>
+        /// <param name="serviceName">Name of the configuration for the service.
+        /// This is the name which will be used when calling the service from controller/pages.</param>
+        /// <param name="configureOptions">Action to configure the options.</param>
+        /// <returns>The builder for chaining.</returns>
+        public static MicrosoftIdentityAppCallsWebApiAuthenticationBuilder AddDownstreamRestApi(
+            this MicrosoftIdentityAppCallsWebApiAuthenticationBuilder builder,
+            string serviceName,
+            Action<DownstreamRestApiOptions> configureOptions)
+        {
+            _ = Throws.IfNull(builder);
+
+            builder.Services.AddDownstreamRestApi(serviceName, configureOptions);
+            return builder;
+        }
+#endif
     }
 }
