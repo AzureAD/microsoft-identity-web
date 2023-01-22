231c202381f05c3f478bc87478adc54e9514658e
Jean-Marc Prieur <jmprieur@microsoft.com>
2023-01-22 15:03:56 -0800

    Adding the AzureAd section by default to TokenAcquirerFactory

--- tests/DevApps/daemon-app/Daemon-app/Program.cs
+++ tests/DevApps/daemon-app/Daemon-app/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// #define UseMicrosoftGraphSdk
+#define UseMicrosoftGraphSdk
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Abstractions;
