ceabb06c0f68d9d144fef2c267fc45064bb87d9f
Jean-Marc Prieur <jmprieur@microsoft.com>
2023-01-24 16:09:07 -0800

    Adding minimal samples (for the doc)

---
+++ tests/DevApps/daemon-app/daemon-console-calling-msgraph/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+
+var tokenAcquirerFactory = TokenAcquirerFactory.GetDefaultInstance();
+tokenAcquirerFactory.Services.AddMicrosoftGraph();
+var sp = tokenAcquirerFactory.Build();
+
+var graphServiceClient = sp.GetRequiredService<GraphServiceClient>();
+var users = await graphServiceClient.Users
+    .Request()
+    .WithAppOnly()
+    .GetAsync();
+Console.WriteLine($"{users.Count} users");
