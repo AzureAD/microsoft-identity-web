f251faa930a55fc6a0c8404320a604673bf19710
Jennyf19 <jeferrie@microsoft.com>
2023-03-16 21:59:25 -0700

    use cert description

--- tests/Microsoft.Identity.Web.Test.LabInfrastructure/KeyVaultSecretsProvider.cs
+++ tests/Microsoft.Identity.Web.Test.LabInfrastructure/KeyVaultSecretsProvider.cs
@@ -2,35 +2,45 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Security.Cryptography.X509Certificates;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using Microsoft.Identity.Abstractions;
 using Microsoft.Identity.Web.Test.Common;
 
 namespace Microsoft.Identity.Web.Test.LabInfrastructure
 {
     public class KeyVaultSecretsProvider
     {
+        public KeyVaultSecretsProvider()
+        {
+            DefaultCertificateLoader defaultcertloader = new();
+            CredentialDescription credentialDescription= new()
+            {
+                SourceType = CredentialSource.StoreWithThumbprint,
+                CertificateStorePath = "CurrentUser/My",
+                CertificateThumbprint = "444B697D869032F29F9A162D711AF3E2791AD748"
+
+            };
+            defaultcertloader.LoadCredentialsIfNeededAsync(credentialDescription).GetAwaiter().GetResult();
+            Certificate = credentialDescription.Certificate;
+        }
+
+        public X509Certificate2 Certificate  { get; set; }
+
         public KeyVaultSecret GetKeyVaultSecret()
         {
             Uri keyVaultUri = new Uri(TestConstants.BuildAutomationKeyVaultName);
-            DefaultAzureCredentialOptions options = new()
-            {
-                ManagedIdentityClientId = TestConstants.LabClientId,
-            };
-            DefaultAzureCredential credential = new(options);
-            SecretClient secretClient = new(keyVaultUri, credential);
+            ClientCertificateCredential clientCertificateCredential = new(TestConstants.ConfidentialClientLabTenant, TestConstants.LabClientId, Certificate);
+            SecretClient secretClient = new(keyVaultUri, clientCertificateCredential);
 
             return secretClient.GetSecret(TestConstants.AzureADIdentityDivisionTestAgentSecret);
         }
 
         public KeyVaultSecret GetMsidLabSecret(string secretName)
-        {
-            DefaultAzureCredentialOptions options = new()
-            {
-                ManagedIdentityClientId = TestConstants.LabClientId,
-            };
-            DefaultAzureCredential credential = new(options);
-            SecretClient secretClient = new(new Uri(TestConstants.MSIDLabLabKeyVaultName), credential);
+        { 
+            ClientCertificateCredential clientCertificateCredential = new(TestConstants.ConfidentialClientLabTenant, TestConstants.LabClientId, Certificate);
+            SecretClient secretClient = new(new Uri(TestConstants.MSIDLabLabKeyVaultName), clientCertificateCredential);
             return secretClient.GetSecret(secretName);
         }
     }
