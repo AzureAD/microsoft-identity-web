b2d183362dac5292e98ba084206640680d4dc78c
Westin Musser <westinmusser@microsoft.com>
2023-04-06 14:29:56 -0700

    create logger instance

--- src/Microsoft.Identity.Web.DownstreamApi/DownstreamApi.cs
+++ src/Microsoft.Identity.Web.DownstreamApi/DownstreamApi.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Identity.Abstractions;
 
@@ -23,6 +24,7 @@ namespace Microsoft.Identity.Web
         private readonly IOptionsMonitor<DownstreamApiOptions> _namedDownstreamApiOptions;
         private const string ScopesNotConfiguredInConfigurationOrViaDelegate = "IDW10107: Scopes need to be passed-in either by configuration or by the delegate overriding it. ";
         private const string Authorization = "Authorization";
+        protected readonly ILogger<DownstreamApi> _logger;
 
         /// <summary>
         /// Constructor.
@@ -30,14 +32,17 @@ namespace Microsoft.Identity.Web
         /// <param name="authorizationHeaderProvider">Authorization header provider.</param>
         /// <param name="namedDownstreamApiOptions">Named options provider.</param>
         /// <param name="httpClientFactory">HTTP client factory.</param>
+        /// <param name="logger">Logger.</param>
         public DownstreamApi(
             IAuthorizationHeaderProvider authorizationHeaderProvider,
             IOptionsMonitor<DownstreamApiOptions> namedDownstreamApiOptions,
-            IHttpClientFactory httpClientFactory)
+            IHttpClientFactory httpClientFactory,
+            ILogger<DownstreamApi> logger)
         {
             _authorizationHeaderProvider = authorizationHeaderProvider;
             _namedDownstreamApiOptions = namedDownstreamApiOptions;
             _httpClientFactory = httpClientFactory;
+            _logger = logger;
         }
 
         /// <inheritdoc/>
