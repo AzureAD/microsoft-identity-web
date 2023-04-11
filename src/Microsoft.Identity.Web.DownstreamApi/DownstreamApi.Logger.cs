75c77978bca38b5709ff1d8b83aa88734783387f
Westin Musser <westinmusser@microsoft.com>
2023-04-10 17:12:04 -0700

    Initial creation of logger class

---
+++ src/Microsoft.Identity.Web.DownstreamApi/DownstreamApi.Logger.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Identity.Web
+{
+    /// <summary>
+    /// LoggerMessage class for DownstreamApi
+    /// </summary>
+    internal partial class DownstreamApi
+    {
+        internal static class Logger
+        {
+            private static readonly Action<ILogger, string, string, Exception?> s_effectiveOptionsError =
+                LoggerMessage.Define<string, string>(
+                    LogLevel.Debug, 
+                    new EventId(400, "EffectiveOptions"), 
+                    "[MsIdWeb] An error occurred during Get: " + 
+                    "BaseUrl: {BaseUrl} " +
+                    "RelativePath: {RelativePath} ");
+
+
+            /// <summary>
+            /// Logger for handling options exceptions in DownstreamApi
+            /// </summary>
+            /// <param name="logger">ILogger</param>
+            /// <param name="BaseUrl">Base url from appsettings.</param>
+            /// <param name="RelativePath">Relative path from appsettings.</param>
+            /// <param name="ex">Exception</param>
+            public static void EffectiveOptionsError(
+                ILogger logger, 
+                string BaseUrl, 
+                string RelativePath, 
+                Exception? ex) => s_effectiveOptionsError(logger, BaseUrl, RelativePath, ex);
+        }
+    }
+}
