221e05d8f8a2c07052401ee45572db6fc5a1c3d2
Westin Musser <westinmusser@microsoft.com>
2023-06-30 14:27:40 -0700

    conditionally target net 8

--- src/Microsoft.Identity.Web.TokenAcquisition/MergedOptions.cs
+++ src/Microsoft.Identity.Web.TokenAcquisition/MergedOptions.cs
@@ -205,7 +205,9 @@ namespace Microsoft.Identity.Web
             mergedOptions.SkipUnrecognizedRequests |= microsoftIdentityOptions.SkipUnrecognizedRequests;
             mergedOptions.StateDataFormat ??= microsoftIdentityOptions.StateDataFormat;
             mergedOptions.StringDataFormat ??= microsoftIdentityOptions.StringDataFormat;
-
+#if NET8_0_OR_GREATER
+            mergedOptions.TimeProvider = microsoftIdentityOptions.TimeProvider;
+#endif
             mergedOptions.TokenValidationParameters = microsoftIdentityOptions.TokenValidationParameters.Clone();
             mergedOptions.UsePkce |= microsoftIdentityOptions.UsePkce;
 
