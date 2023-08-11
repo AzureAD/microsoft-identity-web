09bb055a3a96bc691f5bb236faa24de588d330a3
Jean-Marc Prieur <jmprieur@microsoft.com>
2023-08-11 12:38:40 -0700

    Enable TokenAcquisition to use the IdentityPrincipal.Bootstrapcontext - Adds a PrincipalExtensionsForSecurityTokens.GetBootstrapToken method that extracts   a Security token from the bootstrap context (+unit tests) - In Web APIs, GetAuthenticationResultForUserAsync tries the BootstrapContext first

---
+++ src/Microsoft.Identity.Web.TokenAcquisition/PrincipalExtensionsForSecurityTokens.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Microsoft.Identity.Web
+{
+    /// <summary>
+    /// Extensions to retrive a <see cref="SecurityToken"/> from <see cref="ClaimsPrincipal"/>.
+    /// </summary>
+    public static class PrincipalExtensionsForSecurityTokens
+    {
+        /// <summary>
+        /// Get the <see cref="SecurityToken"/> used to call a protected web API.
+        /// </summary>
+        /// <param name="claimsPrincipal"></param>
+        /// <returns></returns>
+        public static SecurityToken? GetBootstrapToken(this IPrincipal claimsPrincipal)
+        {
+            object? o = (claimsPrincipal?.Identity as ClaimsIdentity)?.BootstrapContext;
+            if (o is SecurityToken securityToken)
+            {
+                return securityToken;
+            }
+            if (o is string s)
+            {
+                return new JsonWebToken(s);
+            }
+            return null;
+        }
+    }
+}
---
+++ tests/Microsoft.Identity.Web.Test/PrincipalExtensionsForSecurityTokensTests.cs
@@ -0,0 +1,73 @@
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using Xunit;
+
+namespace Microsoft.Identity.Web.Tests
+{
+    public class PrincipalExtensionsForSecurityTokensTests
+    {
+        [Fact]
+        public void GetBootstrapToken_Returns_Null_When_ClaimsPrincipal_Is_Null()
+        {
+            // Arrange
+            IPrincipal claimsPrincipal = null;
+
+            // Act
+            var result = claimsPrincipal.GetBootstrapToken();
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetBootstrapToken_Returns_Null_When_BootstrapContext_Is_Null()
+        {
+            // Arrange
+            IPrincipal claimsPrincipal = new GenericPrincipal(new GenericIdentity(""), null);
+
+            // Act
+            var result = claimsPrincipal.GetBootstrapToken();
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetBootstrapToken_Returns_SecurityToken_When_BootstrapContext_Is_SecurityToken()
+        {
+            // Arrange
+            var securityToken = new JwtSecurityToken();
+            IPrincipal claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, "TestUser") })
+            {
+                BootstrapContext = securityToken
+            });
+
+            // Act
+            var result = claimsPrincipal.GetBootstrapToken();
+
+            // Assert
+            Assert.Equal(securityToken, result);
+        }
+
+        [Fact]
+        public void GetBootstrapToken_Returns_JsonWebToken_When_BootstrapContext_Is_String()
+        {
+            // Arrange
+            const string jwtString = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+            IPrincipal claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, "TestUser") })
+            {
+                BootstrapContext = jwtString
+            });
+
+            // Act
+            var result = claimsPrincipal.GetBootstrapToken() as JsonWebToken;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(jwtString, result.EncodedToken);
+        }
+    }
+}
