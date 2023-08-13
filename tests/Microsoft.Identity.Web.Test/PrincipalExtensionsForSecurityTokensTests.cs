3e34c71802059a15b9527ef314b09ba3ea87d0e7
Jean-Marc Prieur <jmprieur@microsoft.com>
2023-08-13 14:10:13 -0700

    Update tests/Microsoft.Identity.Web.Test/PrincipalExtensionsForSecurityTokensTests.cs
    Co-authored-by: jennyf19 <jeferrie@microsoft.com>

--- tests/Microsoft.Identity.Web.Test/PrincipalExtensionsForSecurityTokensTests.cs
+++ tests/Microsoft.Identity.Web.Test/PrincipalExtensionsForSecurityTokensTests.cs
@@ -1,3 +1,6 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
