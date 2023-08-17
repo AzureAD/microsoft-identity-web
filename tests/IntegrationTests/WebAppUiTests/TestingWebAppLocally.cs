65e71015866caffaacd8a865b0444ba59172ec40
JoshLozensky <103777376+JoshLozensky@users.noreply.github.com>
2023-08-17 15:59:47 -0700

    Replace Selenium With Playwright (#2370)
    * Rewrote UI tests to utilize Playwright rather than Selenium.
    
    ---------
    
    Co-authored-by: jennyf19 <jeferrie@microsoft.com>

--- tests/IntegrationTests/WebAppUiTests/TestingWebAppLocally.cs
+++ tests/IntegrationTests/WebAppUiTests/TestingWebAppLocally.cs
@@ -1,65 +1,56 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Xunit;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System;
-using Microsoft.Identity.Lab.Api;
-using Microsoft.Identity.Web.Test.Common;
-using OpenQA.Selenium.Edge;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using Microsoft.Identity.Web.Test.Common.TestHelpers;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.Identity.Lab.Api;
+using Microsoft.Playwright;
+using Xunit;
 
 namespace WebAppUiTests;
 
 #if !FROM_GITHUB_ACTION && !AZURE_DEVOPS_BUILD
-public class TestingWebAppLocally 
+public class TestingWebAppLocally
 {
+    const string UrlString = "https://localhost:5001/MicrosoftIdentity/Account/signin";
+
     [Fact]
-    public async Task ChallengeUser_SignInSucceedsTestAsync_LocalHttp()
+    public async Task ChallengeUser_MicrosoftIdentityFlow_LocalApp_ValidEmailPasswordCreds_SignInSucceedsTestAsync()
     {
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         { return; }
 
-        string testedApplicationPath = "WebAppCallsMicrosoftGraph.exe";
-        Process ?p = ExternalApp.Start(
-            typeof(TestingWebAppLocally),
-            @"DevApps\WebAppCallsMicrosoftGraph",
-            testedApplicationPath);
+        // Arrange
+        Process? p = StartWebAppLocally();
 
         if (p != null)
         {
             if (p.HasExited)
             {
-                Assert.Fail($"Could not run {testedApplicationPath}.");
+                Assert.Fail($"Could not run web app locally.");
             }
 
-            EdgeOptions edgeOptions = new EdgeOptions();
-            edgeOptions.AddArgument("-inprivate");
-            // ~2x faster, no visual rendering
-            // comment-out below when debugging to see the UI automation
-            edgeOptions.AddArgument(TestConstants.Headless);
-            using IWebDriver driver = new EdgeDriver(edgeOptions);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            using var playwright = await Playwright.CreateAsync();
+            IBrowser browser;
+            browser = await playwright.Chromium.LaunchAsync(new() { Headless = true });
+            IPage page = await browser.NewPageAsync();
+            await page.GotoAsync(UrlString);
+            LabResponse labResponse = await LabUserHelper.GetDefaultUserAsync().ConfigureAwait(false);
 
             try
             {
                 // Act
-                Trace.WriteLine("Starting Selenium automation: web app sign-in & call Graph");
-                driver.Navigate()
-                   .GoToUrl($"https://localhost:5001/MicrosoftIdentity/Account/signin");
-                LabResponse labResponse = await LabUserHelper.GetDefaultUserAsync().ConfigureAwait(false);
-                WebAppIntegrationTests.PerformLogin(driver, labResponse.User);
+                Trace.WriteLine("Starting Playwright automation: web app sign-in & call Graph");
+                string email = labResponse.User.Upn;
+                await UiTestHelpers.PerformLogin_MicrosoftIdentityFlow_ValidEmailPasswordCreds(page, email, labResponse.User.GetOrFetchPassword());
 
                 // Assert
-                Assert.Contains(labResponse.User.Upn, driver.PageSource, System.StringComparison.OrdinalIgnoreCase);
-                Assert.Contains(TestConstants.PhotoLabel, driver.PageSource, System.StringComparison.OrdinalIgnoreCase);
-                driver.Quit();
-                driver.Dispose();
+                await Assertions.Expect(page.GetByText("Welcome")).ToBeVisibleAsync();
+                await Assertions.Expect(page.GetByText(email)).ToBeVisibleAsync();
             }
             catch (Exception ex)
             {
@@ -70,6 +61,26 @@ public class TestingWebAppLocally
                 p.Kill(true);
             }
         }
+    }
+
+    private Process? StartWebAppLocally()
+    {
+        string uiTestAssemblyLocation = typeof(TestingWebAppLocally).Assembly.Location;
+        // e.g. microsoft-identity-web\tests\IntegrationTests\WebAppUiTests\bin\Debug\net6.0\WebAppUiTests.dll
+        string testedAppLocation = Path.Combine(Path.GetDirectoryName(uiTestAssemblyLocation)!);
+        // e.g. microsoft-identity-web\tests\IntegrationTests\WebAppUiTests\bin\Debug\net6.0
+        string[] segments = testedAppLocation.Split(Path.DirectorySeparatorChar);
+        int numberSegments = segments.Length;
+        int startLastSegments = numberSegments - 3;
+        int endFirstSegments = startLastSegments - 2;
+        string testedApplicationPath = Path.Combine(
+            Path.Combine(segments.Take(endFirstSegments).ToArray()),
+            @"DevApps\WebAppCallsMicrosoftGraph",
+            Path.Combine(segments.Skip(startLastSegments).ToArray()),
+            "WebAppCallsMicrosoftGraph.exe");
+
+        ProcessStartInfo processStartInfo = new ProcessStartInfo(testedApplicationPath);
+        return Process.Start(processStartInfo);
     }
 }
 #endif //FROM_GITHUB_ACTION
--- tests/IntegrationTests/WebAppUiTests/WebAppIntegrationTests.cs
+++ tests/IntegrationTests/WebAppUiTests/WebAppIntegrationTests.cs
@@ -3,124 +3,50 @@
 
 using System;
 using System.Diagnostics;
-using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.Identity.Lab.Api;
-using Microsoft.Identity.Web.Test.Common;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
+using Microsoft.Playwright;
 using Xunit;
 
 namespace WebAppUiTests
 {
 #if !FROM_GITHUB_ACTION
+
     public class WebAppIntegrationTests
     {
+        const string UrlString = "https://webapptestmsidweb.azurewebsites.net/MicrosoftIdentity/Account/signin";
+
         [Fact(Skip = "We cannot republish the web app atm. https://github.com/AzureAD/microsoft-identity-web/issues/984")]
-        public async Task ChallengeUser_SignInSucceedsTestAsync()
+        public async Task ChallengeUser_MicrosoftIdentityFlow_RemoteApp_ValidEmailPasswordCreds_SignInSucceedsTestAsync()
         {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) { return; } 
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) { return; }
 
             // Arrange
+            using var playwright = await Playwright.CreateAsync();
+
+            IBrowser browser = await playwright.Chromium.LaunchAsync(new() { Headless = true });
+            IPage page = await browser.NewPageAsync();
+            await page.GotoAsync(UrlString);
             LabResponse labResponse = await LabUserHelper.GetDefaultUserAsync().ConfigureAwait(false);
 
-            ChromeOptions options = new ChromeOptions();
-            // ~2x faster, no visual rendering
-            // comment-out below when debugging to see the UI automation
-            options.AddArguments(TestConstants.Headless);
-            using IWebDriver driver = new ChromeDriver(options);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
-
-            // Act
-            Trace.WriteLine("Starting Selenium automation: web app sign-in & call Graph");
-            driver.Navigate()
-               .GoToUrl("https://webapptestmsidweb.azurewebsites.net/MicrosoftIdentity/Account/signin");
-            PerformLogin(driver, labResponse.User);
-
-            // Assert
-            Assert.Contains(labResponse.User.Upn, driver.PageSource, System.StringComparison.OrdinalIgnoreCase);
-            Assert.Contains(TestConstants.PhotoLabel, driver.PageSource, System.StringComparison.OrdinalIgnoreCase);
-            driver.Quit();
-            driver.Dispose();
-        }
-
-        internal static void PerformLogin(
-            IWebDriver driver,
-            LabUser user)
-        {
-            UserInformationFieldIds fields = new UserInformationFieldIds(user);
-
-            EnterUsername(driver, user, fields);
-            EnterPassword(driver, user, fields);
-            HandleStaySignedInPrompt(driver);
-        }
-
-        internal static void EnterUsername(
-            IWebDriver driver,
-            LabUser user,
-            UserInformationFieldIds fields)
-        {
-            // Lab user needs to be a guest in the msidentity-samples-testing tenant
-            Trace.WriteLine(string.Format(CultureInfo.InvariantCulture, "Logging in ... Entering user name: {0}", user.Upn));
-
-            driver.FindElement(By.Id(fields.AADUsernameInputId)).SendKeys(user.Upn.Contains("EXT", System.StringComparison.OrdinalIgnoreCase) ? user.HomeUPN : user.Upn);
-
-            Trace.WriteLine("Logging in ... Clicking <Next> after user name");
-
-            driver.FindElement(By.Id(fields.AADSignInButtonId)).Click();
-        }
-
-        internal static void EnterPassword(
-            IWebDriver driver,
-            LabUser user,
-            UserInformationFieldIds fields)
-        {
-            Trace.WriteLine("Logging in ... Entering password");
-            string password = user.GetOrFetchPassword();
-            WaitUntilWithRetry(driver, fields.GetPasswordInputId())
-                .SendKeys(password);
-
-            // Wait a bit for the dialog to be refreshed before attempting
-            // to get the button (who is already there, inactive, but will be stale
-            // when the dialog refreshes.
-            Task.Delay(1000).Wait();
-
-            Trace.WriteLine("Logging in ... Clicking next after password");
-            WaitUntilWithRetry(driver, fields.GetPasswordSignInButtonId())
-                .Click();
-
-        }
-
-        internal static void HandleStaySignedInPrompt(IWebDriver driver)
-        {
-            Trace.WriteLine("Logging in ... Clicking 'No' for staying signed in");
-            WaitUntilWithRetry(driver, TestConstants.StaySignedInNoId)
-                .Click();
-        }
-
-        private static IWebElement WaitUntilWithRetry(IWebDriver driver, string elementName)
-        {
-            WebDriverWait wait = new WebDriverWait(driver, timeout: TimeSpan.FromSeconds(10))
-            {
-                PollingInterval = TimeSpan.FromSeconds(0.200),
-            };
-            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
-
-            IWebElement element;
             try
             {
-                element = wait.Until(drv => drv.FindElement(By.Id(elementName)));
+                // Act
+                Trace.WriteLine("Starting Playwright automation: web app sign-in & call Graph");
+                string email = labResponse.User.Upn;
+                await UiTestHelpers.PerformLogin_MicrosoftIdentityFlow_ValidEmailPasswordCreds(page, email, labResponse.User.GetOrFetchPassword());
+
+                // Assert
+                await Assertions.Expect(page.GetByText("Welcome")).ToBeVisibleAsync();
+                await Assertions.Expect(page.GetByText(email)).ToBeVisibleAsync();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                element = wait.Until(drv => drv.FindElement(By.Id(elementName)));
+                Assert.Fail($"The UI automation failed: {ex}");
             }
 
-            return element;
         }
-
-    }
 #endif //FROM_GITHUB_ACTION
+    }
 }
