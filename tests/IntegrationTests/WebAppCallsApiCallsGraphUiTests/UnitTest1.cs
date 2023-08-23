cc2d08d606df07566deee1fc7695779730a8ef4f
Josh Lozensky <joshlozensky@microsoft.com>
2023-08-22 18:43:50 -0700

    Created project

---
+++ tests/IntegrationTests/WebAppCallsApiCallsGraphUiTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace WebAppCallsApiCallsGraphUiTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
