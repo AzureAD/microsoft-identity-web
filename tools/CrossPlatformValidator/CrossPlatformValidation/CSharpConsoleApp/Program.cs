ff0dc1f810d1c701b861b269b082a412ec74f2b7
Jenny Ferries <jeferrie@microsoft.com>
2023-09-14 19:34:17 -0700

    Initial commit

---
+++ tools/CrossPlatformValidator/CrossPlatformValidation/CSharpConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using CrossPlatformValidation;
+
+Console.WriteLine("Hello, World!");
+
+RequestValidator requestValidator = new();
+
+requestValidator.Initialize("https://login.microsoftonline.com/7f58f645-c190-4ce5-9de4-e2b7acd2a6ab", "a4c2469b-cf84-4145-8f5f-cb7bacf814bc");
+
+string token = "Bearer ";
+IDictionary<string, object> claims = requestValidator.Validate(token);
+foreach (var claim in claims)
+{
+    Console.WriteLine(claim);
+}
+Console.ReadLine();
---
+++ tools/CrossPlatformValidator/CrossPlatformValidation/CrossPlatformValidation/RequestValidator.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Validators;
+
+namespace CrossPlatformValidation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RequestValidator
+    {
+        ConfigurationManager<OpenIdConnectConfiguration> configurationManager;
+        TokenValidationParameters tokenValidationParameters;
+        JsonWebTokenHandler jsonWebTokenHandler = new();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="authority"></param>
+        /// <param name="audience"></param>
+        public void Initialize(string authority, string audience)
+        {
+            configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(authority.TrimEnd('/') + "/v2.0/.well-known/openid-configuration", new OpenIdConnectConfigurationRetriever());
+            tokenValidationParameters = new TokenValidationParameters();
+            tokenValidationParameters.ConfigurationManager = configurationManager;
+            tokenValidationParameters.ValidAudience = audience;
+            tokenValidationParameters.IssuerValidator = AadIssuerValidator.GetAadIssuerValidator(authority).Validate;
+            tokenValidationParameters.EnableAadSigningKeyIssuerValidation();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="authorizationHeader"></param>
+        /// <returns></returns>
+        public IDictionary<string, object> Validate(string authorizationHeader)
+        {
+            var token = authorizationHeader.Replace("Bearer ", string.Empty, StringComparison.OrdinalIgnoreCase);
+            var result = jsonWebTokenHandler.ValidateTokenAsync(token, tokenValidationParameters).GetAwaiter().GetResult();
+            return result.Claims;
+        }
+    }
+}
