eb1b5c8ae57a5d9b8ad14614a5fb35234f4eed2c
Jean-Marc Prieur <jmprieur@microsoft.com>
2023-09-25 09:05:47 -0700

    Jmprieur/cross plat validator (#2474)
    * Adding a test for Wilson 6 on .NET 7
    * Adding a benchmark

--- tools/CrossPlatformValidator/CrossPlatformValidation/publish.bat
+++ tools/CrossPlatformValidator/CrossPlatformValidation/publish.bat
@@ -1,1 +1,6 @@
  dotnet publish -r win-x64 -c release -f net8.0
+ copy  .\CrossPlatformValidation\bin\Release\net8.0\win-x64\native\* CrossPlatformValidatorTests\bin\Debug\net8.0\
+ copy  .\CrossPlatformValidation\bin\Release\net8.0\win-x64\native\* Benchmark\bin\Release\net8.0\
+
+ dotnet publish -r win-x64 -c release -f net7.0
+ copy  .\CrossPlatformValidation\bin\Release\net7.0\win-x64\native\* CrossPlatformValidatorTests\bin\Debug\net7.0\
