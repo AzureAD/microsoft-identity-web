a291b1a4f0156e780a962676c614b4b1a9029f91
Josh Lozensky <joshlozensky@microsoft.com>
2023-10-09 16:03:43 -0700

    Cleaned up E2E test now that it is functioning

--- tests/IntegrationTests/WebAppCallsApiCallsGraphUiTests/TestingFlowLocally.cs
+++ tests/IntegrationTests/WebAppCallsApiCallsGraphUiTests/TestingFlowLocally.cs
@@ -2,9 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -31,7 +29,7 @@ namespace WebAppCallsApiCallsGraphUiTests
         private const string GrpcPath = @"\gRPC";
         private const string TodoListServiceExecutable = @"\TodoListService.exe";
         private const string TodoListClientExecutable = @"\TodoListClient.exe";
-        private const string GrpcExecutable = "grpc.exe";
+        private const string GrpcExecutable = @"\grpc.exe";
         private const string SignOutPagePath = @"/MicrosoftIdentity/Account/SignedOut";
         private const string TodoTitle1 = "Testing create todo item";
         private const string TodoTitle2 = "Testing edit todo item";
@@ -47,28 +45,16 @@ namespace WebAppCallsApiCallsGraphUiTests
         [Fact]
         public async Task ChallengeUser_MicrosoftIdentityFlow_LocalApp_ValidEmailPasswordCreds_TodoAppFunctionsCorrectly()
         {
-            // Arrange process setup, grpc left commented out for now
-            //Process? grpcProcess = TestingFlowLocally.StartWebAppLocally(UiTestAssemblyLocation, DevAppPath + GrpcPath, GrpcExecutable);
+            // Arrange web app setup
             Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
-            //Environment.SetEnvironmentVariable("Kestrel:Endpoints:Https:Url", "https://*:44321");
-            Process? clientProcess = TestingFlowLocally.StartWebAppLocally(UiTestAssemblyLocation, DevAppPath + TodoListClientPath, TodoListClientExecutable, false, "44321");
-            //Environment.SetEnvironmentVariable("Kestrel:Endpoints:Https:Url", "https://*:44351");
-            Process? serviceProcess = TestingFlowLocally.StartWebAppLocally(UiTestAssemblyLocation, DevAppPath + TodoListServicePath, TodoListServiceExecutable, false, "44351");
+            Process? grpcProcess = TestingFlowLocally.StartWebAppLocally(UiTestAssemblyLocation, DevAppPath + GrpcPath, GrpcExecutable, "5001");
+            Process? clientProcess = TestingFlowLocally.StartWebAppLocally(UiTestAssemblyLocation, DevAppPath + TodoListClientPath, TodoListClientExecutable, "44321");
+            Process? serviceProcess = TestingFlowLocally.StartWebAppLocally(UiTestAssemblyLocation, DevAppPath + TodoListServicePath, TodoListServiceExecutable, "44351");
             try
             {
-                StringDictionary clientProcessVars = clientProcess.StartInfo.EnvironmentVariables;
-                StringDictionary serviceProcessVars = serviceProcess.StartInfo.EnvironmentVariables;
-                foreach (DictionaryEntry variable in clientProcessVars)
+                if (clientProcess != null && serviceProcess != null && grpcProcess != null)
                 {
-                    _output.WriteLine($"Client: {variable.Key} = {variable.Value}");
-                }
-                foreach (DictionaryEntry variable in clientProcessVars)
-                {
-                    _output.WriteLine($"Service: {variable.Key} = {variable.Value}");
-                }
-                if (clientProcess != null && serviceProcess != null /*&& grpcProcess != null*/)
-                {
-                    if (clientProcess.HasExited || serviceProcess.HasExited)//* || grpcProcess.HasExited*//*)
+                    if (clientProcess.HasExited || serviceProcess.HasExited || grpcProcess.HasExited)
                     {
                         Assert.Fail($"Could not run web app locally.");
                     }
@@ -76,7 +62,7 @@ namespace WebAppCallsApiCallsGraphUiTests
                     // Arrange Playwright setup
                     using var playwright = await Playwright.CreateAsync();
                     IBrowser browser;
-                    browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+                    browser = await playwright.Chromium.LaunchAsync(new() { Headless = true }); // To see the browser UI, set Headless = false
                     IPage page = await browser.NewPageAsync();
                     await page.GotoAsync(UrlString);
                     LabResponse labResponse = await LabUserHelper.GetDefaultUserAsync().ConfigureAwait(false);
@@ -98,7 +84,6 @@ namespace WebAppCallsApiCallsGraphUiTests
                     // Sign in again using Todo List button
                     _output.WriteLine("Starting web app sign-in flow using Todo List button after sign out");
                     await page.GetByRole(AriaRole.Link, new() { Name = "TodoList" }).ClickAsync();
-                    // await page.GetByRole(AriaRole.Link, new() { Name = "Sign In", Exact = true}).ClickAsync();
                     await UiTestHelpers.SuccessiveLogin_MicrosoftIdentityFlow_ValidEmailPassword(page, email, labResponse.User.GetOrFetchPassword(), _output);
                     var TodoLink =  page.GetByRole(AriaRole.Link, new() { Name = "Create New" });
                     await Assertions.Expect(TodoLink).ToBeVisibleAsync();
@@ -140,12 +125,27 @@ namespace WebAppCallsApiCallsGraphUiTests
                 Queue<Process> processes = new Queue<Process>();
                 processes.Enqueue(serviceProcess);
                 processes.Enqueue(clientProcess);
-                //processes.Enqueue(grpcProcess);
+                processes.Enqueue(grpcProcess);
                 killProcessTrees(processes);
             }
         }
         
-        public static Process? StartWebAppLocally(string testAssemblyLocation, string appLocation, string executableName, bool elevated = false, string pathNumber=null)
+        public static Process? StartWebAppLocally(string testAssemblyLocation, string appLocation, string executableName, string pathNumber=null)
+        {
+            string applicationWorkingDirectory = GetApplicationWorkingDirectory(testAssemblyLocation, appLocation);
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(applicationWorkingDirectory + executableName);
+            processStartInfo.WorkingDirectory = applicationWorkingDirectory;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+
+            if (!pathNumber.IsNullOrEmpty())
+            {
+                processStartInfo.EnvironmentVariables["Kestrel:Endpoints:Https:Url"] = "https://*:" + pathNumber;
+            }
+            return Process.Start(processStartInfo);
+        }
+     
+        private static string GetApplicationWorkingDirectory(string testAssemblyLocation, string appLocation)
         {
             string testedAppLocation = Path.Combine(Path.GetDirectoryName(testAssemblyLocation)!);
             // e.g. microsoft-identity-web\tests\IntegrationTests\WebAppUiTests\bin\Debug\net6.0
@@ -153,36 +153,12 @@ namespace WebAppCallsApiCallsGraphUiTests
             int numberSegments = segments.Length;
             int startLastSegments = numberSegments - 3;
             int endFirstSegments = startLastSegments - 2;
-            string applicationWorkingDirectory = Path.Combine(
+            return Path.Combine(
                 Path.Combine(segments.Take(endFirstSegments).ToArray()),
                 appLocation,
                 Path.Combine(segments.Skip(startLastSegments).ToArray())
-                
             );
-            ProcessStartInfo processStartInfo = new ProcessStartInfo(applicationWorkingDirectory + executableName);
-            processStartInfo.WorkingDirectory = applicationWorkingDirectory;
-            if (elevated)
-            {
-                processStartInfo.UseShellExecute = true;
-                processStartInfo.Verb = "runas";
-                
-            } else
-            {
-                processStartInfo.RedirectStandardOutput = true;
-                processStartInfo.RedirectStandardError = true;
-                processStartInfo.CreateNoWindow = false;
-                // processStartInfo.EnvironmentVariables["ASPNETCORE_ENVIRONMENT"] = "Development";
-                // processStartInfo.AddEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
-
-
-                if (!pathNumber.IsNullOrEmpty())
-                {
-                    processStartInfo.EnvironmentVariables["Kestrel:Endpoints:Https:Url"] = "https://*:" + pathNumber;
-                }
-            }
-            return Process.Start(processStartInfo);
         }
-     
         private void killProcessTrees(Queue<Process> processQueue)
         {
             Process currentProcess;
@@ -204,14 +180,14 @@ namespace WebAppCallsApiCallsGraphUiTests
     {
         public static IList<Process> GetChildProcesses(this Process process)
         {
+            // Validate platform compatibility
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
- // Validate platform compatibility
                 return new ManagementObjectSearcher(
                     $"Select * From Win32_Process Where ParentProcessID={process.Id}")
                     .Get()
                     .Cast<ManagementObject>()
-#pragma warning disable CA1416 // This call can not be reached except on Windows
+#pragma warning disable CA1416 // This call can not be reached except on Windows due to the enclosing if statement
                     .Select(mo =>
                         Process.GetProcessById(Convert.ToInt32(mo["ProcessID"], System.Globalization.CultureInfo.InvariantCulture)))
 #pragma warning restore CA1416
