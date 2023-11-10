495970e2b2084b46b76b8b02a58c8706a9e629c4
jennyf19 <jeferrie@microsoft.com>
2023-11-09 18:15:35 -0800

    Jennyf/cross plat validator (#2491)
    * Initial commit
    
    * add test and publish.bat
    
    * improve test
    
    * add todo
    
    * Jmprieur/cross plat validator (#2474)
    
    * Adding a test for Wilson 6 on .NET 7
    * Adding a benchmark
    
    * Jmprieur/cross plat validator (#2490)
    
    * Adding a test for Wilson 6 on .NET 7
    
    * Adding a benchmark
    
    * Adding a benchmark
    
    * Removing the token acquisition from the loop
    Updating the script to copy all platforms
    Adding a Readme.md
    
    * Adding a benchmark for pure .NET
    
    * Improving the benchmarks
    
    ---------
    
    Co-authored-by: Jenny Ferries <jeferrie@microsoft.com>
    
    ---------
    
    Co-authored-by: Jean-Marc Prieur <jmprieur@microsoft.com>

---
+++ tools/CrossPlatformValidator/CrossPlatformValidation/Benchmark/Program.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Lab.Api;
+
+namespace Brenchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ValidateBenchmark>();
+        }
+    }
+
+    [Config(typeof(AntiVirusFriendlyConfig))]
+    [MemoryDiagnoser]
+    public class ValidateBenchmark
+    {
+        private static IPublicClientApplication msalPublicClient;
+        private static LabResponse labResponse;
+        private static string authorizationHeader;
+
+        static ValidateBenchmark()
+        {
+            Initialize("https://login.microsoftonline.com/organizations", "f4aa5217-e87c-42b2-82af-5624dd14ee72");
+            labResponse = LabUserHelper.GetSpecificUserAsync(OBOUser).GetAwaiter().GetResult();
+            msalPublicClient = PublicClientApplicationBuilder
+               .Create(OBOClientSideClientId)
+               .WithAuthority(labResponse.Lab.Authority, Organizations)
+               .Build();
+            authorizationHeader = AcquireTokenForLabUserAsync().Result.CreateAuthorizationHeader();
+        }
+
+        public const string Organizations = "organizations";
+        public const string OBOUser = "fIDLAB@msidlab4.com";
+        public const string OBOClientSideClientId = "c0485386-1e9a-4663-bc96-7ab30656de7f";
+        public static string[] s_oBOApiScope = new string[] { "api://f4aa5217-e87c-42b2-82af-5624dd14ee72/.default" };
+        public int numberValidations = 1000000;
+
+        [DllImport("CrossPlatformValidation.dll")]
+        static extern void Initialize(string authority, string audience);
+
+        [DllImport("CrossPlatformValidation.dll")]
+        static extern string Validate(string authorizationHeader);
+
+        [Benchmark]
+        public void ValidateAuthRequestNativeBenchmark()
+        {
+            if (Validate(authorizationHeader) == null)
+        {
+            string authorizationHeader = ValidateBenchmark.AcquireTokenForLabUserAsync().Result.CreateAuthorizationHeader();
+                if (Validate(authorizationHeader) == null)
+                {
+                    throw new ArgumentException("Validation failed");
+                }
+            }
+        }
+
+        private static async Task<AuthenticationResult> AcquireTokenForLabUserAsync()
+        {
+            AuthenticationResult authResult;
+            try
+            {
+                var accounts = await msalPublicClient.GetAccountsAsync()
+                    .ConfigureAwait(false);
+                authResult = await msalPublicClient.AcquireTokenSilent(s_oBOApiScope, accounts.FirstOrDefault())
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException)
+            {
+                authResult = await msalPublicClient
+                    .AcquireTokenByUsernamePassword(
+                    s_oBOApiScope,
+                    OBOUser,
+                    labResponse.User.GetOrFetchPassword())
+                    .ExecuteAsync(CancellationToken.None)
+                    .ConfigureAwait(false);
+            }
+            return authResult;
+        }
+    }
+
+    public class AntiVirusFriendlyConfig : ManualConfig
+    {
+        public AntiVirusFriendlyConfig()
+        {
+            AddJob(Job.MediumRun
+                .WithToolchain(InProcessNoEmitToolchain.Instance));
+        }
+    }
+}
---
+++ tools/CrossPlatformValidator/CrossPlatformValidation/CSharpConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using CrossPlatformValidation;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Lab.Api;
+
+Console.WriteLine("Hello, World!");
+
+RequestValidator requestValidator = new();
+
+requestValidator.Initialize("https://login.microsoftonline.com/organizations", "f4aa5217-e87c-42b2-82af-5624dd14ee72");
+string authorizationHeader = AcquireTokenForLabUserAsync().Result.CreateAuthorizationHeader();
+var result = requestValidator.Validate(authorizationHeader);
+//string token = "Bearer ";
+//IDictionary<string, object> claims = requestValidator.Validate(token);
+//foreach (var claim in claims)
+//{
+//    Console.WriteLine(claim);
+//}
+Console.ReadLine();
+
+
+static async Task<AuthenticationResult> AcquireTokenForLabUserAsync()
+{
+ string Organizations = "organizations";
+ string OBOUser = "fIDLAB@msidlab4.com";
+ string OBOClientSideClientId = "c0485386-1e9a-4663-bc96-7ab30656de7f";
+ string[] s_oBOApiScope = new string[] { "api://f4aa5217-e87c-42b2-82af-5624dd14ee72/.default" };
+
+var labResponse = await LabUserHelper.GetSpecificUserAsync(OBOUser).ConfigureAwait(false);
+    var msalPublicClient = PublicClientApplicationBuilder
+       .Create(OBOClientSideClientId)
+       .WithAuthority(labResponse.Lab.Authority, Organizations)
+       .Build();
+
+    AuthenticationResult authResult = await msalPublicClient
+        .AcquireTokenByUsernamePassword(
+        s_oBOApiScope,
+        OBOUser,
+        labResponse.User.GetOrFetchPassword())
+        .ExecuteAsync(CancellationToken.None)
+        .ConfigureAwait(false);
+
+    return authResult;
+}
