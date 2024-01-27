737f7ddf567280ddb0238d8de09e0a34441d7777
Jenny Ferries <jeferrie@microsoft.com>
2024-01-27 14:48:25 -0800

    update benchmarks and add bencmark-action

---
+++ benchmark/Program.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using Benchmarks;
+
+namespace daemon_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // StepthroughValidateBenchmarks();
+#if DEBUG
+            var benchmarkConfig = ManualConfig.Union(DefaultConfig.Instance, new DebugInProcessConfig()); // Allows debugging into benchmarks
+#else
+            var benchmarkConfig = ManualConfig.Union(DefaultConfig.Instance, new BenchmarkConfig());
+#endif
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, benchmarkConfig);
+        }
+    }
+
+    //public class AntiVirusFriendlyConfig : ManualConfig
+    //{
+    //    public AntiVirusFriendlyConfig()
+    //    {
+    //        AddColumn(StatisticColumn.P90);
+    //        AddColumn(StatisticColumn.P95);
+    //        AddColumn(StatisticColumn.P100);
+
+    //        AddJob(Job.MediumRun
+    //            .WithToolchain(InProcessNoEmitToolchain.Instance));
+    //    }
+}
