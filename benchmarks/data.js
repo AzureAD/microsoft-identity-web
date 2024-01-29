6e92453846ca3e19c6038cf44b7ce0e6cfaf0832
github-action-benchmark <github@users.noreply.github.com>
2024-01-29 17:21:01 +0000

    add TokenAcquisitionBenchmarks (benchmarkdotnet) benchmark result for 0bf282509331eae265548748a418a2111513a93d

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706494210484,
+  "lastUpdate": 1706548861687,
   "repoUrl": "https://github.com/AzureAD/microsoft-identity-web",
   "entries": {
     "TokenAcquisitionBenchmarks": [
@@ -72,6 +72,42 @@ window.BENCHMARK_DATA = {
             "value": 10838.031398840118,
             "unit": "ns",
             "range": "± 54.20439933169983"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jeferrie@microsoft.com",
+            "name": "jennyf19",
+            "username": "jennyf19"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0bf282509331eae265548748a418a2111513a93d",
+          "message": "trying evergreen dependabot updates (#2661)\n\n* trying evergreen dependabot updates\r\n\r\n* Update .github/workflows/evergreen.yml",
+          "timestamp": "2024-01-29T09:17:17-08:00",
+          "tree_id": "23ba737556ce29b6ea076ee0e00931e3b33dcd9f",
+          "url": "https://github.com/AzureAD/microsoft-identity-web/commit/0bf282509331eae265548748a418a2111513a93d"
+        },
+        "date": 1706548861016,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.TokenAcquisitionBenchmark.CreateAuthorizationHeader",
+            "value": 10463.693697947376,
+            "unit": "ns",
+            "range": "± 105.87248855017806"
+          },
+          {
+            "name": "Benchmarks.TokenAcquisitionBenchmark.GetTokenAcquirer",
+            "value": 11166.72570405183,
+            "unit": "ns",
+            "range": "± 52.59453756785643"
           }
         ]
       }
