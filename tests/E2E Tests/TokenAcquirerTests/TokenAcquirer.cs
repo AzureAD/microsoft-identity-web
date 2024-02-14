9cfad65e4b01b40157cdd789cd537415d38f0fd5
Josh Lozensky <joshlozensky@microsoft.com>
2024-02-13 18:56:42 -0800

    adding reset before default instance call

--- tests/E2E Tests/TokenAcquirerTests/TokenAcquirer.cs
+++ tests/E2E Tests/TokenAcquirerTests/TokenAcquirer.cs
@@ -21,7 +21,7 @@ using TaskStatus = System.Threading.Tasks.TaskStatus;
 namespace TokenAcquirerTests
 {
 #if !FROM_GITHUB_ACTION
-    [Collection("TokenAcquirerSerialized")]
+    [CollectionDefinition("TokenAcquirerSerialized")]
     public class TokenAcquirer
     {
         private static readonly string s_optionName = string.Empty;
@@ -303,7 +303,7 @@ namespace TokenAcquirerTests
         }
     }
 
-    [Collection("TokenAcquirerSerialized")]
+    [CollectionDefinition("TokenAcquirerSerialized")]
     public class AcquireTokenManagedIdentity 
     {
         [OnlyOnAzureDevopsFact]
@@ -314,6 +314,7 @@ namespace TokenAcquirerTests
             const string scope = "https://vault.azure.net/.default";
             const string baseUrl = "https://vault.azure.net";
             const string clientId = "9c5896db-a74a-4b1a-a259-74c5080a3a6a";
+            TokenAcquirerFactory.ResetDefaultInstance();
             TokenAcquirerFactory tokenAcquirerFactory = TokenAcquirerFactory.GetDefaultInstance();
             IServiceProvider serviceProvider = tokenAcquirerFactory.Build();
 
