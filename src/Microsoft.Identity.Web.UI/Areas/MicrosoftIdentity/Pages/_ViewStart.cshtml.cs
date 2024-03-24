1b226837ba864ce0330a2834d88bdbd1ec16df16
Jenny Ferries <jeferrie@microsoft.com>
2024-03-23 18:43:02 -0700

    update M.IdentityWeb.UI

---
+++ src/Microsoft.Identity.Web.UI/Areas/MicrosoftIdentity/Pages/_ViewStart.cshtml.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Microsoft.Identity.Web.UI.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
