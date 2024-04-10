542f7d1155db554a0bac04b457d31d29d778fcd6
trwalke <trwalke@microsoft.com>
2024-04-10 16:02:42 -0700

    hiding UI

--- tests/E2E Tests/WebAppUiTests/WebAppCallsApiCallsGraphLocally.cs
+++ tests/E2E Tests/WebAppUiTests/WebAppCallsApiCallsGraphLocally.cs
@@ -187,7 +187,7 @@ namespace WebAppUiTests
             // Arrange Playwright setup, to see the browser UI set Headless = false.
             const string TraceFileName = TraceFileClassName + "_CiamWebApp_WebApiFunctionsCorrectly";
             using IPlaywright playwright = await Playwright.CreateAsync();
-            IBrowser browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+            IBrowser browser = await playwright.Chromium.LaunchAsync(new() { Headless = true });
             IBrowserContext context = await browser.NewContextAsync(new BrowserNewContextOptions { IgnoreHTTPSErrors = true });
             await context.Tracing.StartAsync(new() { Screenshots = true, Snapshots = true, Sources = true });
             IPage page = null;
