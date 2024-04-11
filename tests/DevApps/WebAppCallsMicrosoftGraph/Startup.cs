b7622cde84c7f476088cc9e8a3e9545f73e68a6c
trwalke <trwalke@microsoft.com>
2024-04-10 23:28:25 -0700

    Adding asp auth

--- tests/DevApps/WebAppCallsMicrosoftGraph/Startup.cs
+++ tests/DevApps/WebAppCallsMicrosoftGraph/Startup.cs
@@ -3,6 +3,9 @@
 
 // #define USE_SIGNED_ASSERTION
 using System;
+#if NET8_0
+using Microsoft.AspNetCore.Authentication.Certificate;
+#endif
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
@@ -37,6 +40,10 @@ namespace WebAppCallsMicrosoftGraph
 #endif
 
             int count = 0;
+#if NET8_0
+            services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
+                    .AddCertificate();
+#endif
             services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
                     .AddMicrosoftIdentityWebApp(options =>
                     {
