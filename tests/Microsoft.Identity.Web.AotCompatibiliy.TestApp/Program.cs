2fa38eb14e93c2edeabc7e83ac5cd2393778a575
Saurabh Gautam <saugautam@microsoft.com>
2024-05-16 16:16:29 +0100

    Add script to perform AOT check

---
+++ tests/Microsoft.Identity.Web.AotCompatibiliy.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+
+internal sealed class Program
+{
+    // The code in this program is expected to be trim and AOT compatible
+    private static int Main()
+    {
+        return 100;
+    }
+}
