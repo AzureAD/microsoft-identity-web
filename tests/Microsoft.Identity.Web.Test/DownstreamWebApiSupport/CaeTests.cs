015a3e05223dcd0a9555bf29bd905e26cbc20633
sruthikeerthi <73967733+sruke@users.noreply.github.com>
2024-06-24 10:38:48 -0700

    Implements GetAuthorizationHeaderAsync to support 1P specific scenarios (#2896)
    * Implements IAuthorizationHeaderProviderExtension
    
    * Use DownstreamApiOptions.RequestAppToken to check if header is for app or user
    
    * Updates Abstractions version
    
    * Updates CAE tests
    
    ---------
    
    Co-authored-by: Sruthi Keerthi Rangavajhula (from Dev Box) <srangavajhul@microsoft.com>

--- tests/Microsoft.Identity.Web.Test/DownstreamWebApiSupport/CaeTests.cs
+++ tests/Microsoft.Identity.Web.Test/DownstreamWebApiSupport/CaeTests.cs
@@ -45,9 +45,9 @@ namespace Microsoft.Identity.Web.Test.DownstreamWebApiSupport
 
             var appVal = await _downstreamApi.GetForAppAsync<EmptyClass>("GraphApp");
 
-            await _authorizationHeaderProvider.ReceivedWithAnyArgs(2).CreateAuthorizationHeaderForAppAsync(string.Empty);
-            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderForAppAsync(
-                Arg.Any<string>(), Arg.Is<DownstreamApiOptions>(o => o.AcquireTokenOptions.Claims!.Equals(ParsedClaims, StringComparison.Ordinal)));
+            await _authorizationHeaderProvider.ReceivedWithAnyArgs(2).CreateAuthorizationHeaderAsync(Enumerable.Empty<string>());
+            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderAsync(
+                Arg.Any<IEnumerable<string>>(), Arg.Is<DownstreamApiOptions>(o => o.AcquireTokenOptions.Claims!.Equals(ParsedClaims, StringComparison.Ordinal)));
         }
 
         [Fact]
@@ -61,8 +61,8 @@ namespace Microsoft.Identity.Web.Test.DownstreamWebApiSupport
 
             var userVal = await _downstreamApi.GetForUserAsync<EmptyClass>("GraphUser");
 
-            await _authorizationHeaderProvider.ReceivedWithAnyArgs(2).CreateAuthorizationHeaderForUserAsync(Enumerable.Empty<string>());
-            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderForUserAsync(
+            await _authorizationHeaderProvider.ReceivedWithAnyArgs(2).CreateAuthorizationHeaderAsync(Enumerable.Empty<string>());
+            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderAsync(
                 Arg.Any<IEnumerable<string>>(), Arg.Is<DownstreamApiOptions>(o => o.AcquireTokenOptions.Claims!.Equals(ParsedClaims, StringComparison.Ordinal)));
         }
 
@@ -77,9 +77,9 @@ namespace Microsoft.Identity.Web.Test.DownstreamWebApiSupport
 
             var appVal = await _downstreamApi.GetForAppAsync<EmptyClass>("GraphApp");
 
-            await _authorizationHeaderProvider.ReceivedWithAnyArgs(1).CreateAuthorizationHeaderForAppAsync(string.Empty);
-            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderForAppAsync(
-                Arg.Any<string>(), Arg.Is<DownstreamApiOptions>(o => o.AcquireTokenOptions.Claims == null));
+            await _authorizationHeaderProvider.ReceivedWithAnyArgs(1).CreateAuthorizationHeaderAsync(Enumerable.Empty<string>());
+            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderAsync(
+               Arg.Any<IEnumerable<string>>(), Arg.Is<DownstreamApiOptions>(o => o.AcquireTokenOptions.Claims == null));
         }
 
         [Fact]
@@ -93,8 +93,8 @@ namespace Microsoft.Identity.Web.Test.DownstreamWebApiSupport
 
             var userVal = await _downstreamApi.GetForUserAsync<EmptyClass>("GraphUser");
 
-            await _authorizationHeaderProvider.ReceivedWithAnyArgs(1).CreateAuthorizationHeaderForUserAsync(Enumerable.Empty<string>());
-            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderForUserAsync(
+            await _authorizationHeaderProvider.ReceivedWithAnyArgs(1).CreateAuthorizationHeaderAsync(Enumerable.Empty<string>());
+            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderAsync(
                 Arg.Any<IEnumerable<string>>(), Arg.Is<DownstreamApiOptions>(o => o.AcquireTokenOptions.Claims == null));
         }
 
@@ -109,9 +109,9 @@ namespace Microsoft.Identity.Web.Test.DownstreamWebApiSupport
 
             await Assert.ThrowsAsync<HttpRequestException>(() => _downstreamApi.GetForAppAsync<EmptyClass>("GraphApp"));
 
-            await _authorizationHeaderProvider.ReceivedWithAnyArgs(1).CreateAuthorizationHeaderForAppAsync(string.Empty);
-            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderForAppAsync(
-                Arg.Any<string>(), Arg.Is<DownstreamApiOptions>(o => o.AcquireTokenOptions.Claims == null));
+            await _authorizationHeaderProvider.ReceivedWithAnyArgs(1).CreateAuthorizationHeaderAsync(Enumerable.Empty<string>());
+            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderAsync(
+                Arg.Any<IEnumerable<string>>(), Arg.Is<DownstreamApiOptions>(o => o.AcquireTokenOptions.Claims == null));
         }
 
         [Fact]
@@ -125,8 +125,8 @@ namespace Microsoft.Identity.Web.Test.DownstreamWebApiSupport
 
             await Assert.ThrowsAsync<HttpRequestException>(() => _downstreamApi.GetForUserAsync<EmptyClass>("GraphUser"));
 
-            await _authorizationHeaderProvider.ReceivedWithAnyArgs(1).CreateAuthorizationHeaderForUserAsync(Enumerable.Empty<string>());
-            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderForUserAsync(
+            await _authorizationHeaderProvider.ReceivedWithAnyArgs(1).CreateAuthorizationHeaderAsync(Enumerable.Empty<string>());
+            await _authorizationHeaderProvider.Received().CreateAuthorizationHeaderAsync(
                 Arg.Any<IEnumerable<string>>(), Arg.Is<DownstreamApiOptions>(o => o.AcquireTokenOptions.Claims == null));
         }
 
@@ -135,6 +135,7 @@ namespace Microsoft.Identity.Web.Test.DownstreamWebApiSupport
             _authorizationHeaderProvider = Substitute.For<IAuthorizationHeaderProvider>();
             _authorizationHeaderProvider.CreateAuthorizationHeaderForAppAsync(string.Empty).ReturnsForAnyArgs("Bearer eyJhY2Nlc3NfdG9rZW4iOg==");
             _authorizationHeaderProvider.CreateAuthorizationHeaderForUserAsync(Enumerable.Empty<string>()).ReturnsForAnyArgs("Bearer eyJhY2Nlc3NfdG9rZW4iOg==");
+            _authorizationHeaderProvider.CreateAuthorizationHeaderAsync(Enumerable.Empty<string>()).ReturnsForAnyArgs("Bearer eyJhY2Nlc3NfdG9rZW4iOg==");
 
             var httpMessageHandler = new QueueHttpMessageHandler();
 
