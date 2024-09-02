c5d991d0ac216cf2cddbe27d6f9d541d48b298eb
Saurabh Gautam <saugautam@microsoft.com>
2024-09-02 12:33:10 +0100

    Use getkey algorithm for factory class

--- src/Microsoft.Identity.Web.TokenAcquisition/TokenAcquisition.cs
+++ src/Microsoft.Identity.Web.TokenAcquisition/TokenAcquisition.cs
@@ -198,7 +198,7 @@ namespace Microsoft.Identity.Web
 
         private static string GetApplicationKey(MergedOptions mergedOptions)
         {
-            return mergedOptions.Instance! + mergedOptions.ClientId + mergedOptions.AzureRegion;
+            return DefaultTokenAcquirerFactoryImplementation.GetKey(mergedOptions.Authority, mergedOptions.ClientId, mergedOptions.AzureRegion);
         }
 
         /// <summary>
