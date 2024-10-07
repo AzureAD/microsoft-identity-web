9c824da727b454552d217a81a20f9ae1320dbbf6
Westin Musser <westinmusser@microsoft.com>
2024-10-07 14:41:07 -0700

    remove api files in tests update global suppressions and null annotations disable warnings where not appropriate fix markshipped script

---
+++ src/Microsoft.Identity.Web.MicrosoftGraph/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("ApiDesign", "RS0027:API with optional parameter(s) should have the most parameters amongst its public overloads", Justification = "Existing public API", Scope = "member", Target = "~M:Microsoft.Identity.Web.MicrosoftGraphExtensions.AddMicrosoftGraph(Microsoft.Identity.Web.MicrosoftIdentityAppCallsWebApiAuthenticationBuilder,System.String,System.String)~Microsoft.Identity.Web.MicrosoftIdentityAppCallsWebApiAuthenticationBuilder")]
