cd407c529372ba240b428343527a807db8364b0f
guardrex <1622880+guardrex@users.noreply.github.com>
2024-10-21 18:19:59 -0400

    React to feedback

--- tests/DevApps/B2CWebAppCallsWebApi/Client/Startup.cs
+++ tests/DevApps/B2CWebAppCallsWebApi/Client/Startup.cs
@@ -34,7 +34,7 @@ namespace WebApp_OpenIDConnect_DotNet
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
-                // Handling SameSite cookie according to https://learn.microsoft.com/aspnet/core/security/samesite
+                // Handles SameSite cookies according to https://learn.microsoft.com/aspnet/core/security/samesite.
                 options.HandleSameSiteCookieCompatibility();
             });
 
