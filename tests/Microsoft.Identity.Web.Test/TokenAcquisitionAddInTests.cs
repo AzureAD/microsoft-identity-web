1d438377b64701a7aaa59164344a831e53db7736
trwalke <trwalke@microsoft.com>
2024-10-22 20:10:59 -0700

    Updating test

--- tests/Microsoft.Identity.Web.Test/TokenAcquisitionAddInTests.cs
+++ tests/Microsoft.Identity.Web.Test/TokenAcquisitionAddInTests.cs
@@ -1,7 +1,11 @@
+using System;
 using Microsoft.Identity.Abstractions;
 using Microsoft.Identity.Client;
+using Microsoft.Identity.Web.Test.Common.Mocks;
+using Microsoft.Identity.Web.Test.Common;
 using NSubstitute;
 using Xunit;
+using System.Threading.Tasks;
 
 namespace Microsoft.Identity.Web.Tests
 {
@@ -30,27 +34,49 @@ namespace Microsoft.Identity.Web.Tests
         }
 #endif
         [Fact]
-        public void InvokeOnBeforeTokenAcquisitionForApp_InvokesEvent()
+        public async Task InvokeOnBeforeTokenAcquisitionForApp_InvokesEvent()
         {
             // Arrange
             var options = new TokenAcquisitionExtensionOptions();
             var acquireTokenOptions = new AcquireTokenOptions();
             acquireTokenOptions.ForceRefresh = true;
-            AcquireTokenForClientParameterBuilder builderMock = null!;
 
-            bool eventInvoked = false;
-            options.OnBeforeTokenAcquisitionForApp += (builder, options) =>
+            //Configure mocks
+            using MockHttpClientFactory mockHttpClient = new MockHttpClientFactory();
+            using (mockHttpClient.AddMockHandler(MockHttpCreator.CreateClientCredentialTokenHandler()))
+            using (mockHttpClient.AddMockHandler(MockHttpCreator.CreateClientCredentialTokenHandler()))
             {
-                eventInvoked = true;
-                builder.WithForceRefresh(options!.ForceRefresh);
-                builder.Received().WithForceRefresh(true);
-            };
+                var confidentialApp = ConfidentialClientApplicationBuilder
+                               .Create(TestConstants.ClientId)
+                               .WithAuthority(TestConstants.AuthorityCommonTenant)
+                               .WithHttpClientFactory(mockHttpClient)
+                               .WithInstanceDiscovery(false)
+                               .WithClientSecret(TestConstants.ClientSecret)
+                               .Build();
+                AcquireTokenForClientParameterBuilder builder = confidentialApp.AcquireTokenForClient(new string[] { "scope" });
 
-            // Act
-            options.InvokeOnBeforeTokenAcquisitionForApp(builderMock, acquireTokenOptions);
+                bool eventInvoked = false;
+                options.OnBeforeTokenAcquisitionForApp += async (builder, options) =>
+                {
+                    eventInvoked = true;
+                    var result = await builder.ExecuteAsync().ConfigureAwait(false);
+                    Assert.NotNull(result);
+                    Assert.True(result.AuthenticationResultMetadata.TokenSource == TokenSource.IdentityProvider);
 
-            // Assert
-            Assert.True(eventInvoked);
+                    //Ensure ForceRefresh is set on the builder
+                    builder.WithForceRefresh(options!.ForceRefresh);
+                    result = await builder.ExecuteAsync().ConfigureAwait(false);
+                    Assert.NotNull(result);
+                    Assert.True(result.AuthenticationResultMetadata.TokenSource == TokenSource.IdentityProvider);
+                };
+
+
+                // Act
+                options.InvokeOnBeforeTokenAcquisitionForApp(builder, acquireTokenOptions);
+
+                // Assert
+                Assert.True(eventInvoked);
+            }
         }
 
 #if FUTURE
