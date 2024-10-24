8f9298236f4c4fe45e2185ea04753681a11baf1a
trwalke <trwalke@microsoft.com>
2024-10-24 13:59:54 -0700

    Adding unused code

--- src/Microsoft.Identity.Web.TokenAcquisition/TokenAcquisitionExtensionOptions.cs
+++ src/Microsoft.Identity.Web.TokenAcquisition/TokenAcquisitionExtensionOptions.cs
@@ -13,9 +13,31 @@ namespace Microsoft.Identity.Web
     public partial class TokenAcquisitionExtensionOptions
     {
         /// <summary>
+        /// Event fired when the MSAL application needs to be built.
+        /// </summary>
+        public event BuildApplication? OnBuildConfidentialClientApplication;
+
+        /// <summary>
         /// Event fired when a client credential flow request is being built.
         /// </summary>        
         public event BeforeTokenAcquisitionForApp? OnBeforeTokenAcquisitionForApp;
+
+        /// <summary>
+        /// Event fired when an authentication result is available.
+        /// </summary>
+        public event AfterTokenAcquisition? OnAfterTokenAcquisition;
+
+        /// <summary>
+        /// Invoke the OnBuildConfidentialClientApplication event.
+        /// </summary>
+        internal void InvokeOnBuildConfidentialClientApplication(ConfidentialClientApplicationBuilder builder,
+                                                                 AcquireTokenOptions? acquireTokenOptions)
+        {
+            if (OnBuildConfidentialClientApplication != null)
+            {
+                OnBuildConfidentialClientApplication(builder, acquireTokenOptions);
+            }
+        }
 
         /// <summary>
         /// Invoke the OnBeforeTokenAcquisitionForApp event.
@@ -26,6 +48,18 @@ namespace Microsoft.Identity.Web
             if (OnBeforeTokenAcquisitionForApp != null)
             {
                 OnBeforeTokenAcquisitionForApp(builder, acquireTokenOptions);
+            }
+        }
+
+        /// <summary>
+        /// Invoke the OnAfterTokenAcquisition event.
+        /// </summary>
+        internal void InvokeOnAfterTokenAcquisition(AuthenticationResult result,
+                                                    AcquireTokenOptions? acquireTokenOptions)
+        {
+            if (OnAfterTokenAcquisition != null)
+            {
+                OnAfterTokenAcquisition(result, acquireTokenOptions);
             }
         }
     }
