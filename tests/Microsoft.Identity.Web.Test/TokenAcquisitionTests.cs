29761d2030f7740847ab1079bebbf012188ba088
msbw2 <brettwhite@microsoft.com>
2024-11-07 21:14:45 -0800

    Use AppHomeTenantId for acquiring app token when TenantId is not tenant (#3132)

---
+++ tests/Microsoft.Identity.Web.Test/TokenAcquisitionTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Xunit;
+
+namespace Microsoft.Identity.Web.Test
+{
+    public class TokenAcquisitionTests
+    {
+        private const string Tenant = "tenant";
+        private const string TenantId = "tenant-id";
+        private const string AppHomeTenantId = "app-home-tenant-id";
+
+        [Theory]
+        [InlineData(null, null, null, null)]
+        [InlineData(null, null, AppHomeTenantId, null)]
+        [InlineData(Tenant, null, null, Tenant)]
+        [InlineData(Tenant, TenantId, null, Tenant)]
+        [InlineData(Tenant, null, AppHomeTenantId, Tenant)]
+        [InlineData(Tenant, TenantId, AppHomeTenantId, Tenant)]
+        [InlineData(null, TenantId, null, TenantId)]
+        [InlineData(null, TenantId, AppHomeTenantId, TenantId)]
+        [InlineData(null, Constants.Common, AppHomeTenantId, AppHomeTenantId)]
+        [InlineData(null, Constants.Organizations, AppHomeTenantId, AppHomeTenantId)]
+        public void TestResolveTenantReturnsCorrectTenant(string? tenant, string? tenantId, string? appHomeTenantId, string? expectedValue)
+        {
+            string? resolvedTenant = TokenAcquisition.ResolveTenant(tenant, new MergedOptions { TenantId = tenantId, AppHomeTenantId = appHomeTenantId });
+            Assert.Equal(expectedValue, resolvedTenant);
+        }
+
+        [Theory]
+        [InlineData(Constants.Common, null)]
+        [InlineData(Constants.Organizations, null)]
+        [InlineData(Constants.Common, TenantId)]
+        [InlineData(Constants.Organizations, TenantId)]
+        [InlineData(Constants.Common, Constants.Common)]
+        [InlineData(Constants.Common, Constants.Organizations)]
+        [InlineData(Constants.Organizations, Constants.Organizations)]
+        [InlineData(Constants.Organizations, Constants.Common)]
+        [InlineData(null, Constants.Common)]
+        [InlineData(null, Constants.Organizations)]
+        public void TestResolveTenantThrowsWhenMetaTenant(string? tenant, string? tenantId)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => TokenAcquisition.ResolveTenant(tenant, new MergedOptions { TenantId = tenantId }));
+            Assert.StartsWith(IDWebErrorMessage.ClientCredentialTenantShouldBeTenanted, exception.Message, StringComparison.Ordinal);
+        }
+    }
+}
