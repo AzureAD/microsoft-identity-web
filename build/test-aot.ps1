0a46c98d8375a3a2f6bdc467d216ac01b40ca69c
Jenny Ferries <jeferrie@microsoft.com>
2024-11-13 21:08:25 -0800

    fix aot.ps1

--- build/test-aot.ps1
+++ build/test-aot.ps1
@@ -27,8 +27,6 @@ if ($LastExitCode -ne 0)
 $runtime = if ($IsWindows) {  "win-x64" } elseif ($IsMacOS) { "macos-x64"} else {"linux-x64"}
 $app = if ($IsWindows ) {"./$projectName.exe" } else {"./$projectName" }
 
-Push-Location $rootDirectory/tests/$projectName/bin/Release/$targetNetFramework/$runtime
-
 Write-Host "Executing test App..."
 $app
 Write-Host "Finished executing test App"
