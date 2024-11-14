e0320c2f28edd915d759d5806e5a705d307d3b00
Peter <34331512+pmaytak@users.noreply.github.com>
2024-11-14 03:00:52 -0800

    Update AOT test.

--- build/test-aot.ps1
+++ build/test-aot.ps1
@@ -16,7 +16,7 @@ foreach ($line in $($publishOutput -split "`r`n"))
 }
 
 Write-Host "Actual warning count is: ", $actualWarningCount
-$expectedWarningCount = 63
+$expectedWarningCount = 61
 
 if ($LastExitCode -ne 0)
 {
