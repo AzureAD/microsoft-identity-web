3f9dd5052602937e07590ace34959841180123a7
Josh Lozensky <joshlozensky@microsoft.com>
2025-02-25 14:40:23 -0800

    make logic scheme agnostic

--- src/Microsoft.Identity.Web.TokenAcquisition/MergedOptions.cs
+++ src/Microsoft.Identity.Web.TokenAcquisition/MergedOptions.cs
@@ -442,9 +442,10 @@ namespace Microsoft.Identity.Web
         {
             if (string.IsNullOrEmpty(mergedOptions.TenantId) && string.IsNullOrEmpty(mergedOptions.Instance) && !string.IsNullOrEmpty(mergedOptions.Authority))
             {
-                ReadOnlySpan<char> httpsPrefix = "https://".AsSpan();
+                ReadOnlySpan<char> doubleSlash = "//".AsSpan();
                 ReadOnlySpan<char> authoritySpan = mergedOptions.Authority.AsSpan().TrimEnd('/');
-                int startingIndex = authoritySpan.StartsWith(httpsPrefix) ? httpsPrefix.Length : 0;
+                int doubleSlashIndex = authoritySpan.IndexOf(doubleSlash);
+                int startingIndex = doubleSlashIndex == -1 ? 0 : doubleSlashIndex + doubleSlash.Length;
 
                 // Gets position of first '/' after the "https://" prefix, will return -1 if not found, this checks for presence of instance/tenantId
                 int indexTenant = authoritySpan.Slice(startingIndex).IndexOf('/') + startingIndex;
