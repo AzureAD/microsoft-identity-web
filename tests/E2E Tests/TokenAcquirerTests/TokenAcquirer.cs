192ea2d60eaacc88cca481c64cd40c53efb4f050
Josh Lozensky <joshlozensky@microsoft.com>
2025-03-05 14:05:13 -0800

    adding missing protection for using token acquirer to test

--- tests/E2E Tests/TokenAcquirerTests/TokenAcquirer.cs
+++ tests/E2E Tests/TokenAcquirerTests/TokenAcquirer.cs
@@ -477,6 +477,7 @@ namespace TokenAcquirerTests
         }
     }
 
+    [CollectionDefinition(nameof(TokenAcquirerFactorySingletonProtection))]
     public class AcquireTokenManagedIdentity
     {
         [OnlyOnAzureDevopsFact]
@@ -487,6 +488,7 @@ namespace TokenAcquirerTests
             const string scope = "https://vault.azure.net/.default";
             const string baseUrl = "https://vault.azure.net";
             const string clientId = "5bcd1685-b002-4fd1-8ebd-1ec3e1e4ca4d";
+            TokenAcquirerFactoryTesting.ResetTokenAcquirerFactoryInTest();
             TokenAcquirerFactory tokenAcquirerFactory = TokenAcquirerFactory.GetDefaultInstance();
             IServiceProvider serviceProvider = tokenAcquirerFactory.Build();
 
