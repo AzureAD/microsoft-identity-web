827907549122fc8d15484c2156e955bddb1febc7
Kashif Mehmood <kashifm@ntdev.microsoft.com>
2025-03-14 15:21:35 -0700

    Revert "Make Microsoft.Extensions.Http dependency framework friendly"
    This reverts commit 5082a1fa0c3bb144eb6bef5ef6bc796d6202a39e.

--- src/Microsoft.Identity.Web.TokenAcquisition/AspNetCore/JwtBearerOptionsMerger.cs
+++ src/Microsoft.Identity.Web.TokenAcquisition/AspNetCore/JwtBearerOptionsMerger.cs
@@ -18,7 +18,7 @@ namespace Microsoft.Identity.Web
         private readonly IMergedOptionsStore _mergedOptionsMonitor;
 
         public void PostConfigure(
-#if NET6_0_OR_GREATER
+#if NET7_0_OR_GREATER
             string? name,
 #else
             string name,
