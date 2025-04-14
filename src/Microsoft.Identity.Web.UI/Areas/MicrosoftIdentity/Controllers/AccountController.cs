6a4ff27a01d1a54099ef567af01bc9b81dfb2340
saurabhsathe-ms <saurabhsathe@microsoft.com>
2025-04-13 20:45:34 -0700

    Added new optional paramters in the sign in function. Removed the old public api signatire and added new one.

--- src/Microsoft.Identity.Web.UI/Areas/MicrosoftIdentity/Controllers/AccountController.cs
+++ src/Microsoft.Identity.Web.UI/Areas/MicrosoftIdentity/Controllers/AccountController.cs
@@ -39,11 +39,15 @@ namespace Microsoft.Identity.Web.UI.Areas.MicrosoftIdentity.Controllers
         /// </summary>
         /// <param name="scheme">Authentication scheme.</param>
         /// <param name="redirectUri">Redirect URI.</param>
+        /// <param name="loginHint">Login hint (user's email address).</param>
+        /// <param name = "domainHint" > Domain hint.</param>
         /// <returns>Challenge generating a redirect to Azure AD to sign in the user.</returns>
         [HttpGet("{scheme?}")]
         public IActionResult SignIn(
             [FromRoute] string scheme,
-            [FromQuery] string redirectUri)
+            [FromQuery] string redirectUri,
+            [FromQuery] string loginHint,
+            [FromQuery] string domainHint)
         {
             scheme ??= OpenIdConnectDefaults.AuthenticationScheme;
             string redirect;
@@ -55,7 +59,16 @@ namespace Microsoft.Identity.Web.UI.Areas.MicrosoftIdentity.Controllers
             {
                 redirect = Url.Content("~/")!;
             }
+            var authProps = new AuthenticationProperties { RedirectUri = redirect };
+            if (!string.IsNullOrEmpty(loginHint))
+            {
+                authProps.Parameters[Constants.LoginHint] = loginHint;
+            }
 
+            if (!string.IsNullOrEmpty(domainHint))
+            {
+                authProps.Parameters[Constants.DomainHint] = domainHint;
+            }
             return Challenge(
                 new AuthenticationProperties { RedirectUri = redirect },
                 scheme);
