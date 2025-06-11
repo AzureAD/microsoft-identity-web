c9744b729f05d3e1ee6d2f6d456cee563e181182
Travis Walker <travis.walker@microsoft.com>
2025-06-11 15:45:41 -0700

    Adding fix for OBO cache key error in long running OBO with DefaultAuthorizationHeaderProvider (#3381)
    * Adding fix for OBO cache key error
    
    * Refactoring
    
    * Refactoring, updating test cases
    
    * Adding LRObo mock response
    
    * Fixing test issue
    
    * Adding new test
    
    ---------
    
    Co-authored-by: trwalke <trwalke@microsoft.com>

--- tests/Microsoft.Identity.Web.Test.Common/Mocks/MockHttpCreator.cs
+++ tests/Microsoft.Identity.Web.Test.Common/Mocks/MockHttpCreator.cs
@@ -21,6 +21,38 @@ namespace Microsoft.Identity.Web.Test.Common.Mocks
                 "{\"token_type\":\"" + tokenType + "\",\"expires_in\":" + expiry + ",\"client_info\":\"" + CreateClientInfo() + "\",\"access_token\":\"" + token + "\"}");
         }
 
+        public static HttpResponseMessage GetLrOboTokenResponse(string scopes, string accessToken = "header.payload.signature", string refreshToken = "header.payload.signatureRt")
+        {
+            return CreateSuccessResponseMessage(
+          "{\"token_type\":\"Bearer\",\"expires_in\":\"3599\",\"refresh_in\":\"2400\",\"scope\":" +
+          "\"" + scopes + "\",\"access_token\":\"" + accessToken + "\"" +
+          ",\"refresh_token\":\"" + refreshToken + "\",\"client_info\"" +
+          ":\"" + CreateClientInfo() + "\",\"id_token\"" +
+          ":\"" + CreateIdToken("UniqueId", "DisplayableId") + "\"}");
+        }
+
+        public static string CreateIdToken(string uniqueId, string displayableId)
+        {
+            return CreateIdToken(uniqueId, displayableId, TestConstants.Utid);
+        }
+
+        public static string CreateIdToken(string uniqueId, string displayableId, string tenantId)
+        {
+            string id = "{\"aud\": \"e854a4a7-6c34-449c-b237-fc7a28093d84\"," +
+                        "\"iss\": \"https://login.microsoftonline.com/6c3d51dd-f0e5-4959-b4ea-a80c4e36fe5e/v2.0/\"," +
+                        "\"iat\": 1455833828," +
+                        "\"nbf\": 1455833828," +
+                        "\"exp\": 1455837728," +
+                        "\"ipaddr\": \"131.107.159.117\"," +
+                        "\"name\": \"Marrrrrio Bossy\"," +
+                        "\"oid\": \"" + uniqueId + "\"," +
+                        "\"preferred_username\": \"" + displayableId + "\"," +
+                        "\"sub\": \"K4_SGGxKqW1SxUAmhg6C1F6VPiFzcx-Qd80ehIEdFus\"," +
+                        "\"tid\": \"" + tenantId + "\"," +
+                        "\"ver\": \"2.0\"}";
+            return string.Format(CultureInfo.InvariantCulture, "someheader.{0}.somesignature", Base64UrlHelpers.Encode(id));
+        }
+
         public static HttpResponseMessage CreateSuccessResponseMessage(string successResponse)
         {
             HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
@@ -57,6 +89,18 @@ namespace Microsoft.Identity.Web.Test.Common.Mocks
             {
                 ExpectedMethod = HttpMethod.Post,
                 ResponseMessage = CreateSuccessfulClientCredentialTokenResponseMessage(token, tokenType, expiresIn),
+            };
+
+            return handler;
+        }
+
+        public static MockHttpMessageHandler CreateLrOboTokenHandler(
+            string scopes, string accessToken = "header.payload.signature", string refreshToken = "header.payload.signatureRt")
+        {
+            var handler = new MockHttpMessageHandler()
+            {
+                ExpectedMethod = HttpMethod.Post,
+                ResponseMessage = GetLrOboTokenResponse(scopes, accessToken, refreshToken),
             };
 
             return handler;
