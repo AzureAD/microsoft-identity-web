925863b3ea08ebfdcbbad2f7ccfc04586c7fbf2e
Jean-Marc Prieur <jmprieur@microsoft.com>
2025-06-27 09:29:07 -0700

    Fixing the TryAddEnumerable

--- src/Microsoft.Identity.Web.OidcFIC/OidcFicSignedAssertionProviderExtensions.cs
+++ src/Microsoft.Identity.Web.OidcFIC/OidcFicSignedAssertionProviderExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Identity.Abstractions;
 using Microsoft.Identity.Web.OidcFic;
 
@@ -19,9 +20,11 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <returns>the service collection for chaining.</returns>
         public static IServiceCollection AddOidcFic(this IServiceCollection services)
         {
-
-            
-            services.TryAddEnumerable[ ServiceDescriptor.Singleton<ICustomSignedAssertionProvider, OidcIdpSignedAssertionLoader>] ();
+            services.TryAddEnumerable(
+                new[]
+                {
+                ServiceDescriptor.Singleton<ICustomSignedAssertionProvider, OidcIdpSignedAssertionLoader>()
+                });
             return services;
         }
     }
