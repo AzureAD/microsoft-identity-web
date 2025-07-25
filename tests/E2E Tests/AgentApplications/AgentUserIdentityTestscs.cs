2fe77a76af3e31826ae32de91898b7c4faab0b15
Jean-Marc Prieur <jmprieur@microsoft.com>
2025-07-25 10:49:40 -0700

    Adding a test for the cached user

--- tests/E2E Tests/AgentApplications/AgentUserIdentityTestscs.cs
+++ tests/E2E Tests/AgentApplications/AgentUserIdentityTestscs.cs
@@ -1,13 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Security.Claims;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Graph;
 using Microsoft.Identity.Abstractions;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.TestOnly;
+using Microsoft.Identity.Web.TokenCacheProviders.Distributed;
 using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AgentApplicationsTests
 {
@@ -30,9 +33,9 @@ namespace AgentApplicationsTests
             string userUpn = "aui1@msidlabtoint.onmicrosoft.com";          // Replace with the actual user upn.
 
             services.AddSingleton(configuration);
-            services.AddTokenAcquisition();
+            services.AddDistributedTokenCaches();
+            services.AddDistributedMemoryCache();
             services.AddHttpClient();
-            services.AddInMemoryTokenCaches();
             services.Configure<MicrosoftIdentityOptions>(configuration.GetSection("AzureAd"));
             services.AddAgentIdentities();
             services.AddMicrosoftGraph(); // If you want to call Microsoft Graph
@@ -59,8 +62,60 @@ namespace AgentApplicationsTests
             //IDownstreamApi downstream = serviceProvider.GetService<IDownstreamApi>()!;
             //string? response = await downstream.GetForAppAsync<string>("api", options => options.WithAgentIdentity("your-agent-identity-here"));
             //response = await downstream.GetForUserAsync<string>("api", options => options.WithAgentIdentity("your-agent-identity-here"));
+        }
 
+        [Fact]
+        public async Task AgentUserIdentityGetsTokenForGraphWithCacheAsync()
+        {
+            IServiceCollection services = new ServiceCollection();
+            IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection().Build();
 
+            configuration["AzureAd:Instance"] = "https://login.microsoftonline.com/";
+            configuration["AzureAd:TenantId"] = "31a58c3b-ae9c-4448-9e8f-e9e143e800df";
+            configuration["AzureAd:ClientId"] = "d15884b6-a447-4dd5-a5a5-a668c49f6300"; // Agent application.
+            configuration["AzureAd:ClientCredentials:0:SourceType"] = "StoreWithDistinguishedName";
+            configuration["AzureAd:ClientCredentials:0:CertificateStorePath"] = "LocalMachine/My";
+            configuration["AzureAd:ClientCredentials:0:CertificateDistinguishedName"] = "CN=LabAuth.MSIDLab.com";
+            string agentIdentity = "d84da24a-2ea2-42b8-b5ab-8637ec208024"; // Replace with the actual agent identity
+            string userUpn = "aui1@msidlabtoint.onmicrosoft.com";          // Replace with the actual user upn.
+
+            services.AddSingleton(configuration);
+            services.AddTokenAcquisition();
+            services.AddHttpClient();
+            services.AddDistributedTokenCaches();
+            services.AddDistributedMemoryCache();
+            services.Configure<MicrosoftIdentityOptions>(configuration.GetSection("AzureAd"));
+            services.AddAgentIdentities();
+            services.AddMicrosoftGraph(); // If you want to call Microsoft Graph
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Get an authorization header and handle the call to the downstream API yourself
+            IAuthorizationHeaderProvider authorizationHeaderProvider = serviceProvider.GetService<IAuthorizationHeaderProvider>()!;
+            AuthorizationHeaderProviderOptions options = new AuthorizationHeaderProviderOptions().WithAgentUserIdentity(
+                agentApplicationId: agentIdentity,
+                username: userUpn
+                );
+
+            ClaimsPrincipal user = new ClaimsPrincipal();
+            string authorizationHeaderWithUserToken = await authorizationHeaderProvider.CreateAuthorizationHeaderForUserAsync(
+                scopes: ["https://graph.microsoft.com/.default"],
+                options,
+                user);
+            Assert.NotNull(authorizationHeaderWithUserToken);
+            Assert.True(user.HasClaim(c => c.Type == "uid"));
+            Assert.True(user.HasClaim(c => c.Type == "utid"));
+
+            // Use the cached user
+            authorizationHeaderWithUserToken = await authorizationHeaderProvider.CreateAuthorizationHeaderForUserAsync(
+                scopes: ["https://graph.microsoft.com/.default"],
+                options,
+                user);
+            Assert.NotNull(authorizationHeaderWithUserToken);
+
+            //// If you want to call downstream APIs letting IdWeb handle authentication.
+            //IDownstreamApi downstream = serviceProvider.GetService<IDownstreamApi>()!;
+            //string? response = await downstream.GetForAppAsync<string>("api", options => options.WithAgentIdentity("your-agent-identity-here"));
+            //response = await downstream.GetForUserAsync<string>("api", options => options.WithAgentIdentity("your-agent-identity-here"));
         }
     }
 }
