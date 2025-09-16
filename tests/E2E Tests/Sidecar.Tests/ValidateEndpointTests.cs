1034c0e30a05868eb4befbc723c1f230fe4797dd
Jean-Marc Prieur <jmprieur@microsoft.com>
2025-09-15 18:11:35 -0700

    more end to end tests for sidecar (#3477)
    * Adding E2E test
    
    * Improving the tests
    
    * Fix name
    
    * Apply suggestion from @keegan-caruso
    
    Co-authored-by: Keegan <Keegan.Caruso@microsoft.com>
    
    ---------
    
    Co-authored-by: Keegan <Keegan.Caruso@microsoft.com>

--- tests/E2E Tests/Sidecar.Tests/ValidateEndpointTests.cs
+++ tests/E2E Tests/Sidecar.Tests/ValidateEndpointTests.cs
@@ -2,16 +2,29 @@
 // Licensed under the MIT License.
 
 using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Abstractions;
+using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.Sidecar;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
 using Xunit;
 
 namespace Sidecar.Tests;
 
 public class SidecarApiFactory : WebApplicationFactory<Program>
 {
-    protected override void ConfigureWebHost(Microsoft.AspNetCore.Hosting.IWebHostBuilder builder)
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureAppConfiguration(builder =>
+        {
+            builder.AddJsonFile(
+                 path: Path.Combine(Directory.GetCurrentDirectory().ToString(), "appsettings.agentids.json"),
+                 optional: false,
+                 reloadOnChange: true);
+        });
         builder.ConfigureServices(services =>
         {
         });
@@ -33,5 +46,51 @@ public class ValidateEndpointTests : IClassFixture<SidecarApiFactory>
         Assert.Equal(System.Net.HttpStatusCode.Unauthorized, response.StatusCode);
         var content = await response.Content.ReadAsStringAsync();
         Assert.Contains("invalid_token", response.Headers.WwwAuthenticate.ToString(), StringComparison.CurrentCultureIgnoreCase);
+    }
+
+    [Fact]
+    public async Task Validate_WhenGoodTokenAsync()
+    {
+        // Getting a token to call the API.
+        string authorizationHeader = await GetAuthorizationHeaderToCallTheSideCarAsync();
+
+        // Calling the API
+        var client = _factory.CreateClient();
+
+        client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(authorizationHeader);
+        var response = await client.GetAsync("/Validate");
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.NotEmpty(content);
+    }
+
+    private static async Task<string> GetAuthorizationHeaderToCallTheSideCarAsync()
+    {
+        ServiceCollection services = new();
+        IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection().Build();
+        services.AddSingleton<IConfiguration>(configuration);
+        configuration["Instance"] = "https://login.microsoftonline.com/";
+        configuration["TenantId"] = "31a58c3b-ae9c-4448-9e8f-e9e143e800df";
+        configuration["ClientId"] = "5cbcd9ff-c994-49ac-87e7-08a93a9c0794";
+        configuration["SendX5C"] = "true";
+        configuration["ClientCredentials:0:SourceType"] = "StoreWithDistinguishedName";
+        configuration["ClientCredentials:0:CertificateStorePath"] = "LocalMachine/My";
+        configuration["ClientCredentials:0:CertificateDistinguishedName"] = "CN=LabAuth.MSIDLab.com";
+
+        services.AddTokenAcquisition().AddHttpClient().AddInMemoryTokenCaches();
+        services.Configure<MicrosoftIdentityApplicationOptions>(configuration);
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        IAuthorizationHeaderProvider authorizationHeaderProvider = serviceProvider.GetRequiredService<IAuthorizationHeaderProvider>();
+        string authorizationHeader = await authorizationHeaderProvider.CreateAuthorizationHeaderForAppAsync("api://d15884b6-a447-4dd5-a5a5-a668c49f6300/.default",
+            new AuthorizationHeaderProviderOptions()
+            {
+                AcquireTokenOptions = new AcquireTokenOptions()
+                {
+                    AuthenticationOptionsName = ""
+                }
+            });
+        return authorizationHeader;
     }
 }
