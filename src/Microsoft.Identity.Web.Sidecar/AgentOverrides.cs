fb3e8b257dbafd2e015c3cf59d1e0f78056a5ee3
Keegan <Keegan.Caruso@microsoft.com>
2025-09-30 16:33:10 -0700

    Authorization Header and downstream API endpoint updates (#3507)
    * Authorization Header and downstream API endpoints updates
    * Fix open api generation
    
    ---------
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>

---
+++ src/Microsoft.Identity.Web.Sidecar/AgentOverrides.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Abstractions;
+
+namespace Microsoft.Identity.Web.Sidecar;
+
+public class AgentOverrides
+{
+    public static void SetOverrides(DownstreamApiOptions options, string? agentIdentity, string? agentUsername)
+    {
+        // To override the tenant use DownstreamAPI.AcquireTokenOptions.Tenant
+        if (!string.IsNullOrEmpty(agentIdentity) && !string.IsNullOrEmpty(agentUsername))
+        {
+            options.WithAgentUserIdentity(agentIdentity, agentUsername);
+        }
+        else if (!string.IsNullOrEmpty(agentIdentity))
+        {
+            options.WithAgentIdentity(agentIdentity);
+        }
+    }
+}
---
+++ src/Microsoft.Identity.Web.Sidecar/OptionsOverrideSchemaTransformer.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace Microsoft.Identity.Web.Sidecar
+{
+    public class OptionsOverrideOperationTransformer : IOpenApiOperationTransformer
+    {
+        public Task TransformAsync(OpenApiOperation operation, OpenApiOperationTransformerContext context, CancellationToken cancellationToken)
+        {
+            // Detect custom metadata (attribute example shown below).
+            var overrideMeta = context.Description.RelativePath?.Contains("AuthorizationHeader", StringComparison.InvariantCulture) == true ||
+                context.Description.RelativePath?.Contains("Downstream", StringComparison.InvariantCulture) == true;
+
+            if (!overrideMeta)
+            {
+                return Task.CompletedTask;
+            }
+
+            OpenApiDescriptions.AddOptionsOverrideParameters(operation);
+
+            return Task.CompletedTask;
+        }
+    }
+}
