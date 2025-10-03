c4eadfedbfe418508246bfa9655873d0d4e45617
Keegan <Keegan.Caruso@microsoft.com>
2025-10-02 18:13:25 -0700

    Make the sidecar trim friendlier (#3518)
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>

--- src/Microsoft.Identity.Web.Sidecar/CacheControl.cs
+++ src/Microsoft.Identity.Web.Sidecar/CacheControl.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure;
 using Microsoft.Net.Http.Headers;
 
 namespace Microsoft.Identity.Web.Sidecar;
