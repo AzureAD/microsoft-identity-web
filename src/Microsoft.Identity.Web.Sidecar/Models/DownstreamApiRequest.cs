8bce15773c727082317c7b4385ec80b2a4b05758
Keegan <Keegan.Caruso@microsoft.com>
2025-10-06 17:57:56 -0700

    Microsoft.Identity.Web.Sidecar (#3524)
    * initial commit
    
    * Refactor, add basic error handling
    
    * Adding E2E test (#3476)
    
    * Add authorization header endpoint, minor cleanup
    
    * more end to end tests for sidecar (#3477)
    
    * Adding E2E test
    
    * Improving the tests
    
    * Fix name
    
    * Apply suggestion from @keegan-caruso
    
    Co-authored-by: Keegan <Keegan.Caruso@microsoft.com>
    
    ---------
    
    Co-authored-by: Keegan <Keegan.Caruso@microsoft.com>
    
    * Add more tests
    
    * Improvements (#3478)
    
    * Fix model bindng (#3479)
    
    Other cleanup and improvements
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Update tests to new behavior + 401 on AuthorizationHeader when we are sure it's an auth issue (#3480)
    
    * Improvements
    
    * fix tests with new behavior
    
    * Apply suggestion from @keegan-caruso
    
    Co-authored-by: Keegan <Keegan.Caruso@microsoft.com>
    
    * Apply Keegan's suggestion
    
    ---------
    
    Co-authored-by: Keegan <Keegan.Caruso@microsoft.com>
    
    * add downstream api (#3483)
    
    * add downstream api
    
    ---------
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Enable container workflow (#3496)
    
    * containerization
    
    * Enable container workflows
    
    ---------
    
    Co-authored-by: Jean-Marc Prieur <jmprieur@microsoft.com>
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Add more e2e tests (#3504)
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Authorization Header and downstream API endpoint updates (#3507)
    
    * Authorization Header and downstream API endpoints updates
    * Fix open api generation
    
    ---------
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Sidecar endpoint descriptions (#3510)
    
    * Add more descriptions to open api document
    * Adjust for open api doc generation
    
    ---------
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Current implementation of Open API description generation is not trim friendly
    
    * Add agentuserid to list of params (#3514)
    
    * Add agentuser id to list of params
    
    * Apply suggestion from @jmprieur
    
    Co-authored-by: Jean-Marc Prieur <jmprieur@microsoft.com>
    
    * Apply suggestion from @jmprieur
    
    Co-authored-by: Jean-Marc Prieur <jmprieur@microsoft.com>
    
    * update openapi file
    
    ---------
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    Co-authored-by: Jean-Marc Prieur <jmprieur@microsoft.com>
    
    * Add windows container (#3516)
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Sidecar python adapter devapp (#3508)
    
    * Python adapter
    
    * feedback
    
    ---------
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Add readme (#3517)
    
    * Add readme
    
    * Move runtime composition details in README
    
    move runtime composition section
    
    ---------
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Make the sidecar trim friendlier (#3518)
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Update python adapter (#3519)
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Don't use R2R (#3523)
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    
    * Exclude EndpointsE2ETests when ran from GH action
    
    * include FROM_GITHUB_ACTION in sidecar tests
    
    * Use in-memory config for sidecar e2e test
    
    * Apply suggestions from code review
    
    Co-authored-by: Jean-Marc Prieur <jmprieur@microsoft.com>
    
    * Move SidecarApiFactory to separate file
    
    * Also needs test filter
    
    ---------
    
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>
    Co-authored-by: Jean-Marc Prieur <jmprieur@microsoft.com>

---
+++ src/Microsoft.Identity.Web.Sidecar/Models/DownstreamApiRequest.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microsoft.Identity.Web.Sidecar.Models;
+
+/// <summary>
+/// Represents the inputs to the downstream API endpoint.
+/// </summary>
+public readonly struct DownstreamApiRequest
+{
+    [FromQuery]
+    [Description("The identity of the agent.")]
+    public string? AgentIdentity { get; init; }
+
+    [FromQuery]
+    [Description("The username (UPN) of the agent.")]
+    public string? AgentUsername { get; init; }
+
+    [FromQuery]
+    [Description("The Object ID of the user agent identity (OID).")]
+    [StringSyntax(StringSyntaxAttribute.GuidFormat)]
+    public string? AgentUserId { get; init; }
+}
