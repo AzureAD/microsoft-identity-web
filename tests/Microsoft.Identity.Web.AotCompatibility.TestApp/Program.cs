eca74bb5800db0d7a8a1bfa6b384f75e6108299a
Jean-Marc Prieur <jmprieur@microsoft.com>
2026-02-03 19:34:54 -0800

    AotTest

--- tests/Microsoft.Identity.Web.AotCompatibility.TestApp/Program.cs
+++ tests/Microsoft.Identity.Web.AotCompatibility.TestApp/Program.cs
@@ -1,11 +1,26 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+
 internal sealed class Program
 {
     // The code in this program is expected to be trim and AOT compatible
     private static int Main()
     {
-        return 100;
+        var builder = WebApplication.CreateSlimBuilder();
+
+        builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApiAot(builder.Configuration.GetSection("AzureAd"), JwtBearerDefaults.AuthenticationScheme,  (o) => {});
+
+        builder.Services.AddTokenAcquisition()
+            .AddInMemoryTokenCaches();
+
+        return 0;
     }
 }
