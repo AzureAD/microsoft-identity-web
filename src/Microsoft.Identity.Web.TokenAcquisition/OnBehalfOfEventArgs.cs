fd9fa35ee8df25c1dbebec82f5f29bab9217bab3
trwalke <trwalke@microsoft.com>
2026-02-09 22:29:04 -0800

    Updated comments

--- src/Microsoft.Identity.Web.TokenAcquisition/OnBehalfOfEventArgs.cs
+++ src/Microsoft.Identity.Web.TokenAcquisition/OnBehalfOfEventArgs.cs
@@ -7,18 +7,20 @@ namespace Microsoft.Identity.Web
 {
     /// <summary>
     /// Event arguments for on-behalf-of token acquisition operations.
-    /// Contains context information about the user and additional data for the token request.
+    /// Contains user assertion token information about the user and additional data for the token request.
     /// </summary>
     public class OnBehalfOfEventArgs
     {
         /// <summary>
         /// Gets or sets the claims principal representing the user for whom the token is being acquired.
-        /// This is the claims principal into the the api
+        /// This is the claims principal into the the api.
+        /// This value can be null.
         /// </summary>
         public ClaimsPrincipal? User { get; set; }
 
         /// <summary>
         /// Gets or sets additional context information for the token acquisition operation.
+        /// If the incoming token is encrypted, it will be decrypted and made available in this property.
         /// </summary>
         public string? UserAssertionToken { get; set; }
     }
