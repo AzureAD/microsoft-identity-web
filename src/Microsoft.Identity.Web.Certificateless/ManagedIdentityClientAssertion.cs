aaf41c9478cafc3507161afd7c85b09724a0b641
avdunn <avdunn@microsoft.com>
2026-04-23 14:34:59 -0700

    Update Azure.Identity and Extensions.Http

--- src/Microsoft.Identity.Web.Certificateless/ManagedIdentityClientAssertion.cs
+++ src/Microsoft.Identity.Web.Certificateless/ManagedIdentityClientAssertion.cs
@@ -138,6 +138,11 @@ namespace Microsoft.Identity.Web
           string message,
           bool containsPii)
         {
+            if (_logger == null)
+            {
+                return;
+            }
+
             switch (level)
             {
                 case Client.LogLevel.Always:
